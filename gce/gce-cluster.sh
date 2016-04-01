89886ed5de2ca462e4e12b5d8b96a97484f4b12e
Amit Bakshi <abakshi@xcalar.com>
2016-03-31 19:54:54 -0700

    Fixes for gce-cluster
    Change-Id: I3ff30c46174c863336ebc755f07606618b2f7c53

--- gce/gce-cluster.sh
+++ gce/gce-cluster.sh
@@ -10,9 +10,48 @@ if [ -z "$1" ] || [ "$1" == "--help" ] || [ "$1" == "-h" ]; then
 fi
 DIR="$(cd $(dirname ${BASH_SOURCE[0]}) && pwd)"
 INSTALLER="${1}"
-CLUSTER="${2:-`whoami`-xcalar}"
-COUNT="${3:-3}"
+INSTALLER_FNAME="$(basename $INSTALLER)"
+COUNT="${2:-3}"
+CLUSTER="${3:-`whoami`-xcalar}"
+CONFIG=/tmp/$CLUSTER-config.cfg
+UPLOADLOG=/tmp/$CLUSTER-manifest.log
+WHOAMI="$(whoami)"
+EMAIL="$(git config user.email)"
 INSTANCES=($(set -o braceexpand; eval echo $CLUSTER-{1..$COUNT}))
+if test -f "$INSTALLER"; then
+    INSTALLER_URL="repo.xcalar.net/builds/$INSTALLER_FNAME"
+    if ! gsutil ls gs://$INSTALLER_URL &>/dev/null; then
+        say "Uploading $INSTALLER to gs://$INSTALLER_URL"
+        until gsutil -m -o GSUtil:parallel_composite_upload_threshold=100M \
+                     cp -c -L "$UPLOADLOG" \
+                     "$INSTALLER" gs://$INSTALLER_URL; do
+            sleep 1
+        done
+        mv $UPLOADLOG $(basename $UPLOADLOG .log)-finished.log
+    else
+        say "$INSTALLER_URL already exists. Not uploading."
+    fi
+    INSTALLER=http://${INSTALLER_URL}
+fi
+
+if [[ "${INSTALLER}" =~ ^http:// ]]; then
+    if ! curl -Is "${INSTALLER}" | head -n 1 | grep -q '200 OK'; then
+        say "Unable to access ${INSTALLER}"
+        exit 1
+    fi
+elif [[ "${INSTALLER}" =~ ^gs:// ]]; then
+    if ! gsutil ls "${INSTALLER}" &>/dev/null; then
+        say "Unable to access ${INSTALLER}"
+        exit 1
+    fi
+else
+    say "WARNING: Unknown protocol ${INSTALLER}"
+fi
+
+rm -f $CONFIG
+$DIR/../bin/genConfig.sh $DIR/../bin/template.cfg $CONFIG ${INSTANCES[@]}
+
+
 say "Launching ${#INSTANCES[@]} instances: ${INSTANCES[@]} .."
 set -x
 gcloud compute instances create ${INSTANCES[@]} \
@@ -20,6 +59,6 @@ gcloud compute instances create ${INSTANCES[@]} \
     --zone ${ZONE:-us-central1-f} \
     --machine-type ${INSTANCE_TYPE:-n1-standard-4} \
     --network=private \
-    --metadata installer="$INSTALLER" \
-    --preemptible \
-    --metadata-from-file user-data=$DIR/gce-userdata.sh
+    --metadata "installer=$INSTALLER,count=$COUNT,cluster=$CLUSTER,owner=$WHOAMI,email=$EMAIL" \
+    --metadata-from-file user-data=$DIR/gce-userdata.sh,config=$CONFIG \
+    --preemptible
--- gce/gce-userdata.sh
+++ gce/gce-userdata.sh
@@ -1,21 +1,39 @@
 #!/bin/bash
-sudo apt-get update -y
-sudo apt-get install -y nfs-common
-sudo mkdir -p /mnt/nfs
-echo 'nfs:/srv/share/nfs /mnt/nfs   nfs defaults 0   0' | sudo tee -a /etc/fstab
-sudo mount -a
-
 cd /tmp
 IP="$(ifconfig eth0 | grep inet | awk '{print $2}' | awk -F':' '{print $2}')"
-echo "$IP   $(hostname -f) $(hostname -s)" | tee -a /etc/hosts
-curl -sSL $(/usr/share/google/get_metadata_value attributes/installer) > xcalar-installer
-chmod +x ./xcalar-installer
-./xcalar-installer
 CLUSTER=$(/usr/share/google/get_metadata_value attributes/cluster)
 if [ -z "$cluster" ]; then
     CLUSTER="${HOSTNAME%%-[0-9]*}"
 fi
-mkdir -p /mnt/nfs/cluster/$CLUSTER
-sudo mkdir -p /var/opt/xcalar
+COUNT=$(/usr/share/google/get_metadata_value attributes/count)
+
+sudo apt-get update -y
+sudo apt-get install -y nfs-common
+sudo mkdir -p /mnt/nfs
+sudo sed -i "@/mnt/nfs@d" /etc/fstab
+echo 'nfs:/srv/share/nfs /mnt/nfs   nfs defaults 0   0' | sudo tee -a /etc/fstab
+sudo mount -a
+
+CLUSTERDIR=/mnt/nfs/cluster/$CLUSTER
+
+mkdir -p $CLUSTERDIR/members
+sudo mkdir -m 0777 -p /var/opt/xcalar
+sudo sed -i "/$CLUSTER/d" /etc/fstab
 echo "nfs:/srv/share/nfs/cluster/$CLUSTER /var/opt/xcalar   nfs defaults 0   0" | sudo tee -a /etc/fstab
 sudo mount -a
+
+test -f /etc/hosts.orig || cp /etc/hosts /etc/hosts.orig
+(cat /etc/hosts.orig ; echo "$IP	$(hostname -f) $(hostname -s)") | sudo tee /tmp/hosts && sudo mv /tmp/hosts /etc/hosts
+echo "$IP	$(hostname -f) $(hostname -s)" | sudo tee $CLUSTERDIR/members/$(hostname -f)
+
+# Download and run the installer
+curl -sSL "$(/usr/share/google/get_metadata_value attributes/installer)" > xcalar-installer
+chmod +x ./xcalar-installer
+set +e
+set -x
+./xcalar-installer
+/usr/share/google/get_metadata_value attributes/config > xcalar-config
+sudo service xcalar stop || true
+sudo killall childnode
+sudo cp xcalar-config /etc/xcalar/default.cfg
+sudo service xcalar start
