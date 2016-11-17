9017d4fc143e9da0b7bef4a04cf5a021901c32c4
Amit Bakshi <abakshi@xcalar.com>
2016-11-16 17:45:00 -0800

    kvm/guest-ips: Add domain/vm/name/mac/ip
    Change-Id: I02270b463ed7701f5c4ee2cb077844e02a60c599

--- kvm/guest-ips.sh
+++ kvm/guest-ips.sh
@@ -6,8 +6,10 @@
 # get all domain ids
 for dom in $(virsh list | tail -n+3 | awk '{print $1}' | sed -e '/^$/d'); do
     # get all mac addresses for each domain
+    name="$(virsh domname $dom | head -1)"
     for mac in $(virsh domiflist $dom | grep -o -E "([0-9a-f]{2}:){5}([0-9a-f]{2})"); do
         # look at the arp table for the mac address
-        arp -e | grep $mac | grep -o -P "^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}"
+        guest_ip="$(arp -e | grep $mac | grep -o -P "^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}")"
+        echo "$dom  $name   $mac    $guest_ip"
     done
-done
+done | column -t
