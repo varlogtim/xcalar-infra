bdf1e9c6dfe6956b9c32f1be5a3efc4bf8642d24
Amit Bakshi <abakshi@xcalar.com>
2016-11-28 10:50:16 -0800

    gen-mac: Dont require python-virtinst
    Change-Id: I066d46f5fe7b556866e3e1edaa55a67b9845f565

--- kvm/gen-mac.py
+++ kvm/gen-mac.py
@@ -1,3 +1,14 @@
 #!/usr/bin/env python2.7
-import virtinst.util
-print virtinst.util.randomMAC()
+try:
+    import virtinst.util
+    print virtinst.util.randomMAC()
+except ImportError:
+    import random
+
+    def randomMAC():
+            mac = [ 0x00, 0x16, 0x3e,
+                    random.randint(0x00, 0x7f),
+                    random.randint(0x00, 0xff),
+                    random.randint(0x00, 0xff) ]
+            return ':'.join(map(lambda x: "%02x" % x, mac))
+    print randomMAC()
