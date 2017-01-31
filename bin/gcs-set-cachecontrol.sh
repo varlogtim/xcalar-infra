19ab65e26d130270b5aedba3e0f704f61949474b
Amit Bakshi <abakshi@xcalar.com>
2017-01-31 14:13:42 -0800

    gcs: Add cache control script
    Change-Id: If930155b69ab31fff73293efab9e65cbd3504fbc

---
+++ bin/gcs-set-cachecontrol.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#
+# Disable http caching for certain objects (like repo databases)
+
+CACHE_CONTROL="${CACHE_CONTROL:-no-cache, no-store, must-revalidate, max-age=0, no-transform}"
+
+gcs_dontcache () {
+    (set -x
+    gsutil -m setmeta -h "Cache-Control: $CACHE_CONTROL" "$@")
+}
+
+DEFAULT=('gs://repo.xcalar.net/rpm-deps/el6/x86_64/repodata/*'
+'gs://repo.xcalar.net/rpm-deps/el7/x86_64/repodata/*'
+'gs://repo.xcalar.net/apt/ubuntu/conf/*'
+'gs://repo.xcalar.net/apt/ubuntu/db/*'
+'gs://repo.xcalar.net/apt/ubuntu/dists/trusty/main/binary-amd64/*')
+
+test $# -eq 0 && set -- "${DEFAULT[@]}"
+gcs_dontcache "$@"
