e875d583c6d7b07fb2ea1eb621009bfaba94803e
Darrin Willis <dwillis@xcalar.com>
2017-06-12 21:35:21 -0700

    Rewrite license-server to be more production ready
    Requirements:
    A. We want usrnode to 'activate' its license at some regular interval, which
       will phone home to make sure we still allow the given license
    B. Phone-home should only done for some licenses
      a. Trusted customers are allowed to have disconnected, on-prem setups
      b. Untrusted customers must have internet to phone-home
    C. License management should not involve any manual script execution
      a. Adding a new license should be done via a web interface
      b. Existing Zendesk functionality should still work (with refactor)
      c. License activation as described in A
    
    Design / Implementation Plan:
    I. Activation webapp
      i. Publicly accessible so the usrnode can phone-home(see A)
      ii. Restricted database access so malicious user cannot fiddle with
          licenses (i.e. list all licenses, make new license)
    II. Management webapp
      i. Internally accessible so we can add new licenses
      ii. Unrestricted database access we can effectively manage licenses
    III. License 'Phone-Home' field
      i. Determines whether usrnode must phone-home
    IV. Usrnode changes
      i. Usrnode to check license at some regular interval
      ii. Usrnode to phone-home as a part of checking the license
        * Maybe use libcurl for HTTP ?
    
    Open Questions:
    Q1. Can we leverage existing technologies for an alternative approach?
    Q2. Should we use a heavy-weight RDBMS like Postgres?
       - If we want to be security conscious, this seems like a yes, barring Q1
    Q3. Barring Q1, how will we host this?
       - Amazon RDS seems good & suggested by Amit via Jerene
    
    Next steps:
    1. Figure out all needed APIs for Zendesk
    2. Figure out optimal database (probably not SQLite; PostgreSQL?)
    3. Make Zendesk changes
    4. Update license with 'needs to phone-home' field
    5. Make usrnode licensing changes & phone-home impl
    6. Roll out server & zendesk
    7. Roll out usrnode
    
    Change-Id: I275c21e23960dd52da3cfb12a9aa150f1fcca8b0

---
+++ license-server/license-server/app.py
@@ -0,0 +1,174 @@
+from datetime import datetime
+from collections import defaultdict
+import subprocess
+import ast
+import re
+import sqlite3
+import os
+import sys
+
+from flask import Flask, jsonify, abort, render_template, g
+
+app = Flask(__name__)
+
+app.config.from_object('config')
+app.config.from_envvar('XC_LICENSE_SERVER_SETTINGS')
+
+licenseKeyDb = app.config["LICENSE_KEY_DB"]
+dbUpgrade = app.config["LICENSE_KEY_DB_UPGRADE"]
+pubKey = app.config["XCALAR_PUBLIC_KEY"]
+
+readKeyCmd = "readKey"
+
+
+# Database management
+def getDb():
+    """Opens a new database connection if there is none yet for the
+    current application context.
+    """
+    if not hasattr(g, 'sqlite_db'):
+        g.sqlite_db = sqlite3.connect(licenseKeyDb)
+    return g.sqlite_db
+
+@app.teardown_appcontext
+def closeDb(error):
+    """Closes the database again at the end of the request."""
+    if hasattr(g, 'sqlite_db'):
+        g.sqlite_db.close()
+
+def initDb():
+    db = getDb()
+    with app.open_resource(dbUpgrade, mode='r') as f:
+        db.cursor().executescript(f.read())
+    db.commit()
+
+@app.cli.command('initdb')
+def initDbCommand():
+    initDb()
+    print 'Database initialized.'
+
+# Helper functions
+def getKeysForOrg(organization):
+    conn = getDb()
+
+    orgKeys = []
+    with conn:
+        cursor = conn.cursor()
+        cursor.execute("""
+            SELECT (license.key)
+            FROM license
+            LEFT JOIN organization ON license.org_id = organization.org_id
+            WHERE organization.name = :orgname
+            """,
+            {"orgname": organization})
+        orgKeys = cursor.fetchall()
+
+    if not orgKeys:
+        return []
+
+    unpackedKeys = []
+    for key in [k[0] for k in orgKeys]:
+         cmdOutput = subprocess.Popen([readKeyCmd, "-k", pubKey, "-l", key], stdout=subprocess.PIPE).communicate()[0]
+
+         keyProps = {}
+         for line in cmdOutput.splitlines():
+             elements = line.split(':')
+             propName = elements[0].replace('"', '')
+             propValue = elements[1].replace('"', '')
+             keyProps[propName] = propValue
+
+         unpackedKeys.append(keyProps)
+
+    return unpackedKeys
+
+# Request handlers
+@app.errorhandler(404)
+def pageNotFound(error):
+    return render_template('_error_license.html'), 404
+
+@app.route('/')
+def index():
+    return "Hello, World!"
+
+@app.route('/license/api/v1.0/keys', methods=['GET'])
+def getAllKeys():
+    conn = getDb()
+
+    allKeys = defaultdict(list)
+    with conn:
+        cursor = conn.cursor()
+        cursor.execute("""
+            SELECT organization.name, license.key
+            FROM license
+            LEFT JOIN organization ON license.org_id = organization.org_id""")
+        dbKeys = cursor.fetchall()
+        for key in dbKeys:
+            allKeys[key[0]].append(key[1])
+
+    return jsonify({'keys': allKeys})
+
+@app.route('/license/api/v1.0/keys/<string:organization>', methods=['GET'])
+def getKeys(organization):
+    keys = getKeysForOrg(organization)
+    if not keys:
+        abort(404)
+
+    return jsonify({'key': keys})
+
+@app.route('/license/api/v1.0/checkvalid/<string:key>', methods=['POST'])
+def checkvalid(key):
+    conn = getDb()
+    retObj = {"success": False}
+
+    with conn:
+        cursor = conn.cursor()
+        cursor.execute("""
+            INSERT INTO activation (key, active)
+            SELECT :key, active
+            FROM license
+            WHERE license.key = :key;
+            """,
+            {"key": key})
+        if not cursor.rowcount:
+            retObj["error"] = "License key not found"
+
+        activeRowId = cursor.lastrowid
+
+        cursor.execute("""
+            SELECT active
+            FROM activation
+            WHERE rowid = :rowid""",
+            {"rowid": activeRowId})
+        dbActive = cursor.fetchone()
+        if dbActive[0]:
+            retObj["success"] = True
+        else:
+            retObj["error"] = "License key inactive"
+
+    return jsonify(retObj)
+
+
+@app.route('/license/api/v1.0/activations/<string:key>', methods=['GET'])
+def activations(key):
+    conn = getDb()
+    actives = []
+    with conn:
+        cursor = conn.cursor()
+        cursor.execute("""
+            SELECT (timestamp, key)
+            FROM activation
+            """)
+        actives = cursor.fetchall()
+    return jsonify({"activations": actives})
+
+@app.route('/license/api/v1.0/keyshtml/<string:organization>', methods=['GET'])
+def getHtmlkey(organization):
+    keys = getKeysForOrg(organization)
+    if not keys:
+        abort(404)
+
+    output = sorted(keys, key=lambda k: datetime.strptime(k['expiration'], '%m/%d/%Y'), reverse=True)
+    return render_template('_table_render.html', keys=output)
+
+if __name__ == '__main__':
+    app.run(debug=True,host='0.0.0.0')
