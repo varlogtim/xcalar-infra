cc3a0d0902b351944ba47b4e884726ee95cdd9ce
Devang Shah <dshah@xcalar.com>
2017-06-16 13:09:40 -0700

    Xc-0000: fix functest launcher.sh for logging (redirect stdout/stderr to *.out/*.err not *.log)
    Change-Id: I12132a76eb159de8b435326da50f56130ddfecfe

--- functests/launcher.sh
+++ functests/launcher.sh
@@ -16,15 +16,15 @@ if [ -r "/etc/default/xcalar" ]; then
     . /etc/default/xcalar
 fi
 
-
 export XCE_CONFIG="${XCE_CONFIG:-/etc/xcalar/default.cfg}"
 XCE_USER="${XCE_USER:-root}"
-XCE_LOGDIR="${XCE_LOGDIR:-/var/log/xcalar}"
 export XLRDIR="${XLRDIR:-/opt/xcalar}"
 export XLRGUIDIR="${XLRGUIDIR:-/opt/xcalar/xcalar-gui}"
 LIBHDFS3_CONF="${LIBHDFS3_CONF:-/etc/xcalar/hdfs-client.xml}"
 PATH="$XLRDIR/bin:$PATH"
 XCE_LICENSEDIR="${XCE_LICENSEDIR:-/etc/xcalar}"
+XCE_LOGDIR="$(awk -F'=' '/^Constants.XcalarLogCompletePath/{print $2}' $XCE_CONFIG)"
+XCE_LOGDIR="${XCE_LOGDIR:-/var/log/xcalar}"
 
 export XCE_CONFIG XCE_USER XCE_LOGDIR XLRDIR LIBHDFS3_CONF PATH XCE_LICENSEDIR
 
@@ -52,11 +52,11 @@ NumNodes=$(awk -F= '/^Node.NumNodes/{print $2}' $XCE_CONFIG)
 
 declare -A monitorTmpLogs
 for ii in $(seq 0 $(( $NumNodes - 1 ))); do
-    monitorLog=$XCE_LOGDIR/xcmonitor.${ii}.log
+    monitorLog=$XCE_LOGDIR/xcmonitor.${ii}.out
     monitorTmpLog=`mktemp $XCE_LOGDIR/xcmonitorTmp.${ii}.XXXXXX`
     monitorTmpLogs[$ii]="$monitorTmpLog"
 
-    /opt/xcalar/bin/usrnode --nodeId $ii --numNodes $NumNodes --configFile $XCE_CONFIG >> $XCE_LOGDIR/node.${ii}.log 2>&1 </dev/null &
+    /opt/xcalar/bin/usrnode --nodeId $ii --numNodes $NumNodes --configFile $XCE_CONFIG 1>> $XCE_LOGDIR/node.${ii}.out 2>> $XCE_LOGDIR/node.${ii}.err </dev/null &
     pid=$!
     echo $pid > /var/run/xcalar/node.${ii}.pid
     ( /opt/xcalar/bin/xcmonitor -n $ii -c $XCE_CONFIG 2>&1 </dev/null & echo $! > /var/run/xcalar/xcmonitor.${ii}.pid ) | tee -a $monitorLog > $monitorTmpLog &
