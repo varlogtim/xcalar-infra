5b5579edefa1dc9c3f42f798c44cc4d5ad090764
Srinath Krishnamachari <skrishnamachari@xcalar.com>
2017-06-23 16:31:31 -0700

    Xc-0000 Update Jenkins job scripts.
    Removing some scripts that cannot/need not be moved to xcalar-infra repo.
    
    Change-Id: Id5a628beb3f144efd144ce8b19dbbbfb08642be4

--- jenkins/Cloud-Base-Packer2.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-make
-cd packer
-touch ~/.packer-vars
-
-. ~/lib/xcalar-infra/bin/activate
-make cloud-base 
--- jenkins/GerritReview-XcalarIDL.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# TODO: Add any verification code here for xcalar-solutions
-exit 0
