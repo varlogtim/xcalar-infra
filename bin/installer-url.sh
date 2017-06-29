5066cf848e8e5339ac718b94b0de7864e221a144
Amit Bakshi <abakshi@xcalar.com>
2017-06-29 02:22:44 -0700

    installer-url: Support -d s3 for self destructing URLs
    Change-Id: Ie6e4206ae4c48b69f82ec5dcddff633cc57369d1

--- bin/installer-url.sh
+++ bin/installer-url.sh
@@ -39,41 +39,68 @@ fi
 
 case "$1" in
     -h|--help)
-        say "usage: $0 <path/to/installer>"
+        say "usage: $0 [-d <gs|s3>] <path/to/installer>"
         say " upload the installer to repo.xcalar.net and print out new http url"
         exit 1
+        ;;
+    -d|--dest)
+        DEST="$2"
+        shift 2
         ;;
     -*)
         say "ERROR: Unknown option $1"
         exit 1
         ;;
-    *)
-        INSTALLER="$1"
-        shift
-        ;;
 esac
+INSTALLER="$1"
 
 
 if test -f "$INSTALLER"; then
     INSTALLER="$(readlink_f "${INSTALLER}")"
+    BUILD_SHA="$(dirname ${INSTALLER})/../BUILD_SHA"
+    if test -f "$BUILD_SHA"; then
+        SHAS=($(awk '{print $(NF)}'  "${BUILD_SHA}" | tr -d '()'))
+        SHA1="${SHAS[0]}-${SHAS[1]}"
+    else
+        SHA1="$(sha1sum $INSTALLER | awk '{print $1}')"
+    fi
     INSTALLER_FNAME="$(basename $INSTALLER)"
     if [[ "$INSTALLER" =~ '/debug/' ]]; then
-        INSTALLER_URL="repo.xcalar.net/builds/debug/$INSTALLER_FNAME"
+        DEST_FNAME="debug/$INSTALLER_FNAME"
     elif [[ "$INSTALLER" =~ '/prod/' ]]; then
-        INSTALLER_URL="repo.xcalar.net/builds/prod/$INSTALLER_FNAME"
+        DEST_FNAME="prod/$INSTALLER_FNAME"
     else
-        INSTALLER_URL="repo.xcalar.net/builds/$INSTALLER_FNAME"
+        DEST_FNAME="$INSTALLER_FNAME"
     fi
-    if ! check_url "http://${INSTALLER_URL}"; then
-        say "Uploading $INSTALLER to gs://$INSTALLER_URL"
-        until gsutil -m -o GSUtil:parallel_composite_upload_threshold=100M -q \
-                      cp -c "$INSTALLER" gs://$INSTALLER_URL; do
-            sleep 1
-        done
-    else
-        say "http://$INSTALLER_URL already exists. Not uploading."
-    fi
-    echo http://${INSTALLER_URL}
+    test -z "$DEST" && DEST=gs
+    case "$DEST" in
+        gs) DEST="gs://repo.xcalar.net/builds";;
+        s3) DEST="s3://xcrepo/builds";;
+    esac
+    DEST_URL="${DEST}/${SHA1}/${DEST_FNAME}"
+    case "${DEST_URL}" in
+        s3://*)
+            if ! aws s3 ls "$DEST_URL" &>/dev/null; then
+                say "Uploading $INSTALLER to $DEST_URL"
+                aws s3 cp --only-show-errors "$INSTALLER" "$DEST_URL"
+            fi
+            aws s3 presign --expires-in ${EXPIRY:-1800} "$DEST_URL"
+            ;;
+        gs://*)
+            if ! gsutil ls "$DEST_URL"; then
+                say "Uploading $INSTALLER to $DEST_URL"
+                until gsutil -m -o GSUtil:parallel_composite_upload_threshold=100M -q \
+                            cp -c "$INSTALLER" "$DEST_URL"; do
+                    sleep 1
+                done
+            fi
+            echo http://${INSTALLER_URL}
+            ;;
+        *)
+            say "Unknown resource ${DEST_URL}"
+            exit 1
+            ;;
+    esac
     exit 0
 elif [[ "${INSTALLER}" =~ ^http[s]?:// ]]; then
     if ! check_url "${INSTALLER}"; then
