ec78fa72e90703b324c1ff87c10e07f1c7de9515
Amit Bakshi <abakshi@xcalar.com>
2017-08-29 10:32:16 -0700

    Add jenkins-cli args to list plugins
    Change-Id: If6b6d97f713a854aa6a30116d5c96ae211a5b3c7

--- bin/jenkins-cli.sh
+++ bin/jenkins-cli.sh
@@ -5,6 +5,29 @@ JENKINS_HOST="${JENKINS_URL#https://}"
 JENKINS_HOST="${JENKINS_HOST%/}"
 PORT="${PORT:-22022}"
 
+jenkins_cli () {
+    ssh -oPort=${PORT} -oUser=${USER} ${JENKINS_HOST} "$@"
+}
+
+usage () {
+    cat << EOF
+$0 [-l list current plugins] [-n list newest plugins after restart] [-h help] -- [jenkins-cli args]
+EOF
+    exit 1
+}
+
 test $# -gt 0 || set -- help
 
-exec ssh -oPort=${PORT} ${JENKINS_HOST} "$@"
+filter () { cat; }
+
+while getopts "ln" opt "$@"; do
+    case $opt in
+        h) usage;;
+        l) filter() { tr -d '()' | awk '{printf "%s:%s\n",$1,$(NF)}'; };;
+        n) filter() { sed -re 's/\([0-9].*//g' | awk '{printf "%s:%s\n",$1,$(NF)}'; };;
+        -*) usage;;
+        --) break;;
+    esac
+done
+shift $((OPTIND-1))
+jenkins_cli "$@" | filter
