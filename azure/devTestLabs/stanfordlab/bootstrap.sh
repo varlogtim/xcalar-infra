4e2011014e23eccf7aef973cf26ef4f18e9b088b
Amit Bakshi <abakshi@xcalar.com>
2017-10-13 08:32:28 -0700

    StanfordLab fixes
    - Fixes to bootstrap to add certs and basic tools
    - Last minute fixes patched in
    - Add tool register-dns.sh and aws-route53-add-cname.sh
    
    Change-Id: I4100b19ebd92be51d71e75864137a6d1c2e4f6af

--- azure/devTestLabs/stanfordlab/bootstrap.sh
+++ azure/devTestLabs/stanfordlab/bootstrap.sh
@@ -3,11 +3,12 @@ XCE_HOME=${XCE_HOME:-/var/opt/xcalar}
 XLRDIR=${XLRDIR:-/opt/xcalar}
 XCE_LICENSEDIR=${XCE_LICENSEDIR:-/etc/xcalar}
 ADMIN_USERNAME=${ADMIN_USERNAME:-"xcuser"}
-ADMIN_PASSWORD=${ADMIN_PASSWORD:-"Xc4larStanf0rd"}
+ADMIN_PASSWORD=${ADMIN_PASSWORD:-"StanfordLab"}
 ADMIN_EMAIL=${ADMIN_EMAIL:-"xcuser@xcalar.com"}
 XCALAR_ADVENTURE_DATASET=${XCALAR_ADVENTURE_DATASET:-"http://pub.xcalar.net/datasets/xcalarAdventure.tar.gz"}
-
+XCE_CONFDIR="${XCE_CONFDIR:-/etc/xcalar}"
 LICENSE="AEAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAJAAAJAAAAACAVZHBAJXAXN7SB5D3Y45B7BJSHE62ST8S7N7QWHYP9XQE6AP67552FAS3Y8FXPAE6CXVXGWMDY4YTST2AABWAQ========"
+CADDYURL=http://repo.xcalar.net/deps/caddy_linux_amd64_custom-0.10.10.tar.gz
 #License key version is: Version 1.0.2
 #Product family is: XcalarX
 #Product is: Xce
@@ -22,40 +23,94 @@ safe_curl () {
 }
 
 postConfig() {
-    sudo cp "$XCE_CONFIG" "${XCE_CONFIG}.bak"
+    cp "$XCE_CONFIG" "${XCE_CONFIG}.bak"
 
     # Turn on support bundles
-    echo "Constants.SendSupportBundle=true" | sudo tee -a "$XCE_CONFIG"
+    echo "Constants.SendSupportBundle=true" | tee -a "$XCE_CONFIG"
 
     # Add in Azure Blob Storage SAS tokens
-    echo "AzBlob.stanfordstudentsdatasets.sasToken=?sv=2017-04-17&ss=b&srt=sco&sp=rwlac&se=2017-10-14T22:55:26Z&st=2017-10-12T14:55:26Z&spr=https&sig=7KpOaXGXX3sID3b1bhPrlIF7m0ALQsuPW9A4PkQ5rm0%3D" | sudo tee -a "$XCE_CONFIG"
-    echo "AzBlob.xcalardatawarehouse.sasToken=?sv=2017-04-17&ss=b&srt=sco&sp=rl&se=2017-10-14T23:55:11Z&st=2017-10-12T15:55:11Z&spr=https&sig=%2BKMdhtUbBKrDicTGVAAPzkCXk6azD3DgHAoGbhdN7MQ%3D" | sudo tee -a "$XCE_CONFIG"
+    echo "AzBlob.stanfordstudentsdatasets.sasToken=?sv=2017-04-17&ss=b&srt=sco&sp=rwlac&se=2017-10-14T22:55:26Z&st=2017-10-12T14:55:26Z&spr=https&sig=7KpOaXGXX3sID3b1bhPrlIF7m0ALQsuPW9A4PkQ5rm0%3D" | tee -a "$XCE_CONFIG"
+    echo "AzBlob.xcalardatawarehouse.sasToken=?sv=2017-04-17&ss=b&srt=sco&sp=rl&se=2017-10-14T23:55:11Z&st=2017-10-12T15:55:11Z&spr=https&sig=%2BKMdhtUbBKrDicTGVAAPzkCXk6azD3DgHAoGbhdN7MQ%3D" | tee -a "$XCE_CONFIG"
 
     # Burn the trial license
     LICENSE_FILE="$XCE_LICENSEDIR/XcalarLic.key"
-    sudo cp "$LICENSE_FILE" "${LICENSE_FILE}.bak"
-    echo "$LICENSE" | sudo tee "$LICENSE_FILE"
+    cp "$LICENSE_FILE" "${LICENSE_FILE}.bak"
+    echo "$LICENSE" | tee "$LICENSE_FILE"
 
-    # Add default admin user
-    jsonData="{ \"defaultAdminEnabled\": true, \"username\": \"$ADMIN_USERNAME\", \"email\": \"$ADMIN_EMAIL\", \"password\": \"$ADMIN_PASSWORD\" }"
-    sudo mkdir -p "$XCE_HOME/config"
-    sudo chown -R xcalar:xcalar "$XCE_HOME/config"
-    echo "Creating default admin user $ADMIN_USERNAME ($ADMIN_EMAIL)"
-    # Don't fail the deploy if this curl doesn't work
-    safe_curl -H "Content-Type: application/json" -X POST -d "$jsonData" "http://127.0.0.1:12124/login/defaultAdmin/set" || true
+    mkdir -p "$XCE_HOME/config"
+    chown -R xcalar:xcalar "$XCE_HOME/config"
 
     # Let's retrieve the xcalar adventure datasets now
     if [ ! -d "/netstore" ]; then
-        sudo mkdir -p /netstore/datasets/adventure
+        mkdir -p /netstore/datasets/adventure
         safe_curl -sSL "$XCALAR_ADVENTURE_DATASET" > xcalarAdventure.tar.gz
         tar -zxvf xcalarAdventure.tar.gz
-        sudo mv XcalarTraining /netstore/datasets/ || true
-        sudo mv dataPrep /netstore/datasets/adventure/ || true
-        sudo chown -R xcalar:xcalar /netstore
+        mv XcalarTraining /netstore/datasets/ || true
+        mv dataPrep /netstore/datasets/adventure/ || true
+        chown -R xcalar:xcalar /netstore
     fi
+    # Download puppet
+    curl http://repo.xcalar.net/scripts/install-puppet-agent.sh -o /tmp/install-puppet.sh || true
+    bash -x /tmp/install-puppet.sh || true
 
-    sudo service xcalar stop
-    sudo service xcalar start
+    # Download caddy
+    curl -L ${CADDYURL} | tar zxvf - -C ${XLRDIR}/bin caddy
+    setcap cap_net_bind_service=+ep ${XLRDIR}/bin/caddy
+
+    # download certs
+    curl -L https://xccerts.s3.amazonaws.com/certs/xcalar.io/xcalar-stanfordlab-100.xcalar.io.key -o ${XCE_CONFDIR}/cert.key
+    curl -L https://xccerts.s3.amazonaws.com/certs/xcalar.io/xcalar-stanfordlab-100.xcalar.io.pem -o ${XCE_CONFDIR}/cert.pem
+    # fix Apache
+    sed -i -e 's|^SSLCertificateKeyFile.*$|SSLCertificateKeyFile /etc/xcalar/cert.key|g' /etc/httpd/conf.d/XI-ssl.conf
+    sed -i -e 's|^SSLCertificateFile.*$|SSLCertificateFile /etc/xcalar/cert.pem|g' /etc/httpd/conf.d/XI-ssl.conf
+    sed -i -e 's|^#SSLCertificateChainFile.*$|SSLCertificateChainFile /etc/xcalar/cert.pem|g' /etc/httpd/conf.d/XI-ssl.conf
+    # fix Jupyter
+    echo "c.NotebookApp.keyfile = u'/etc/xcalar/cert.key'" >> $XCE_HOME/.jupyter/jupyter_notebook_config.py
+    echo "c.NotebookApp.certfile = u'/etc/xcalar/cert.pem'" >> $XCE_HOME/.jupyter/jupyter_notebook_config.py
+
+    # fix Caddyfile
+    cp -n ${XCE_CONFDIR}/Caddyfile ${XCE_CONFDIR}/Caddyfile.orig
+    (
+    echo ":443 {"
+    tail -n+2 ${XCE_CONFDIR}/Caddyfile.orig
+    echo ":80 {"
+    echo "  redir https://{host}{uri}"
+    echo "}"
+    ) | sed -e "s|tls.*$|tls ${XCE_CONFDIR}/cert.pem ${XCE_CONFDIR}/cert.key|g" -e "s|root.*$|root /var/www/xcalar-gui|g" | tee ${XCE_CONFDIR}/Caddyfile
+    chown -R xcalar:xcalar ${XCE_CONFDIR}
+    groupadd -r -g 999 docker
+    groupadd -r sudo
+    echo '%xcuser  ALL=(ALL) NOPASSWD: ALL' > /etc/sudoers.d/xcuser
+    echo '%sudo ALL=(ALL) NOPASSWD: ALL' > /etc/sudoers.d/sudo
+    sed -i -r 's|^#?Port 22.*|Port 22022|g' /etc/ssh/sshd_config
+
+    service xcalar stop-supervisor || true
+    service xcalar stop || true
+    systemctl stop httpd || true
+    systemctl disable httpd || true
+    touch ${XCE_CONFDIR}/Caddyfile.run
+    service xcalar start
+    chown xcalar:xcalar ${XCE_CONFDIR}/Caddyfile.run
+    chkconfig xcalar on
+    chown xcalar:apache /etc/xcalar/cert.*
+    chmod 0640 /etc/xcalar/cert.*
+    # AzureCLI (ref: https://docs.microsoft.com/en-us/cli/azure/install-azure-cli?view=azure-cli-latest)
+    rpm --import https://packages.microsoft.com/keys/microsoft.asc
+    echo -e "[azure-cli]\nname=Azure CLI\nbaseurl=https://packages.microsoft.com/yumrepos/azure-cli\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/azure-cli.repo
+    yum makecache fast
+    # Some handy tools to have around
+    yum install -y htop vim-enhanced iperf3 yum-utils samba-client samba-common cifs-utils \
+                   nfs-utils parted gdisk curl \
+                   jq python-pip awscli azure-cli \
+                   htop iftop iperf3 vim-enhanced tmux
+    #yum install -y http://repo.xcalar.net/deps/jdk-8u144-linux-x64.rpm
+    #groupadd -g 1000 xcdev
+    #useradd -m -s /bin/bash -u 1000 -g 1000 -G docker,sudo xcdev
+    echo "Creating default admin user $ADMIN_USERNAME ($ADMIN_EMAIL)"
+    # Add default admin user
+    jsonData="{ \"defaultAdminEnabled\": true, \"username\": \"$ADMIN_USERNAME\", \"email\": \"$ADMIN_EMAIL\", \"password\": \"$ADMIN_PASSWORD\" }"
+    # Don't fail the deploy if this curl doesn't work
+    safe_curl -H "Content-Type: application/json" -X POST -d "$jsonData" "http://127.0.0.1:12124/login/defaultAdmin/set" || true
 }
 
 postConfig
