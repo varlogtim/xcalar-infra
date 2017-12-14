73bc02fa1c40ddc50958906aab416272072a689b
Amit Bakshi <abakshi@xcalar.com>
2017-12-14 11:12:06 -0800

    Terraform: Fix remote_state locations
    Change-Id: Icd52cf9473a7851e8b73a7f4a908052499ae5591

--- tf/aws/infra/state.tf
+++ tf/aws/infra/state.tf
@@ -1,17 +1,17 @@
 terraform {
   backend "s3" {
     bucket = "xctfstate-us-east-1"
-    key    = "tf/aws/customer/terraform.tfstate"
+    key    = "tf/aws/infra/terraform.tfstate"
     region = "us-east-1"
   }
 }
 
 ## Use the following to load remote state
-#data "terraform_remote_state" "customer" {
+#data "terraform_remote_state" "infra" {
 #  backend = "s3"
 #  config {
 #    bucket = "xctfstate-us-east-1"
-#    key    = "tf/aws/customer/terraform.tfstate"
+#    key    = "tf/aws/infra/terraform.tfstate"
 #    region = "us-east-1"
 #  }
 #}
