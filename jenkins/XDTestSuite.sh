f9e5ab8e2f0fd2aaff455c5c8a0c428b1f81b1fa
Jerene Yang <jyang@xcalar.com>
2018-03-07 21:21:47 -0800

    Add a lag
    Change-Id: Ie3f72fa54195fa752937f9e0ba8bb246852afc08

--- jenkins/XDTestSuite.sh
+++ jenkins/XDTestSuite.sh
@@ -112,7 +112,7 @@ HTTP_RESPONSE=$(curl --silent --write-out "HTTPSTATUS:%{http_code}" -X GET $URL)
 
 URL="http://$TEST_DRIVER_HOST:$TEST_DRIVER_PORT/action?name=getstatus"
 HTTP_BODY="Still running"
-
+sleep 5
 ls -art /tmp | grep "chromium" | tail -n 1 | xargs -n1 -I@ tail -f @/chrome_debug.log &
 
 while [ "$HTTP_BODY" == "Still running" ]
