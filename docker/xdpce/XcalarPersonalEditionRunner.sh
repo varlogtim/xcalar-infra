fed7cd02265ae7472a0cd801be26aae0546fb8cc
Jolie Olsen <jolsen@xcalar.com>
2018-03-30 23:54:22 -0700

    [xpe cmd tool] code review follow up
    1. addressing remaining code review issues here
    https://gerrit.int.xcalar.com/#/c/9052/
    
    2. tags and docker cleanup:
       add build number tag to xdpce image once it is made.
       in XcalarPersonalEditionBuilder.sh (the script run by Jenkins)
       remove images generated (latest as well as tagged with build number)
       @todo: cleanup task in jenkins to do same, in case build fails
    
    3. use 127.0.0.1 as XEMHOSTIP all the time now,
       since the docker containers will be on the localhost.
       This allows xem.cfg to be fully static, so check that in as
       a static file and remove file manipulation of it in the local_installer
    
    4. /var/opt/xcalar needs the dirs .ipython and .jupyter for
       jupyter to run.  These are generated during install, but lost
       when mounting user dir in to /var/opt/xcalar on their remote
       machine running the installer.  previously was cocpying in
       a fully copy of /var/opt/xcalar, but since it is only these
       dirs needed, only docker cp these in the Makefile, and map them
       directly in the local_installer.
       The dependency on these for jupyter being done at install time
       only is expected to change in the installer, at which time
       should stop copying these in to package in the installer.
    
    5. introduce a blank license key.  can use as placeholder and write
       to it when authentication portion of project is implemented
       So the license key for dev purposes is no longer being used, only
       this blank license file
    
    6. call new make target for generating the grafana-graphite image
       (see review: https://gerrit.int.xcalar.com/9102)
    
    7. remove staging dir when done (has tar file)
    
    Change-Id: I0547bf2b75e65d6fa935ec96d75a6c8de4d0abc5

--- docker/xdpce/XcalarPersonalEditionRunner.sh
+++ docker/xdpce/XcalarPersonalEditionRunner.sh
@@ -13,34 +13,42 @@ mkdir -p $FINALDEST
 cd "$XLRINFRADIR/docker/xdpce"
 make docker-image INSTALLER_PATH="$PATH_TO_XCALAR_INSTALLER"
 # running make will save an image of the container (xdpce.tar.gz)
-# along with these dirs which are default dirs for xcalar's config and home,
+# along with these dirs which get generated install time in xcalar home,
 # that you will need if you map local volumes there (as it will overwrite
 # what is currently there)
-cp -R configcopy/ $FINALDEST
+cp -R .ipython/ $FINALDEST
+cp -R .jupyter/ $FINALDEST
+cp -R jupyterNotebooks $FINALDEST
 cp xcalar $FINALDEST
 cp xdpce.tar.gz $FINALDEST
-cp -R homecopy/ $FINALDEST
 
 # build the grafana-graphite container.
 cd $GRAFANADIR
-make
+make grafanatar
 # it will have saved an image of the grafana container
 cp grafana_graphite.tar.gz $FINALDEST
 
 # go to the final dir tar these both together
 cd $FINALDEST
-cp "$XLRINFRADIR/docker/xdpce/XcalarLic.key" .
+cp "$XLRINFRADIR/docker/xdpce/trial.key" .
 cp "$XLRINFRADIR/docker/xdpce/xem.cfg" .
 cp "$XLRINFRADIR/docker/xdpce/local_installer.sh" .
-thingstotar="xdpce.tar.gz grafana_graphite.tar.gz configcopy/ homecopy/ XcalarLic.key xcalar xem.cfg"
+thingstotar="xdpce.tar.gz grafana_graphite.tar.gz trial.key xcalar xem.cfg .ipython/ .jupyter/ jupyterNotebooks"
 tarfile=restar.tar.gz
 tar -czf $tarfile $thingstotar
 
 # stop the docker containers created and remove them so not left over on jenkins slave after Job completes
+# if you dont remove the images, then next time Jenkins slave runs this job, when it saves the xdpce and
+# grafana/graphite images, will be saving all existing tagged images - not just the current one!
 docker rm -f xdpce || true
+docker rm -f grafana_graphite || true
+docker rmi -f xdpce || true
+docker rmi -f xdpce:$BUILD_NUMBER || true
+docker rmi -f grafana_graphite || true
+docker rmi -f grafana_graphite:$BUILD_NUMBER || true
 
 # remove the files we put in to the tar file
 rm -r $thingstotar #$tarfile
 
 # printing to stdout for other scripts to call
-echo $FINALDEST >&2
+echo $FINALDEST
--- docker/xdpce/local_installer.sh
+++ docker/xdpce/local_installer.sh
@@ -9,7 +9,7 @@
 #
 # script should exist in the same dir as restar.tar.gz tarball
 # (that tarball should contain the docker images, required install files,
-# and default Xcalar mappings for xlr home, config, etc.)
+# and default Xcalar mappings for xlr config, etc.)
 #
 # - You can generate the restar.tar.gz tarball by running Jenkins job:
 #   XcalarPersonalEditionBuilder
@@ -23,11 +23,6 @@
 # one for XEM cluster), and load the saved images for Grafana and XCE cluster
 # included in the tarball, in to those containers.
 # Grafana and XCE will be configured to communiicate automatically for stats collection.
-#
-# dirs on local filesystem are mapped in to the xdpce container
-# to certain Xcalar directories (such as xcalar home, config, logs),
-# running this installer if you already have XPE installed will 'upgrade' to latest Xcalar,
-# but persist current session data
 #
 set -e
 
@@ -49,45 +44,9 @@ GRAFANA_CONTAINER_NAME=grafana_graphite
 clear_containers() {
 
     debug "Remove old docker containers..."
-    remove_image $XCALAR_IMAGE
-    remove_image $GRAFANA_IMAGE
-    remove_container $XCALAR_CONTAINER_NAME
-    remove_container $GRAFANA_CONTAINER_NAME
-}
+    docker rm -f $XCALAR_CONTAINER_NAME >/dev/null 2>&1 || true
+    docker rm -f $GRAFANA_CONTAINER_NAME >/dev/null 2>&1 || true
 
-remove_image() {
-
-    # must remove any containers hosting that image, too
-
-    # stop any running containers before deleting them
-    # ('docker ps' returns only running containers)
-    debug "Stop any running containers hosting image $1..."
-    docker ps | awk '{ print $1,$2 }' | grep $1 | awk '{print $1 }' | xargs -I {} docker stop {}
-
-    # 'docker ps -a' returns all dockers containrers running or not...
-    debug "Remove any stopped containers hosting image $1..."
-    docker ps -a | awk '{ print $1,$2 }' | grep $1 | awk '{print $1 }' | xargs -I {} docker rm {}
-
-    # remove the image, if it exists
-    debug "Remove image of name $1..."
-    docker ps -a | awk '{ print $1,$2 }' | grep $1 | awk '{print $1 }' | xargs -I {} docker rmi {}
-}
-
-remove_container() {
-
-    # 'docker ps' returns only running containers
-    debug "Stop any running containers by name $1..."
-    if [ "$(docker ps -q -f name=$1)" ]; then
-        debug "stopping $1..."
-        docker stop $1
-    fi
-    # 'docker ps -a' will return all up and down; remove any
-    debug "Remove any containers by name $1 ... "
-    if [ "$(docker ps -a -q -f name=$1)" ]; then
-        # get the image
-        debug "removing container $1..."
-        docker rm $1
-    fi
 }
 
 # to make debug statements
@@ -103,162 +62,72 @@ debug() {
 # (not the staging/tmp dir where install work is done,
 # these should remain even after installation complete, as long as they want XPE)
 XPEDIR="$HOME/xcalar_personal_edition"
+XPEIMPORTS="$XPEDIR/imports" # dedicated dir where user can map datasets, etc. for access in Xcalar
+XPEDATA="$XPEDIR/data"
+LOCALLOGDIR="$XPEDIR/xceLogs" # will mount to /var/log/xcalar so logs persist through upgrades
+LOCALXCEHOME="$XPEDIR/xceHome" # will mount /var/opt/xcalar here so session data, etc. persissts through upgrde
 
 XEM_PORT_NUMBER=15000 # should be port # in xemconfig
 # files that will be required for completing the installation process
 XDPCE_TARBALL=xdpce.tar.gz
 GRAFANA_TARBALL=grafana_graphite.tar.gz
-XEMCONFIGFILE=xem.cfg # going to create default.cfg from this
-REQ_INSTALL_FILES=("$GRAFANA_TARBALL" "$XDPCE_TARBALL" "$XEMCONFIGFILE" "$VOLMNTS")
 
-# create tmp staging dir for extracting the installation contents
-# move the tarball there and extract it
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-STAGING_DIR=`mktemp -d 2>/dev/null || mktemp -d -t 'xpeinstaller'` # creates tmp dir on osx and linux
-debug "Staging directory: $STAGING_DIR"
-cd $STAGING_DIR
+## @TODO:
+## prior to clearing container:
+## BACKUP EXISTING DATA (XPEDIR and curr docker images for containers)
+## so if upgrading and upgrade does not succeed, can go back to previous state
+clear_containers
 
-            ## UNINSTALL OPTION ##
+        ##  UNINSTALL OPTION ##
 
 # if user running this as an uninstall, back up contents before deleting the dir
 if [ "$1" == uninstall ]; then
 
     if [ -d $XPEDIR ]; then
         BACKUPDIR="$HOME/xpe_backup_$(date +%F_%T)"
-        mkdir -p $BACKUPDIR
-        debug "Backup $XPEDIR to $BACKUPDIR then remove"
-        cp -a "$XPEDIR/." "$BACKUPDIR"
-        rm -r $XPEDIR
+        debug "mv $XPEDIR to $BACKUPDIR"
+        mv $XPEDIR $BACKUPDIR
+        addmsg=" A backup of previous install data, can be found at $BACKUPDIR"
     fi
 
-    clear_containers
-
-    echo "XPE has been uninstalled.  A backup of previous install data found, can be found here $BACKUPDIR" >&2
-
+    echo "XPE has been uninstalled. $addmsg" >&2
     exit 0
 fi
 
     ## CREATE INSTALLER DIRS, and UNPACK TAR FILE W REQUIRED FILES INTO INSTALLER DIR ##
 
 # make the default user installation dirs
-mkdir -p $XPEDIR
+mkdir -p "$XPEIMPORTS"
+mkdir -p "$XPEDATA"
+mkdir -p "$LOCALLOGDIR"
+mkdir -p "$LOCALXCEHOME"
 
-echo "Copy tar file.. (if over netstore could take some time...)" >&2
+# create tmp staging dir for extracting the installation contents
+# move the tarball there and extract it
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+STAGING_DIR=`mktemp -d 2>/dev/null || mktemp -d -t 'xpeinstaller'` # creates tmp dir on osx and linux
+cd $STAGING_DIR
+
+echo "Copy tar file.. to staging dir $STAGING_DIR (if over netstore could take some time...)" >&2
 cp "$DIR/restar.tar.gz" .
 tar -xzf restar.tar.gz
 
-# all required files should be here now that untarred...
-for reqfile in "${REQ_INSTALL_FILES[@]}"
-do
-    if [ ! -e $reqfile ]; then
-        echo "Missing required file $reqfile (is it being packed in your tarball?)" >&2
-        exit 1
-    fi
-done
+cp -R .ipython/ .jupyter/ jupyterNotebooks/ $LOCALXCEHOME # put these here in case of initial install, need them in xce home
+cp trial.key xem.cfg $XPEDATA # will mount these files individually from data dir
 
-    ## CONFIGURE default.cfg FOR XCALAR / GRAFANA COMMUNICATION
+    ###  LOAD THE PACKED IMAGES AND START THE NEW CONTAINERS ##
 
-# base version of the file required to link Xcalar with Grafana is included in the installer (xem.cfg)
-# append the user's IP hosting grafana, to it
-
-debug "OS type: $OSTYPE"
-if [ $OSTYPE == 'linux-gnu' ]; then
-    hostip="$(ifconfig eth0 | grep "inet addr" | cut -d ':' -f 2 | cut -d ' ' -f 1)"
-else
-    hostip="$(ipconfig getifaddr en0)"
-fi
-debug "HOST IP: $hostip"
-
-if [ ! -f $XEMCONFIGFILE ]; then
-    echo "No xem config file $XEMCONFIGFILE (is it being packed in your tar?)" >&2
-    exit 1
-fi
-
-REQVARS=(
-    XemStatsPushHeartBeat=10000
-    XemClusterName=xemmonitorcluster
-    XemHostAddress=$hostip
-    XemHostPortNumber=$XEM_PORT_NUMBER
-    XemStatsShipmentEnabled=true
-)
-for extravar in "${REQVARS[@]}"
-do
-    echo "Constants.$extravar" >> "$STAGING_DIR/$XEMCONFIGFILE"
-done
-#echo "Constants.XemHostAddress=$hostip" >> "$STAGING_DIR/$XEMCONFIGFILE"
-
-# copy to mount location for default.cfg, even on upgrade, in case ip has changed
-cp "$XEMCONFIGFILE" "$HOME/xcalar_personal_edition/default.cfg"
-
-    ## set up volume mounts for xdpce container ##
-
-# when creating docker containers via docker run,
-# will map in outside volumes with -v option
-# while going through mount file, construct the arg string
-# for each volume to map
-MNTARGS=""
-
-# for each path to mount:
-# d|f (file or dir) <path on local file system> <destination path on xdpce container> <default content packaged in installer (if any)> 
-mounts=(
-    "d" "$XPEDIR/IMPORTS" "/mnt/imports2" ""
-    "d" "$XPEDIR/XCE_HOME" "/var/opt/xcalar" "homecopy/"
-    "d" "$XPEDIR/XCE_CONFIG" "/etc/xcalar" "configcopy/"
-    "d" "$XPEDIR/XCE_LOGS" "/var/log/xcalar" ""
-    "f" "$XPEDIR/xcalar" "/etc/default/xcalar" "xcalar"
-    "f" "$XPEDIR/XcalarLic.key" "/etc/xcalar/XcalarLic.key" "XcalarLic.key"
-    "f" "$XPEDIR/default.cfg" "/etc/xcalar/default.cfg" "default.cfg"
-)
-
-ITER=0
-mtype=""
-lpath=""
-dpath=""
-installerDefault=""
-for item in "${mounts[@]}"
-do
-    ITER=$((ITER+1))
-    if [ "$ITER" -gt "0" ] && [ $((ITER % 4)) -eq 0 ]; then
-        mtype=${mounts[$((ITER-4))]}
-        lpath=${mounts[$((ITER-3))]}
-        dpath=${mounts[$((ITER-2))]}
-        installerDefault=${mounts[$((ITER-1))]}
-    else
-        continue
-    fi
-
-    if [ ! -e "$lpath" ]; then
-
-        if [ "$mtype" == "d" ]; then
-            mkdir -p $lpath
-        fi
-
-        if [ -n "$installerDefault" ]; then
-            if ! [ -e "$installerDefault" ]; then
-                echo " $installerDefault specified as default for $dpath, but not in the installer dir. (is it being packed in your tar?)" >&2
-                exit 1
-            fi
-
-            if [ "$mtype" == "d" ]; then
-                cp -a "$installerDefault/." "$lpath"
-            elif [ "$mtype" == "f" ]; then
-                cp "$installerDefault" "$lpath"
-            fi
-        else
-            if [ "$mtype" == "f" ]; then
-                "Trying to map local file $lpath; can't find installer default and file doesn't exist on your computer..."
-                exit 1
-            fi
-        fi
-    fi
-
-    MNTARGS="$MNTARGS -v $lpath:$dpath"
-done
-
-    ###  REMOVE OLD DOCKER GRAFANA AND XDPCE CONTAINERS AND INSTALLE NEW ONES ##
-
-clear_containers
-
+# each load, will have 2 versions of the image:
+# xdpce:latest and xdpce:<build number> (same for grafana-graphite)
+# want to keep previous install images, but only need one - the xdpce:<old build number>
+# (if you specify 'rmi image' without a tag, will remove image:latest)
+# also if you do not do this, when you load the new images in the tar
+# (xdpce:latest and xdpce:<new build number>, and similar for grafana-graphite)
+# will detect existing xdpce:latest (the one from the old build),
+# and rename it to an unnamed image (keeping the xdpce:<old buld> as well)
+debug "rmi xdpce:latest and grafana-graphite:latest"
+docker rmi -f $XCALAR_IMAGE || true
+docker rmi -f $GRAFANA_IMAGE || true
 debug "load the packed images"
 gzip -dc ${XCALAR_IMAGE}.tar.gz | docker load -q
 gzip -dc ${GRAFANA_IMAGE}.tar.gz | docker load -q
@@ -285,6 +154,11 @@ run_cmd="docker run -d -t --user xcalar --cap-add=ALL --cap-drop=MKNOD \
 --memory-swappiness=10 -e IN_DOCKER=1 \
 -e XLRDIR=/opt/xcalar -e container=docker \
 -v /var/run/docker.sock:/var/run/docker.sock \
+-v $LOCALXCEHOME:/var/opt/xcalar \
+-v $LOCALLOGDIR:/var/log/xcalar \
+-v $XPEDATA/trial.key:/etc/xcalar/XcalarLic.key \
+-v $XPEDATA/xem.cfg:/etc/xcalar/default.cfg \
+-v $XPEIMPORTS:/mnt/imports2 \
 -p $XEM_PORT_NUMBER:15000 \
 --name $XCALAR_CONTAINER_NAME \
 -p 8080:8080 -p 443:443 -p 5000:5000 \
@@ -300,13 +174,12 @@ docker exec -it --user xcalar $XCALAR_IMAGE /opt/xcalar/bin/xcalarctl start
 debug "get admin credentials"
 docker exec -it --user xcalar $XCALAR_IMAGE mkdir -p /var/opt/xcalar/config/ && curl -4 -H "Content-Type: application/json" -X POST -d "{ \"defaultAdminEnabled\": true, \"username\": \"admin\", \"email\": \"admin@xyz.com\", \"password\": \"admin\" }" "http://127.0.0.1:12124/login/defaultAdmin/set"
 
-echo "
+    ## TODO: VALIDATE UPGRADE WAS SUCCESSFUL,
+    ## IF YES, QUERY USER IF THEY WANT TO RETAIN THE BACKUP YOU MADE
+    ##         DISCARD BACKUP IF THEY DON'T
+    ## IF NO, SET BACK TO PREVIOUS STATE
 
-    Xcalar : http://$hostip:8080
-    Grafana: http://$hostip:8082
-
-    start a bash session:
-        docker exec -it $XCALAR_IMAGE bash
-        docker exec -it $GRAFANA_IMAGE bash
-
-" >&2
+# go back to script dir and delete stging dir
+cd "$DIR"
+debug "remove staging dir: $STAGING_DIR"
+rm -r "$STAGING_DIR"
