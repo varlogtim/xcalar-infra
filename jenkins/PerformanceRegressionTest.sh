643e5061fa1b2cb68227b45eac8d07880fb7589d
Brent Lim <blim@xcalar.com>
2018-05-14 17:39:46 -0700

    installerPath ignored for local perf test
    As it is, we're running perf test locally, in which case installerPath
    is ignored anyway.
    
    In future, we can augment this job to run the perf test on a remote
    node. In which case, we need to also specify the --remote flag, so
    additional intelligence will need to be wired into this Jenkins job.
    
    Change-Id: Ia598d5d1ca5d636770ad299065911a1347e815a1

--- jenkins/PerformanceRegressionTest.sh
+++ jenkins/PerformanceRegressionTest.sh
@@ -1,31 +1,17 @@
 #!/bin/bash -x
 
-sudo pkill -9 gdbserver || true
-sudo pkill -9 usrnode || true
-sudo pkill -9 childnode || true
-sudo pkill -9 xcmonitor || true
-sudo pkill -9 xcmgmtd || true
+trap '(xclean; kill $(jobs -p)) || true' SIGINT SIGTERM EXIT
 
 # build
 cmBuild clean
 cmBuild config prod
 cmBuild qa
 
-installerPath="$(readlink -f $INSTALLER_PATH)"
-
-# This extracts the SHA from the installer name
-# xcalar-0.9.10.10-3.2de1db29-installer
-# The stuff before '\K' is cut off and we are left with
-# 2de1db29-installer
-# Then the '(?=)' bit makes it cut off the installer part,
-# so only the SHA is captured in '()', and grep -o makes it return the capture
-sha=`echo $installerPath | awk -F '/' '{print $(NF-2)}' | awk -F '-' '{print $NF}'`
-
 # Copy in the sqlite db file locally from netstore
 # This is because the nfs file locking is flaky(seen that taking in minutes)
 cp $PERFTEST_DB $XLRDIR/
 export ExpServerd=false
-python "$XLRDIR/src/bin/tests/perfTest/runPerf.py" -p "$installerPath" -t "$XLRDIR/src/bin/tests/perfTest/perfTests" -r "$XLRDIR/perf.db" -s "$sha"
+python "$XLRDIR/src/bin/tests/perfTest/runPerf.py" -p "" -t "$XLRDIR/src/bin/tests/perfTest/perfTests" -r "$XLRDIR/perf.db" -s "`git rev-parse HEAD`"
 ret="$?"
 
 [ "$ret" = "0" ] || exit 1
