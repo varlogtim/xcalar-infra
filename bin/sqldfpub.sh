6ebc8d6cf96f424fa9faf1483909b011e287d08f
Eric Cohen <ecohen@xcalar.com>
2018-07-05 23:11:17 -0700

    Add amzn1 repo
    Change-Id: I9a84994275ceb38c18b03f602e244cedeabe685e

--- bin/sqldfpub.sh
+++ bin/sqldfpub.sh
@@ -29,5 +29,8 @@ $XLRDIR/bin/reposync.sh rhel6-repo xcalar-sqldf-0.2-${ITER}.el6.noarch.rpm
 wget http://jenkins.int.xcalar.com/job/BuildSqldf/${ITER}/artifact/build/el7/xcalar-sqldf-0.2-${ITER}.el7.noarch.rpm
 $XLRDIR/bin/reposync.sh rhel7-repo xcalar-sqldf-0.2-${ITER}.el7.noarch.rpm
 
+wget http://jenkins.int.xcalar.com/job/BuildSqldf/${ITER}/artifact/build/amzn1/xcalar-sqldf-0.2-${ITER}.amzn1.noarch.rpm
+$XLRDIR/bin/reposync.sh amzn1-repo xcalar-sqldf-0.2-${ITER}.amzn1.noarch.rpm
+
 wget http://jenkins.int.xcalar.com/job/BuildSqldf/lastSuccessfulBuild/artifact/build/ub14/xcalar-sqldf_0.2-${ITER}_all.deb
 $XLRDIR/bin/apt-includedeb.sh xcalar-sqldf_0.2-${ITER}_all.deb
