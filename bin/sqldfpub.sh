89b063e003cf39ec72acbc448a4d2d21169caedd
Ted Haining <thaining@xcalar.com>
2018-07-13 17:16:03 -0700

    Update sqldf jar file publishing to make the jar file available for user installer builds
    This change extracts the jar file from the tar file produced by the Jenkins
    job and places it on repo.xcalar.net for download by the user installer build
    process.  It also parameterizes the VERSION number of sqldf and makes sure that
    the TMPDIR is properly removed.
    
    Change-Id: I0c927abcb610f3daedfcf091279ea624771fe31f

--- bin/sqldfpub.sh
+++ bin/sqldfpub.sh
@@ -10,11 +10,13 @@ then
 fi
 
 ITER=$1
+VERSION=${2:-"0.2"}
 TMPDIR="/tmp/tmpsqldf_$(date +%s)"
 mkdir "$TMPDIR"
 
 onExit() {
-    rm "$TMPDIR"/*.rpm "$TMPDIR"/*.deb
+    rm -f "$TMPDIR"/*.rpm "$TMPDIR"/*.deb "$TMPDIR"/*.jar
+    rm -rf "$TMPDIR"/tmp "$TMPDIR"/el7
     cd
     rmdir "$TMPDIR"
 }
@@ -23,14 +25,24 @@ cd "$TMPDIR"
 
 trap onExit EXIT
 
-wget http://jenkins.int.xcalar.com/job/BuildSqldf/${ITER}/artifact/build/el6/xcalar-sqldf-0.2-${ITER}.el6.noarch.rpm
-$XLRDIR/bin/reposync.sh rhel6-repo xcalar-sqldf-0.2-${ITER}.el6.noarch.rpm
+wget http://jenkins.int.xcalar.com/job/BuildSqldf/${ITER}/artifact/build/el6/xcalar-sqldf-${VERSION}-${ITER}.el6.noarch.rpm
+$XLRDIR/bin/reposync.sh rhel6-repo xcalar-sqldf-${VERSION}-${ITER}.el6.noarch.rpm
 
-wget http://jenkins.int.xcalar.com/job/BuildSqldf/${ITER}/artifact/build/el7/xcalar-sqldf-0.2-${ITER}.el7.noarch.rpm
-$XLRDIR/bin/reposync.sh rhel7-repo xcalar-sqldf-0.2-${ITER}.el7.noarch.rpm
+wget http://jenkins.int.xcalar.com/job/BuildSqldf/${ITER}/artifact/build/el7/xcalar-sqldf-${VERSION}-${ITER}.el7.noarch.rpm
+$XLRDIR/bin/reposync.sh rhel7-repo xcalar-sqldf-${VERSION}-${ITER}.el7.noarch.rpm
 
-wget http://jenkins.int.xcalar.com/job/BuildSqldf/${ITER}/artifact/build/amzn1/xcalar-sqldf-0.2-${ITER}.amzn1.noarch.rpm
-$XLRDIR/bin/reposync.sh amzn1-repo xcalar-sqldf-0.2-${ITER}.amzn1.noarch.rpm
+wget http://jenkins.int.xcalar.com/job/BuildSqldf/${ITER}/artifact/build/amzn1/xcalar-sqldf-${VERSION}-${ITER}.amzn1.noarch.rpm
+$XLRDIR/bin/reposync.sh amzn1-repo xcalar-sqldf-${VERSION}-${ITER}.amzn1.noarch.rpm
 
-wget http://jenkins.int.xcalar.com/job/BuildSqldf/lastSuccessfulBuild/artifact/build/ub14/xcalar-sqldf_0.2-${ITER}_all.deb
-$XLRDIR/bin/apt-includedeb.sh xcalar-sqldf_0.2-${ITER}_all.deb
+wget http://jenkins.int.xcalar.com/job/BuildSqldf/lastSuccessfulBuild/artifact/build/ub14/xcalar-sqldf_${VERSION}-${ITER}_all.deb
+$XLRDIR/bin/apt-includedeb.sh xcalar-sqldf_${VERSION}-${ITER}_all.deb
+
+tar xvf /netstore/builds/byJob/BuildSqldf/${ITER}/archive.tar el7/xcalar-sqldf-${VERSION}-${ITER}.el7.tar.gz
+! test -f el7/xcalar-sqldf-${VERSION}-${ITER}.el7.tar.gz && \
+    echo "xcalar-sqldf-${VERSION}-${ITER}.el7.tar.gz not found. repo.xcalar.net/deps not updated" && exit 1
+tar xvzf el7/xcalar-sqldf-${VERSION}-${ITER}.el7.tar.gz
+SQLDF_FILE=$(find tmp -name xcalar-sqldf.jar -print | head -1)
+test -z "$SQLDF_FILE" && echo "xcalar-sqldf.jar not found. repo.xcalar.net/deps not updated." && exit 1
+mv ${SQLDF_FILE} ./xcalar-sqldf-${VERSION}.jar
+gsutil cp ./xcalar-sqldf-${VERSION}.jar gs://repo.xcalar.net/deps
+
