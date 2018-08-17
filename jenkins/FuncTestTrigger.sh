ac92892da0529f76898e682bda1d1adaf4f18e98
Brent Lim <blim@xcalar.com>
2018-08-17 15:10:45 -0700

    Allow FuncTestTrigger to run more than 1 iteration
    Change-Id: I8b9288866bed4d0116006d012dddd0de1e768cef

--- jenkins/FuncTestTrigger.sh
+++ jenkins/FuncTestTrigger.sh
@@ -111,71 +111,64 @@ fi
 
 echo $GuardRailsArgs > grargs.txt
 
-if [ "$CURRENT_ITERATION" = "0" ]; then
-    set +e
-    # Kill previous instances of xcalar processes
-    sudo pkill -9 usrnode
-    sudo pkill -9 childnode
-    sudo pkill -9 xcmonitor
-    sudo pkill -9 xcmgmtd
-    sleep 60
+set +e
+# Kill previous instances of xcalar processes
+sudo pkill -9 usrnode
+sudo pkill -9 childnode
+sudo pkill -9 xcmonitor
+sudo pkill -9 xcmgmtd
+sleep 60
 
-    sudo find /var/opt/xcalar -type f -not -path "/var/opt/xcalar/support/*" -delete
-    sudo rm -rf /var/opt/xcalar/kvs/
-    sudo find . -name "core.childnode.*" -type f -delete
-    set -e
+sudo find /var/opt/xcalar -type f -not -path "/var/opt/xcalar/support/*" -delete
+sudo rm -rf /var/opt/xcalar/kvs/
+sudo find . -name "core.childnode.*" -type f -delete
+set -e
 
-    sudo yum -y remove xcalar
+sudo yum -y remove xcalar
 
-    sudo $INSTALLER_PATH --noStart
+sudo $INSTALLER_PATH --noStart
 
-    sudo rm $XCE_CONFIG
-    sudo -E $XLRDIR/scripts/genConfig.sh /etc/xcalar/template.cfg $XCE_CONFIG `hostname`
-    echo "$FuncParams" | sudo tee -a $XCE_CONFIG
+sudo rm $XCE_CONFIG
+sudo -E $XLRDIR/scripts/genConfig.sh /etc/xcalar/template.cfg $XCE_CONFIG `hostname`
+echo "$FuncParams" | sudo tee -a $XCE_CONFIG
 
-    # Enable XEM
-    #echo "Constants.XcalarEnterpriseManagerEnabled=true" | sudo tee -a $XCE_CONFIG
-    #echo "Constants.XcalarClusterName=`hostname`" | sudo tee -a $XCE_CONFIG
-    #echo "XcalarEnterpriseManager.Host=xem-202-1.int.xcalar.com" | sudo tee -a $XCE_CONFIG
-    #echo "XcalarEnterpriseManager.Port=15000" | sudo tee -a $XCE_CONFIG
-    #echo "XcalarEnterpriseManager.IsVirtualXceCluster=true" | sudo tee -a $XCE_CONFIG
+# Enable XEM
+#echo "Constants.XcalarEnterpriseManagerEnabled=true" | sudo tee -a $XCE_CONFIG
+#echo "Constants.XcalarClusterName=`hostname`" | sudo tee -a $XCE_CONFIG
+#echo "XcalarEnterpriseManager.Host=xem-202-1.int.xcalar.com" | sudo tee -a $XCE_CONFIG
+#echo "XcalarEnterpriseManager.Port=15000" | sudo tee -a $XCE_CONFIG
+#echo "XcalarEnterpriseManager.IsVirtualXceCluster=true" | sudo tee -a $XCE_CONFIG
 
-    # Enable NoChildLDPreload so that childnodes do not inherit LD_PRELOAD from usrnode
-    echo "Constants.NoChildLDPreload=true" | sudo tee -a $XCE_CONFIG
+# Enable NoChildLDPreload so that childnodes do not inherit LD_PRELOAD from usrnode
+echo "Constants.NoChildLDPreload=true" | sudo tee -a $XCE_CONFIG
 
-    sudo sed --in-place '/\dev\/shm/d' /etc/fstab
-    tmpFsSizeGb=`cat /proc/meminfo | grep MemTotal | awk '{ printf "%.0f\n", $2/1024/1024 }'`
-    let "tmpFsSizeGb = $tmpFsSizeGb * 95 / 100"
+sudo sed --in-place '/\dev\/shm/d' /etc/fstab
+tmpFsSizeGb=`cat /proc/meminfo | grep MemTotal | awk '{ printf "%.0f\n", $2/1024/1024 }'`
+let "tmpFsSizeGb = $tmpFsSizeGb * 95 / 100"
 
-    echo "none  /dev/shm    tmpfs   defaults,size=${tmpFsSizeGb%.*}G    0   0" | sudo tee -a /etc/fstab
-    sudo mount -o remount /dev/shm
+echo "none  /dev/shm    tmpfs   defaults,size=${tmpFsSizeGb%.*}G    0   0" | sudo tee -a /etc/fstab
+sudo mount -o remount /dev/shm
 
-    # Increase the mmap map count for GuardRails to work
-    echo 100000000 | sudo tee /proc/sys/vm/max_map_count
+# Increase the mmap map count for GuardRails to work
+echo 100000000 | sudo tee /proc/sys/vm/max_map_count
 
-    # Build GuardRails
-    make -C xcalar-infra/GuardRails clean
-    make -C xcalar-infra/GuardRails deps
-    make -C xcalar-infra/GuardRails
+# Build GuardRails
+make -C xcalar-infra/GuardRails clean
+make -C xcalar-infra/GuardRails deps
+make -C xcalar-infra/GuardRails
 
-    # kill any previous test run instances
-    for pid in $(pidof -x FuncTestTrigger.sh); do
-        if [ $pid != $$ ]; then
-            kill -9 $pid
-        fi
-    done
-
-    restartXcalar || true
-
-    if xccli -c version 2>&1 | grep -q 'Error'; then
-        echo "Could not even start usrnodes after install"
-        exit 1
+# kill any previous test run instances
+for pid in $(pidof -x FuncTestTrigger.sh); do
+    if [ $pid != $$ ]; then
+        kill -9 $pid
     fi
-else
-    if xccli -c version 2>&1 | grep -q 'Error'; then
-        echo "Could not contact usrnodes. Did it crash?"
-        exit 1
-    fi
+done
+
+restartXcalar || true
+
+if xccli -c version 2>&1 | grep -q 'Error'; then
+    echo "Could not even start usrnodes after install"
+    exit 1
 fi
 
 TMPDIR="${TMPDIR:-/tmp/`id -un`}/$JOB_NAME/functests"
@@ -190,95 +183,108 @@ NumTests="${#TestsToRun[@]}"
 ii=1
 hostname=`hostname -f`
 
-echo "prod.functests.$TEST_TYPE.${hostname//./_}.currentIter:$CURRENT_ITERATION|g" | nc -4 -w 5 -u $GRAPHITE 8125
-echo "prod.functests.$TEST_TYPE.${hostname//./_}.numberOfIters:$NUMBER_ITERATIONS|g" | nc -4 -w 5 -u $GRAPHITE 8125
+echo "prod.functests.$TEST_TYPE.${hostname//./_}.numberOfIters:$NUM_ITERATIONS|g" | nc -4 -w 5 -u $GRAPHITE 8125
 
 NumNodes=$(awk -F= '/^Node.NumNodes/{print $2}' $XCE_CONFIG)
 
-echo "1..$NumTests" | tee "$TAP"
+echo "1..$(( $NumTests * $NUM_ITERATIONS ))" | tee "$TAP"
 set +e
-for Test in "${TestsToRun[@]}"; do
-    anyfailed=0
-    logfile="$TMPDIR/${hostname//./_}_${Test//::/_}.log"
+for jj in `seq 1 $NUM_ITERATIONS`; do
+    echo "Iteration $jj"
+    echo "prod.functests.$TEST_TYPE.${hostname//./_}.currentIter:$jj|g" | nc -4 -w 5 -u $GRAPHITE 8125
 
-    echo Running $Test on $hostname ...
-    if xccli -c version 2>&1 | grep -q 'Error'; then
-       genSupport
-       restartXcalar || true
-       if xccli -c version 2>&1 | grep -q 'Error'; then
-            echo "Could not restart usrnodes after previous crash"
-            exit 1
-       fi
-    fi
+    for Test in "${TestsToRun[@]}"; do
+        anyfailed=0
+        logfile="$TMPDIR/${hostname//./_}_${Test//::/_}.log"
 
-    if xccli -c "loglevelset Debug" 2>&1 | grep -q 'Error'; then
-       genSupport
-       restartXcalar || true
-       if xccli -c version 2>&1 | grep -q 'Error'; then
-            echo "Could not restart usrnodes after previous crash"
-            exit 1
-       fi
-    fi
+        echo Running $Test on $hostname ...
+        if xccli -c version 2>&1 | grep -q 'Error'; then
+           genSupport
+           restartXcalar || true
+           if xccli -c version 2>&1 | grep -q 'Error'; then
+                echo "Could not restart usrnodes after previous crash"
+                exit 1
+           fi
+        fi
 
-    time xccli -c "functests run --allNodes --testCase $Test" 2>&1 | tee "$logfile"
+        if xccli -c "loglevelset Debug" 2>&1 | grep -q 'Error'; then
+           genSupport
+           restartXcalar || true
+           if xccli -c version 2>&1 | grep -q 'Error'; then
+                echo "Could not restart usrnodes after previous crash"
+                exit 1
+           fi
+        fi
 
-    rc=${PIPESTATUS[0]}
-    if [ $rc -ne 0 ]; then
-        anyfailed=1
-    else
-        if grep -q Error "$logfile"; then
-            echo "Failed test output in $logfile at `date`"
-            cat >&2 "$logfile"
+        time xccli -c "functests run --allNodes --testCase $Test" 2>&1 | tee "$logfile"
+
+        rc=${PIPESTATUS[0]}
+        if [ $rc -ne 0 ]; then
             anyfailed=1
+        else
+            if grep -q Error "$logfile"; then
+                echo "Failed test output in $logfile at `date`"
+                cat >&2 "$logfile"
+                anyfailed=1
+            fi
         fi
-    fi
 
-    # Print and check the stats from all the nodes for any xdb page leaks
-    for nodeid in $(seq 0 $(( $NumNodes - 1 ))); do
-        count=`xccli -c "stats $nodeid" | tee /dev/stderr | grep -e "xdb.pagekvbuf.bc    fastAllocs" -e "xdb.pagekvbuf.bc    fastFrees" | awk '{print $3}' | uniq  | wc -l`
-        # count = 2 when fastAllocs and fastFrees differ
-        # count = 1 when fastAllocs and fastFrees are same
-        # count = 0 when xccli command fails(happens when usrnode crashed/sick)
-        if [ $count -gt 1 ]; then
-            # fastAllocs and fastFrees are different which means that there is a leak
-            echo "Failed pagekvbuf leak test"
-            # abort the usrnode to get a core file.
-            # XXX: Commenting this out out for now to reduce noise.
-            # pgrep -f "usrnode --nodeId $nodeid" | xargs -r sudo kill -6
-            # anyfailed=1
+        # Print and check the stats from all the nodes for any xdb page leaks
+        for nodeid in $(seq 0 $(( $NumNodes - 1 ))); do
+            count=`xccli -c "stats $nodeid" | tee /dev/stderr | grep -e "xdb.pagekvbuf.bc    fastAllocs" -e "xdb.pagekvbuf.bc    fastFrees" | awk '{print $3}' | uniq  | wc -l`
+            # count = 2 when fastAllocs and fastFrees differ
+            # count = 1 when fastAllocs and fastFrees are same
+            # count = 0 when xccli command fails(happens when usrnode crashed/sick)
+            if [ $count -gt 1 ]; then
+                # fastAllocs and fastFrees are different which means that there is a leak
+                echo "Failed pagekvbuf leak test"
+                # abort the usrnode to get a core file.
+                # XXX: Commenting this out out for now to reduce noise.
+                # pgrep -f "usrnode --nodeId $nodeid" | xargs -r sudo kill -6
+                # anyfailed=1
+            fi
+        done
+
+        if [ $anyfailed -eq 1 ]; then
+            # copy out the usrnode binary and retinas
+            now=$(date +"%T")
+            filepath="`pwd`/usrnode.$now"
+            retinapath="`pwd`/retina.$now"
+            exportpath="`pwd`/export.$now"
+            sudo cp /opt/xcalar/bin/usrnode "$filepath"
+            sudo cp -r /var/opt/xcalar/dataflows/ "$retinapath"
+            sudo cp -r /var/opt/xcalar/export/ "$exportpath"
+            # mark the test as failed
+            funcstatsd "$Test" "FAIL" "$gitsha"
+            echo "not ok ${ii} - $Test" | tee -a $TAP
+        else
+            echo "Passed test at `date`"
+            funcstatsd "$Test" "PASS" "$gitsha"
+            echo "ok ${ii} - $Test"  | tee -a $TAP
         fi
+
+        ii=$(( $ii + 1 ))
     done
-
-    if [ $anyfailed -eq 1 ]; then
-        # copy out the usrnode binary and retinas
-        now=$(date +"%T")
-        filepath="`pwd`/usrnode.$now"
-        retinapath="`pwd`/retina.$now"
-        exportpath="`pwd`/export.$now"
-        sudo cp /opt/xcalar/bin/usrnode "$filepath"
-        sudo cp -r /var/opt/xcalar/dataflows/ "$retinapath"
-        sudo cp -r /var/opt/xcalar/export/ "$exportpath"
-        # mark the test as failed
-        funcstatsd "$Test" "FAIL" "$gitsha"
-        echo "not ok ${ii} - $Test" | tee -a $TAP
-    else
-        echo "Passed test at `date`"
-        funcstatsd "$Test" "PASS" "$gitsha"
-        echo "ok ${ii} - $Test"  | tee -a $TAP
-    fi
-
-    ii=$(( $ii + 1 ))
 done
 
-if [ "$(($CURRENT_ITERATION + 1))" = "$NUMBER_ITERATIONS" ]; then
-    # Kill previous instances of xcalar processes
-    # not using xcalarctl here as that will create a support
-    # bundle with the core files and that is inconvenient to
-    # unzip/untar to look at large core files that the tests
-    # generate with miniDump off
+# Shutdown the cluster
+time xccli -c "shutdown" 2>&1 | tee "$logfile"
+
+# Wait for cluster to shutdown
+shutdownSuccessful="false"
+for ii in `seq 1 60`; do
+    pgrep usrnode
+    ret=$?
+    if [ "$ret" != "0" ]; then
+        shutdownSuccessful="true"
+        break
+    fi
+    sleep 1
+done
+
+if [ "$shutdownSuccessful" = "false" ]; then
     sudo pkill -9 usrnode
     sudo pkill -9 childnode
     sudo pkill -9 xcmonitor
     sudo pkill -9 xcmgmtd
-    sleep 60
 fi
