f88cd4f570f47399c882a3a141305601408e9016
Jolie Olsen <jolsen@xcalar.com>
2018-09-05 16:19:05 -0700

    Xcalar Design EE: Following up build process.
    Xcalar Design EE build process goes like this:
    
    Jenkins job 'XcalarPersonalEditionBuilder' triggers shell script:
    <infra>/docker/xpe/jenkins/XcalarPersonalEditionBuilder.sh
    
    This script does the following:
    (1) calls Makefile in <infra>/docker/xdpce to create/save image of a Docker
    	container with Xcalar installed (using installer specified as paramter to the Jenkins job.)
        optionally creates/saves grafana image.
    (2) calls bash script <infra>/docker/xpe/scripts/makeapp.sh which creates the
        actual app (using artefacts from (1) and other repos)
    (3) tars this all up and stores on netstore build dir for consumption.
    
    Previously the build was relying on some hacks to deal with xcalar-gui code that
      was not checked in.  Additionally, it copied dependencies in to the app
      for that gui code, some of which are no longer required.
      Now that gui code is checked in/"finalized", the build process needed to
     be revisited, to remove both the hacks as well as the dependencies which are
     no longer required.
     While doing this I just wanted to modularize/revisit the structure of the code,
     which was largely pieced together as the process evolved, so it'll be easier for
     someone else to read in future.
    
    Change-Id: I82180df4741e79e666c11ebd44ac7288eb9b997a

--- docker/xpe/jenkins/XcalarPersonalEditionRunner.sh
+++ docker/xpe/jenkins/XcalarPersonalEditionRunner.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
+# to be called by Jenkins job 'XcalarPersonalEditionBuilder'
+
 set -e
 
 : "${BUILD_DIRECTORY:?Need to set non-empty env var BUILD_DIRECTORY}"
 : "${BUILD_NUMBER:?Need to set non-empty env var BUILD_NUMBER}"
 : "${PATH_TO_XCALAR_INSTALLER:?Need to set netstore rpm installer path as PATH_TO_XCALAR_INSTALLER)}"
 
+SCRIPTSTART=$(pwd)
 SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 export XLRINFRADIR="${XLRNFRADIR:-$(readlink -f $SCRIPTDIR/../../..)}"
 export XLRGUIDIR="${XLRGUIDIR:-$XLRINFRADIR/../xcalar-gui}"
@@ -13,13 +16,23 @@ export GRAFANADIR="${GRAFANADIR:-$XLRINFRADIR/../graphite-grafana}"
 export CADDY_PORT="${CADDYPORT:-443}"
 export BUILD_GRAFANA="${BUILD_GRAFANA:-true}"
 export DEV_BUILD="${DEV_BUILD:-true}"
-export OFFICIAL_RELEASE="${OFFICIAL_RELEASE:-false}" # if true will tag the Docker images with official Xcalar build Strings
+export OFFICIAL_RELEASE="${OFFICIAL_RELEASE:-false}" # if true will tag Docker images with official Xcalar build Strings
 export XCALAR_IMAGE_NAME="${XCALAR_IMAGE_NAME:-xcalar_design}"
 export XCALAR_CONTAINER_NAME="${XCALAR_CONTAINER_NAME:-xcalar_design}"
 
-BASH_HELPER_FUNCS="$SCRIPTDIR/../scripts/local_installer_mac.sh"
+INFRA_XPE_DIR="$XLRINFRADIR/docker/xpe"
+BASH_HELPER_FUNCS="$INFRA_XPE_DIR/scripts/local_installer_mac.sh"
+XDEE_BUILD_TARGET_DIR="xcalar-design-ee" # dirname of the build target of the xcalar-gui project this app will require
+XDEE_GUI_BUILD_DIR="${XDEE_GUI_BUILD_DIR:-"$XLRGUIDIR/$XDEE_BUILD_TARGET_DIR"}" # custom gui build for xdee; will build in XLRGUIDIR if this dir doesn't exist
 
-XPEDIR="$XLRINFRADIR/docker/xpe"
+APPBASENAME="${APPBASENAME:-"Xcalar Design"}" # basename for the app
+APPTARFILE="${APPTARFILE:-"$APPBASENAME.tar.gz"}" # name of final tarred app that ends up in bld
+INSTALLERTARFILE=installertarball.tar.gz # name for tarball containing docker images, dependencies, etc.
+
+### CREATE STAGING DIR TO DO BUILDING IN ###
+STAGING_DIR="$(mktemp -d --tmpdir xpeBldStagingXXXXXX)"
+
+###  CLEAENUP/HELPER FUNCTIONS ###
 
 # removes xcalar and grafana Docker artefacts. to run at job start and cleanup.
 # clear xcalar repo entirely; don't want to cache
@@ -44,13 +57,142 @@ cleanup() {
     # if you are running through Jenkins - if the job fails or completes, this entire function will run
     # but if aborted, there are only a couple seconds before shell is terminated
     # therefore prioritize cleaning up objects which could interfere w future runs or other jobs using this machine
+    echo "cleanup"
+    cd "$SCRIPTSTART" # go back before removing staging dir in case you're in staging dir
+    rm -r "$STAGING_DIR"
     removeBuildArtefacts
     clearDockerArtefacts
+}
+
+# builds the xcalar-gui project in the state required by the app to be in the Docker container
+# (builds from XLRGUIDIR if build does not exist)
+# (RPM installers will install xcalar-gui build with standard build targets; for xcalar-gui
+# to work in the app needs to be built with --product=XDEE option)
+buildXcalarGuiForApp() {
+    if [ ! -d "$XDEE_GUI_BUILD_DIR" ]; then
+        echo ">>> gui build dir does NOT exist; building from $XLRGUIDIR" >&2
+        cd "$XLRGUIDIR"
+        git submodule update --init
+        git submodule update
+        npm install --save-dev
+        node_modules/grunt/bin/grunt init
+        node_modules/grunt/bin/grunt dev --product=XDEE
+        # make sure expected target exists
+        if [ ! -d "$XDEE_BUILD_TARGET_DIR" ]; then
+            echo "Gui build target $XDEE_BUILD_TARGET_DIR does not exist in $XLRGUIDIR after building (has target name changed?)" >&2
+            exit 1
+        else
+            cd "$XDEE_BUILD_TARGET_DIR"
+            XDEE_GUI_BUILD_DIR=$(pwd)
+        fi
+    fi
+
+    # build the xpe server
+    cd "$XDEE_GUI_BUILD_DIR/services/xpeServer"
+    npm install
+}
+
+# create installer tarball to be packaged in the app (tarball with
+# the files needed during app install-time on host) by generating all the
+# required assets including the saved Docker images.
+# saves the tarball in the staging dir.
+generate_app_assets()  {
+
+    cd "$STAGING_DIR"
+
+    echo "Create installer tarball for app " >&2
+
+    # create dir to get tarrred at end of this function,
+    # and included in the installer assets for the app.
+    # it should include all files required for setting up the container
+    # on the host, including the Docker images
+    local tarDir="tarfiles"
+    mkdir -p "$tarDir"
+    # get full path so know where to copy files in to
+    cd "$tarDir"
+    local tarDirPath=$(pwd)
+    cd "$STAGING_DIR"
+
+    # build the grafana-graphite container if requested.
+    # (BUILD_GRAFANA is a boolean arg in the Jenkins job)
+    if [ "$BUILD_GRAFANA" = true ]; then
+        cd $GRAFANADIR
+        make grafanatar
+        # it will have saved an image of the grafana container
+        # add saved image to dir for installer tarball
+        cp grafana_graphite.tar.gz "$tarDirPath"
+    fi
+
+    # build custom GUI to be consumed by Makefile for building the Xcalar Docker container
+    # (Makefile provides option to swap out what gets installed by rpm installer)
+    # (this is temporary until RPM installers are generated with the GUI needed)
+    buildXcalarGuiForApp
+
+    # build xdpce container
+    # (do not actually need any port exposed since just need the image, so do not expose any port when blding
+    # just to reduce chances of port conflicts on this machine if the container gets left over somehow)
+    cd "$XLRINFRADIR/docker/xdpce"
+    make docker-image INSTALLER_PATH="$PATH_TO_XCALAR_INSTALLER" CONTAINER_NAME="$XCALAR_CONTAINER_NAME" CONTAINER_IMAGE="$XCALAR_IMAGE_NAME" PORT_MAPPING="" CUSTOM_GUI="$XDEE_GUI_BUILD_DIR"
+
+    # make will save an image of the container (xdpce.tar.gz),
+    # and also copies of dirs saved in 'xcalar home'.
+    # these dirs are needed on the host at app install time, if you map local
+    # volumes to them (as this mapping will overwrite what's saved in the
+    # image, so need these as defaults for initial installs)
+    # - move these dirs to be included in the installer tarball
+    mv .ipython "$tarDirPath"
+    mv .jupyter "$tarDirPath"
+    mv jupyterNotebooks "$tarDirPath"
+    mv xdpce.tar.gz "$tarDirPath"
+    # make also saves a copy of the xcalar-gui dir that got installed in the
+    # Docker container; move to staging dir to be consumed by makeapp.sh
+    # (this should NOT be part of installer tarball; its just a byproduct of
+    # generating the installer tarball assets so dealing with it here)
+    mv xcalar-gui "$STAGING_DIR"
+
+    # copy in defaultAdmin from the infra repo, for installer tarball
+    cp "$XLRINFRADIR/docker/xdpce/defaultAdmin.json" "$tarDirPath"
+
+    # set caddy port as a text file, so host side will know which Caddyport to use
+    echo "$CADDY_PORT" > "$tarDirPath/.caddyport"
+
+    # download sample datasets for the installer tarball
+    # (they will be saved locally on the host installing the app,
+    # and mapped in to the container created on that host.)
+    cd "$tarDirPath"
+    curl -f -L http://repo.xcalar.net/deps/sampleDatasets.tar.gz -O
+
+    # create the installer tarball with the assets gathered
+    cd "$STAGING_DIR"
+    tar -czf "$INSTALLERTARFILE" -C "$tarDirPath" .
+}
+
+# generates the .app, tars it, and put that tarfile in final build dest
+build_app() {
+    # generate installer tarball and other assets needed for app
+    generate_app_assets
+
+    echo "making mac app..." >&2
+    # create the app; supply gui that was copied out from the container during Makefile
+    APPOUT="$STAGING_DIR/$APPBASENAME.app" GUIBUILD="$STAGING_DIR/xcalar-gui" INSTALLERTARBALL="$STAGING_DIR/$INSTALLERTARFILE" bash -x "$INFRA_XPE_DIR/scripts/makeapp.sh"
+
+    # tar the app
+    # (will need to be downloaded from Mac, but nwjs binaries
+    # are not world readbale)
+    tar -czf "$APPTARFILE" "$APPBASENAME.app"
+
+    # copy to build directory
+    # (building app in staging dir instead of directly in to build because most likely the
+    # build dir is remotely on netstore, want all work to be done exclusively on the
+    # jenkins slave then copy it in only when everything complete)
+    cp -r "$APPTARFILE" "$FINALDEST"
 }
 
 trap cleanup EXIT SIGTERM SIGINT # Jenkins sends SIGTERM on abort
 
 ### START JOB ###
+
+cd "$STAGING_DIR"
 
 clearDockerArtefacts
 
@@ -66,73 +208,12 @@ DEV_BUILD=$DEV_BUILD
 BUILD_GRAFANA=$BUILD_GRAFANA
 EOF
 
-# contents of tarfile that will be packaged with the app and used by the installer
-# (all additional files required for an install on the host)
-# script generates these during build process; everythign needs to end up in FINALDEST
-TARCONTENTS="xdpce.tar.gz defaultAdmin.json .ipython/ .jupyter/ jupyterNotebooks/ sampleDatasets.tar.gz .caddyport"
-
-# build the grafana-graphite container if requested.
-# (BUILD_GRAFANA is a boolean arg in the Jenkins job)
-if [ "$BUILD_GRAFANA" = true ]; then
-    cd $GRAFANADIR
-    make grafanatar
-    # it will have saved an image of the grafana container
-    cp grafana_graphite.tar.gz $FINALDEST
-    TARCONTENTS="$TARCONTENTS grafana_graphite.tar.gz"
-fi
-
-# build xdpce container
-# (do not actually need any port exposed since just need the image, so do not expose any port when blding
-# just to reduce chances of port conflicts on this machine if the container gets left over somehow)
-cd "$XLRINFRADIR/docker/xdpce"
-make docker-image INSTALLER_PATH="$PATH_TO_XCALAR_INSTALLER" CONTAINER_NAME="$XCALAR_CONTAINER_NAME" CONTAINER_IMAGE="$XCALAR_IMAGE_NAME" PORT_MAPPING=""
-
-# running make will save an image of the container (xdpce.tar.gz)
-# along with these dirs which get generated install time in xcalar home,
-# that you will need if you map local volumes there (as it will overwrite
-# what is currently there)
-cp -R .ipython $FINALDEST
-cp -R .jupyter $FINALDEST
-cp -R jupyterNotebooks $FINALDEST
-cp defaultAdmin.json $FINALDEST
-# makefile copies the xcalar-gui dir that was installed in to the Docker container
-# by the rpm installer, out of the Docker container.
-# TODO: Once all the gui code is checked in to the xcalar-gui project,
-# use this xcalar-gui dir as the one being included in the app
-# until then, having to build own xcalar-gui from private branch.
-#cp "$XPEDIR/staticfiles/config.js" "xcalar-gui/assets/js/" <-- add in the needed files here?
-#cp -R xcalar-gui/ $FINALDEST <-- @TODO: will cp in to $FINALDEST so makeapp.sh can use
-cp xdpce.tar.gz $FINALDEST
-
-# set caddy port as a text file, so host side will know which Caddyport to use
-echo "$CADDY_PORT" > "$FINALDEST/.caddyport"
-
-# tar what's needed for the local install
-cd $FINALDEST
-curl -f -L http://repo.xcalar.net/deps/sampleDatasets.tar.gz -O
-TARFILE=installertarball.tar.gz
-tar -czf "$TARFILE" $TARCONTENTS
-
-# run mkshar (ubuntu)
-# (taking this out for now to speed up bld process)
-# @TODO: Add options in Jenkins job for build mac, ubuntu, etc.
-# and do these based on that.
-#ubuntuinstaller=local_installer.sh
-#cp "$XPEDIR/scripts/$ubuntuinstaller" .
-#"$XLRINFRADIR/bin/mkshar.sh" "$TARFILE" "$ubuntuinstaller" > xpe_installer_ubuntu.sh
-#chmod u+x xpe_installer_ubuntu.sh
-
-# make the mac app (mac)
-echo "making mac app..." >&2
-bash -x "$XPEDIR/scripts/makeapp.sh"
-
-# remove from final dir everything but the final mkshar installers
-# don't do this as general cleanup - want to keep these dirs on build failures
-rm -r $TARCONTENTS
-rm "$TARFILE"
+build_app
 
 # symlink to this bld
 cd "$BUILD_DIRECTORY" && ln -sfn "$BUILD_NUMBER" lastSuccessful
 
+# (staging dir removed in cleanup, which is called on normal exit)
+
 # printing to stdout for other scripts to call
-echo $FINALDEST
+echo "$FINALDEST"
--- docker/xpe/scripts/makeapp.sh
+++ docker/xpe/scripts/makeapp.sh
@@ -1,143 +1,179 @@
 #!/usr/bin/env bash
 
-# Constructs the actual Xcalar Design app directory by piecing together files
-# from the various repos
-#
-# optional arg forces build of xcalar-gui project ($XLRGUIDIR)
+# Constructs the Xcalar Design app
+# Mac app is just directory with specific directory structure; creates that and
+# adds in all required files the app needs to run on the host.
 
 set -e
 
 : "${XLRINFRADIR:?Need to set non-empty XLRINFRADIR}"
-: "${XLRGUIDIR:?Need to set non-empty XLRGUIDIR}"
+: "${GUIBUILD:?Need to set non-empty GUIBUILD (path to built gui to include in app)}"
 : "${BUILD_GRAFANA:?Need to set true or false env var BUILD_GRAFANA}"
 : "${DEV_BUILD:?Need to set true or false env var DEV_BUILD}"
 : "${XCALAR_IMAGE_NAME:?Need to set name of Docker image for .imgid app file, as env var XCALAR_IMAGE_NAME}"
+: "${APPOUT:?Need to set non-empty APPOUT (path to app to generate)}"
+: "${INSTALLERTARBALL:?Need to set non-empty INSTALLERTARBALL (path to installer tarball)}"
 
 startCwd=$(pwd)
+SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-APPBASENAME="Xcalar Design"
-APPNAME="${APPBASENAME}.app"
-DMGNAME="${APPBASENAME}.dmg"
-EXECUTABLENAME="Xcalar Design"
+# make sure app out specified is .app
+if [[ ! $APPOUT == *.app ]]; then
+    echo "APPOUT must end in .app" >&2
+    exit 1
+fi
+# create app; get basename and full path
+if [ -e "$APPOUT" ]; then
+    echo "$APPOUT already exists!" >&2
+    exit 1
+fi
+APPBASENAME=$(basename "$APPOUT" .app)
+mkdir -p "$APPOUT"
+cd "$APPOUT"
+APP_ABS_PATH=$(pwd)
+cd "$startCwd"
 
 XPEINFRAROOT="$XLRINFRADIR/docker/xpe"
 
-# create base app dir at cwd and get its full path
-mkdir -p "$APPNAME"
-cd "$APPNAME"
-APPPATH=$(pwd)
+CONTENTS="$APP_ABS_PATH/Contents"
+MACOSDIR="$CONTENTS/MacOS"
+LOGS="$CONTENTS/Logs"
+RESOURCES="$CONTENTS/Resources"
+BIN="$RESOURCES/Bin"
+APPGUIDIR="$RESOURCES/gui/xcalar-gui"
+SCRIPTS="$RESOURCES/scripts"
+DATA="$RESOURCES/Data"
+INSTALLER="$RESOURCES/Installer"
+DMGBIN="$BIN"
 
-mkdir -p "$APPPATH/Contents/MacOS"
-mkdir -p "$APPPATH/Contents/Resources/Bin"
-mkdir -p "$APPPATH/Contents/Resources/Installer"
-mkdir -p "$APPPATH/Contents/Resources/scripts"
-mkdir -p "$APPPATH/Contents/Resources/Data"
-mkdir -p "$APPPATH/Contents/Logs"
-APPGUIDIR="$APPPATH/Contents/Resources/gui/xcalar-gui"
-mkdir -p "$APPGUIDIR"
+# icon to use for the app (must be a .icns file; see general MacOS app icon guidelines)
+APPICON_PATH="$GUIBUILD/assets/images/appIcons/AppIcon.icns"
 
-# if xcalar-gui not built, build it
-if [ ! -d "$XLRGUIDIR/xcalar-gui" ] || [ "$1" ]; then
-    cd "$XLRGUIDIR"
-#    git submodule update --init
-#    git submodule update
-#    npm install --save-dev
-#    node_modules/grunt/bin/grunt init
-#    node_modules/grunt/bin/grunt dev
-    make dev # once submodule update in Jenkins resolved, remove this in favor of all the other comments lines (see targui.sh)
-    cd "$startCwd"
-fi
+# MacOS apps require a certain structure in the app dir.
+# create that here along with other dirs needed specifically for this app
+create_app_structure() {
+    mkdir -p "$CONTENTS"
+    mkdir -p "$MACOSDIR"
+    mkdir -p "$LOGS"
+    mkdir -p "$RESOURCES"
+    mkdir -p "$BIN"
+    mkdir -p "$APPGUIDIR"
+    mkdir -p "$SCRIPTS"
+    mkdir -p "$DATA"
+    mkdir -p "$INSTALLER"
+}
 
-# app essential metadata
-cp "$XPEINFRAROOT/staticfiles/Info.plist" "$APPPATH/Contents"
+# MacOS apps require certain metadata; add that essential metadata here
+setup_required_app_files() {
+    # app essential metadata
+    cp "$XPEINFRAROOT/staticfiles/Info.plist" "$CONTENTS"
 
-# add xcalar-gui (config.js and package.json for nwjs should already be present)
-tar xzf /netstore/users/jolsen/xcalar-gui.tar.gz -C "$APPPATH/Contents/Resources/gui" ### AMIT:: This a temporary hack for my testing
-#cp -r "$XLRGUIDIR/xcalar-gui/"* "$APPGUIDIR" # use this once git submodule update in Jenkins resolved
+    # add app entrypoint (executable file in this dir will be run when user
+    # double clicks app); must make exeuctable
+    # executable MUST be same name as appbase name (mac requirement)
+    # so check first if this file is in infra, in case we changed the name of the app
+    executablePathInInfra="$XPEINFRAROOT/scripts/$APPBASENAME"
+    if [ ! -f "$executablePathInInfra" ]; then
+        echo "Can't find executable file in infra repo: $executablePathInInfra \
+(The executable file must be the same name as the app, as per MacOS \
+requirements.)" >&2
+        exit 1
+    fi
+    cp "$XPEINFRAROOT/scripts/$APPBASENAME" "$MACOSDIR"
+    chmod 777 "$MACOSDIR/$APPBASENAME"
 
-# build the xpe server
-# put back in once figured out the git submodule update in jenkins issue
-#cd "$APPGUIDIR/services/xpeServer"
-#npm install
-#cd "$startCwd"
+    # set app icon
+    cp "$APPICON_PATH" "$RESOURCES"
+}
 
-# add the nwjs javascript entrypoint to the gui root
-mv "$APPGUIDIR/assets/js/xpe/starter.js" "$APPGUIDIR"
-# nwjs's package.json for xcalar-gui dir
-# config.js for letting xcalar-gui on the host machine communicate in to Docker for Xcalar backend
-cp "$XPEINFRAROOT/staticfiles/package.json" "$APPGUIDIR"
-cp "$XPEINFRAROOT/staticfiles/config.js" "$APPGUIDIR/assets/js/"
+# Copies in GUIs and makes modifications required for app
+setup_app_gui() {
+    cp -r "$GUIBUILD"/* "$APPGUIDIR"
 
-# the built xcalar-gui project, will not include node_modules
-# because everything is intended to be run in browser context, so needed js files
-# are imported via <script> tags in the html
-# however, nwjs is rooting at xcalar-gui, and entrypoint is a js running in node context,
-# which will need to require files in node context, before any GUI runs.
-# therefore, need node_modules/<require module> modules, to get those js files.
-# normally could just add in the package.json, but package.json would need to be
-# shared by both nodejs (for npm install) and nwjs (to open gui);
-# package.json for nodejs does not allow capital letters in name field, but
-# nwjs' package.json needs name field to match app's name (Xcalar Design) else it
-# will generate additional Application Support directories by that name.
-# since right now only need one module - jquery - (for doing Deferreds)
-# just go ahead and npm install it
-# later if more needed, create a separate package.json
-cd "$APPGUIDIR"
-npm install jquery
-cd "$startCwd"
+    # config.js so xcalar-gui on host filesystem will communicatee to Xcalar backend in Docker
+    cp "$XPEINFRAROOT/staticfiles/config.js" "$APPGUIDIR/assets/js/"
+}
 
-# a few missing files
-curl http://repo.xcalar.net/deps/bootstrap.css -o "$APPGUIDIR/3rd/bootstrap.css"
-curl http://repo.xcalar.net/deps/googlefonts.css -o "$APPGUIDIR/3rd/googlefonts.css"
-curl http://netstore/users/jolsen/makeinstaller/xdlogo.png -o "$APPGUIDIR/assets/images/xdlogo.png"
+setup_installer_assets() {
+    # functions called by the xpeServer during install
+    cp "$XPEINFRAROOT/scripts/local_installer_mac.sh" "$INSTALLER"
+    cp "$INSTALLERTARBALL" "$INSTALLER" # the docker images and other files needed by local_installer_mac.sh
+}
 
-# installer assets
-cp "$XPEINFRAROOT/scripts/local_installer_mac.sh" "$APPPATH/Contents/Resources/Installer"
-cp installertarball.tar.gz "$APPPATH/Contents/Resources/Installer" # has files needed by local_installer_mac.sh
+setup_nwjs() {
+    # setup nwjs
+    cd "$BIN"
+    curl http://repo.xcalar.net/deps/nwjs-sdk-v0.29.3-osx-x64.zip -O
+    unzip -aq nwjs-sdk-v0.29.3-osx-x64.zip
+    rm nwjs-sdk-v0.29.3-osx-x64.zip
+    # must change app metadata to get customized nwjs menus to display app name
+    # http://docs.nwjs.io/en/latest/For%20Users/Advanced/Customize%20Menubar/ <- see MacOS section
+    find nwjs-sdk-v0.29.3-osx-x64/nwjs.app/Contents/Resources/*.lproj/InfoPlist.strings -type f -print0 | xargs -0 sed -i 's/CFBundleName\s*=\s*"nwjs"/CFBundleName = "Xcalar Design"/g'
+    # replace nwjs default icon with app icon (hack for now, not getting icon attr to work)
+    # nwjs icon will dispaly on refresh/quit prompts, even when running Xcalar Design app
+    cp "$APPICON_PATH" nwjs-sdk-v0.29.3-osx-x64/nwjs.app/Contents/Resources/app.icns
+    cp "$APPICON_PATH" nwjs-sdk-v0.29.3-osx-x64/nwjs.app/Contents/Resources/document.icns
 
-# setup nwjs
-cd "$APPPATH/Contents/Resources/Bin"
-curl http://repo.xcalar.net/deps/nwjs-sdk-v0.29.3-osx-x64.zip -O
-unzip -aq nwjs-sdk-v0.29.3-osx-x64.zip
-rm nwjs-sdk-v0.29.3-osx-x64.zip
-# must change app metadata to get customized nwjs menus to display app name
-# http://docs.nwjs.io/en/latest/For%20Users/Advanced/Customize%20Menubar/ <- see MacOS section
-find nwjs-sdk-v0.29.3-osx-x64/nwjs.app/Contents/Resources/*.lproj/InfoPlist.strings -type f -print0 | xargs -0 sed -i 's/CFBundleName\s*=\s*"nwjs"/CFBundleName = "Xcalar Design"/g'
-# replace nwjs default icon with app icon (hack for now, not getting icon attr to work)
-# nwjs icon will dispaly on refresh/quit prompts, even when running Xcalar Design app
-cp "$XLRGUIDIR/xcalar-gui/assets/images/appIcons/AppIcon.icns" nwjs-sdk-v0.29.3-osx-x64/nwjs.app/Contents/Resources/app.icns
-cp "$XLRGUIDIR/xcalar-gui/assets/images/appIcons/AppIcon.icns" nwjs-sdk-v0.29.3-osx-x64/nwjs.app/Contents/Resources/document.icns
+    # add nwjs's package.json (uses as config file on nwjs start)
+    cp "$XPEINFRAROOT/staticfiles/package.json" "$APPGUIDIR"
+    # nwjs entrypoint specified by the package.json
+    mv "$APPGUIDIR/assets/js/xpe/starter.js" "$APPGUIDIR"
 
-# nodejs in to Bin directory
-curl http://repo.xcalar.net/deps/node-v8.11.1-darwin-x64.tar.gz | tar zxf -
-cd "$startCwd"
+    # install npm modules required by nwjs' entrypoint
+    # [[- the gui build will not include node_modules dir because everything is
+    # intended to be run in browser context, so needed js files are imported
+    # via <script> tags in the html rather than required.
+    # however, in the app, nwjs will be rooted in the gui build,
+    # and its entrypoint is a js file running in node context which must require
+    # modules in node context prior to any GUI running.
+    # therefore, npm install to get node_modules/<require module> modules for
+    # each such 3rd party module the entrypoint requires.
+    # (can't use package.json for this - it would need to be shared by both
+    # nodejs and nwjs; but package.json for nodejs does not allow capital letters
+    # in name field, while nwjs' package.json needs name field to match app's name
+    #  (Xcalar Design) else it will generate additional Application Support
+    # directories by that name.  So require the modules directly]]
+    cd "$APPGUIDIR"
+    npm install jquery
+}
 
-# file to indicate which img is associated with this installer bundle
-# so host program will know weather to open installer of main app at launch
-# this should have been made by Jenkins job and in cwd
-if ! imgsha=$(docker image inspect "$XCALAR_IMAGE_NAME":lastInstall -f '{{ .Id }}' 2>/dev/null); then
-    echo "No $XCALAR_IMAGE_NAME:lastInstall to get image sha from!!" >&2
-    exit 1
-else
-    echo "$imgsha" > "$APPPATH/Contents/Resources/Data/.imgid"
-fi
+setup_bin() {
+    setup_nwjs
+    # nodejs in to Bin directory
+    # make sure you are curling directly in to bin dir
+    cd "$BIN" # setup_nwjs will change dir
+    curl http://repo.xcalar.net/deps/node-v8.11.1-darwin-x64.tar.gz | tar zxf -
+}
 
-# executable app entrypoint
-cp "$XPEINFRAROOT/scripts/$EXECUTABLENAME" "$APPPATH/Contents/MacOS"
-chmod 777 "$APPPATH/Contents/MacOS/$EXECUTABLENAME"
+# hidden files in the MacOS dir are used on the host at install time, to determine
+# how the install should be done.
+setup_hidden_files() {
+    # file to indicate which img is associated with this installer bundle
+    # so host program will know weather to open installer of main app at launch
+    # this should have been made by Jenkins job and in cwd
+    if ! imgsha=$(docker image inspect "$XCALAR_IMAGE_NAME":lastInstall -f '{{ .Id }}' 2>/dev/null); then
+        echo "No $XCALAR_IMAGE_NAME:lastInstall to get image sha from!!" >&2
+        exit 1
+    else
+        echo "$imgsha" > "$DATA/.imgid"
+    fi
 
-# if supposed to build grafana, add a mark for this for host-side install
-if $BUILD_GRAFANA; then
-    touch "$APPPATH/Contents/MacOS/.grafana"
-fi
-# if a dev build (will expose right click feature in GUIs), add a mark for this for host-side install
-if $DEV_BUILD; then
-    touch "$APPPATH/Contents/MacOS/.dev"
-fi
+    # if supposed to build grafana, add a mark for this for host-side install
+    if $BUILD_GRAFANA; then
+        touch "$MACOSDIR/.grafana"
+    fi
+    # if a dev build (will expose right click feature in GUIs), add a mark for this for host-side install
+    if $DEV_BUILD; then
+        touch "$MACOSDIR/.dev"
+    fi
+}
 
-# set app icon
-cp "$XLRGUIDIR/xcalar-gui/assets/images/appIcons/AppIcon.icns" "$APPPATH/Contents/Resources"
+create_app_structure
+setup_required_app_files
+setup_app_gui # run before setup_bin
+setup_installer_assets
+setup_bin
+setup_hidden_files
 
-# zip app
-cd "$startCwd"
-tar -zcf "$APPNAME.tar.gz" "$APPNAME"
+# echo for other scripts
+echo "$APPOUT"
