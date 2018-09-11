fbf8d729e1ba6b913987c3f68beb569f0f3b4f86
Amit Bakshi <abakshi@xcalar.com>
2018-09-11 11:02:20 -0700

    KPMG Fixes
    - EFS Mount point was not formatted correct
    - Consistent naming between parameters/args
    - Add CreationPolicy to fail the template if anything goes wrong
    - Open up port 8000/udp
    -
    
    Change-Id: I8d7341a1d5d6f911e569344cf70d6721b15c980f

--- aws/templates/scripts/user-data.sh
+++ aws/templates/scripts/user-data.sh
@@ -12,18 +12,20 @@ else
   exec > >(tee -a $LOGFILE |logger -t user-data -s 2>/dev/console) 2>&1
 fi
 
+eval $(ec2-tags -s -i)
+
 while [ $# -gt 0 ]; do
     cmd="$1"
     shift
     case "$cmd" in
-        --nfs-mount-target) EFSMOUNT="$1"; shift;;
+        --nfs-mount) NFSMOUNT="$1"; shift;;
         --tag-key) TAG_KEY="$1"; shift;;
         --tag-value) TAG_VALUE="$1"; shift;;
         --s3bucket) S3BUCKET="$1"; shift;;
         --s3prefix) S3PREFIX="$1"; shift;;
         --bootstrap-expect) BOOTSTRAP_EXPECT="$1"; shift;;
         --license) test -z "$1" || LICENSE="$1"; shift;;
-        --installerurl) test -z "$1" || INSTALLERURL="$1"; shift;;
+        --installer-url) test -z "$1" || INSTALLER_URL="$1"; shift;;
         --stack-name) STACK_NAME="$1"; shift;;
         --resource-id) RESOURCE_ID="$1"; shift;;
         *) echo >&2 "WARNING: Unknown command $cmd";;
@@ -47,35 +49,34 @@ INSTANCE_TYPE=$(curl -sSf http://169.254.169.254/latest/meta-data/instance-type)
 export AWS_DEFAULT_REGION="${AVZONE%[a-f]}"
 
 export PATH=/opt/mssql-tools/bin:/opt/xcalar/bin:/usr/local/bin:/usr/local/sbin:/usr/bin:/usr/sbin:/sbin:/bin:/opt/aws/bin
+echo "export PATH=$PATH" > /etc/profile.d/path.sh
 
 set +e
 if [ -e /etc/ec2.env ]; then
     . /etc/ec2.env
 fi
 
-eval $(ec2-tags -s -i)
 
 case "$AWS_DEFAULT_REGION" in
-    us-west-2)
-        test -n "$EFSMOUNT" || EFSMOUNT="netstore.${AWS_DEFAULT_REGION}.aws.xcalar.com"
-        ;;
-    us-east-1)
-        test -n "$EFSMOUNT" || EFSMOUNT="netstore.${AWS_DEFAULT_REGION}.aws.xcalar.com"
+    us-east-1|us-west-2)
+        test -n "$NFSMOUNT" || NFSMOUNT="netstore.${AWS_DEFAULT_REGION}.aws.xcalar.com:/"
         ;;
     *)
         echo >&2 "Region ${AWS_DEFAULT_REGION} is not supported properly!"
-        test -n "$EFSMOUNT" || EFSMOUNT="netstore.${AWS_DEFAULT_REGION}.aws.xcalar.com"
+        test -n "$NFSMOUNT" || NFSMOUNT="netstore.${AWS_DEFAULT_REGION}.aws.xcalar.com:/"
         ;;
 esac
+
+yum clean all --enablerepo='*'
+yum install -y unzip yum-utils epel-release patch
+yum install -y http://repo.xcalar.net/xcalar-release-${OSID}.rpm
+yum-config-manager --enable xcalar-deps xcalar-deps-common epel
+yum install -y jq amazon-efs-utils
 
 if ! rpm -q xcalar; then
     mkdir -p -m 0700 /var/lib/xcalar-installer
     cd /var/lib/xcalar-installer
 
-    yum install -y unzip yum-utils epel-release patch
-    yum install -y http://repo.xcalar.net/xcalar-release-${OSID}.rpm
-
-    yum-config-manager --enable xcalar-deps xcalar-deps-common epel
     yum install -y  xcalar-sqldf ## Workaround a packaging bug
 
     curl -L "https://s3.amazonaws.com/aws-cli/awscli-bundle.zip" -o awscli-bundle.zip && unzip awscli-bundle.zip && ./awscli-bundle/install -i /opt/aws -b /usr/local/bin/aws
@@ -90,22 +91,22 @@ if ! rpm -q xcalar; then
 
     sed -i -r 's/^(G|U)ID_MIN.*$/\1ID_MIN            1000/g' /etc/login.defs
 
-    if ! test -s installer.sh; then
-        if [[ "$INSTALLERURL" =~ ^http ]]; then
-            curl -fL "$INSTALLERURL" -o installer.sh
-        elif [[ "$INSTALLERURL" =~ s3:// ]]; then
-            aws s3 cp "$INSTALLERURL" installer.sh
-        fi
+    if test -e installer.sh; then
+        mv installer.sh installer.sh.$$
     fi
-    if test -s installer.sh; then
-        bash -x installer.sh --nostart --startonboot 2>&1 | tee installer.log
+    if [[ "$INSTALLER_URL" =~ ^http ]]; then
+        curl -fL "$INSTALLER_URL" -o installer.sh
+    elif [[ "$INSTALLER_URL" =~ s3:// ]]; then
+        aws s3 cp "$INSTALLER_URL" installer.sh
+    fi
+    if [ $? -eq 0 ] && test -s installer.sh; then
+        export ACCEPT_EULA=Y
+        bash -x installer.sh --nostart --startonboot 2>&1 | tee -a installer.log
         if [ ${PIPESTATUS[0]} -ne 0 ]; then
-            echo "Failed to install ${PWD}/installer.sh (from $INSTALLERURL)"
+            echo "Failed to install ${PWD}/installer.sh (from $INSTALLER_URL)"
             exit 1
         fi
-        touch /var/lib/xcalar-installer/finished
     fi
-
 fi
 
 if [ -n "$LICENSE" ]; then
@@ -116,8 +117,8 @@ touch /etc/xcalar/XcalarLic.key
 chown xcalar:xcalar /etc/xcalar/XcalarLic.key
 
 mkdir -p /netstore
-if [ -n "$EFSMOUNT" ]; then
-    echo "${EFSMOUNT}:/ /netstore nfs4 nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2	0	0" >>/etc/fstab
+if [ -n "$NFSMOUNT" ]; then
+    echo "${NFSMOUNT} /netstore efs defaults,_netdev 0	0" >>/etc/fstab
     mount /netstore
 fi
 
@@ -154,9 +155,12 @@ if [ $NUM_INSTANCES -gt 1 ]; then
     XLRROOT=/mnt/xcalar
     mkdir -p /netstore/cluster/$CLUSTER_ID
     chown xcalar:xcalar /netstore/cluster/$CLUSTER_ID
-    echo "${EFSMOUNT}:/cluster/$CLUSTER_ID ${XLRROOT} nfs4 nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2	0	0" >>/etc/fstab
+    echo "${NFSMOUNT}cluster/$CLUSTER_ID ${XLRROOT} efs  defaults,_netdev	0	0" >>/etc/fstab
     mkdir -p ${XLRROOT}
     mount ${XLRROOT}
+    if ! test -d ${XLRROOT}/jupyterNotebooks; then
+        rsync -avzr /var/opt/xcalar/ ${XLRROOT}/
+    fi
 else
     XLRROOT=/var/opt/xcalar
     mkdir -p $XLRROOT
@@ -189,6 +193,7 @@ sed -i '/^Constants.XdbLocalSerDesPath=/d' $XCE_CONFIG
 echo "Constants.XdbLocalSerDesPath=$XCE_XDBSERDESPATH" >>$XCE_CONFIG
 mkdir -p $XCE_XDBSERDESPATH
 chown xcalar:xcalar $XCE_XDBSERDESPATH
+chkconfig xcalar on
 service xcalar start
 rc=$?
 
@@ -197,3 +202,4 @@ rc=$?
 
 # test_db
 echo >&2 "All done with user-data.sh"
+exit $rc
