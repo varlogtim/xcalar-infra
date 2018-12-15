a47e573436a5eebe94d85209b6b168207590100a
Amit Bakshi <abakshi@xcalar.com>
2018-12-15 11:40:48 -0800

    jenkins-log.sh: Accept wider range of inputs
    Change-Id: I2e06c77f25d7a00ea8cb48cea284a4941b4a13be

--- bin/jenkins-log.sh
+++ bin/jenkins-log.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 jenkins_log() {
-    local job_build=($(echo "$1" | sed -r 's@.*job/([A-Za-z0-9_\.-]+)/([0-9]+)$@\1 \2@'))
+    local job_build=($(echo "$1" | sed -r 's@.*job/([A-Za-z0-9_\.-]+)/([0-9]+).*$@\1 \2@'))
     ssh jenkins@jenkins "cat jobs/${job_build[0]}/builds/${job_build[1]}/log"
 }
 
@@ -9,8 +9,10 @@ usage() {
     cat << EOF
     usage: $0 job-url
 
-    job-url         A url to a job such as http://jenkins-url/job/SomeJob/30, or a partial url
-                    such as job/SomeJob/30
+    job-url         A url to a job such as http://jenkins-url/job/SomeJob/30, or a partial url such as:
+
+                    job/SomeJob/30
+                    https://jenkins.int.xcalar.com/job/GerritXCETest/15287/
 EOF
 }
 
