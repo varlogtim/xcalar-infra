327e047babdc24c3509f074c48e3a8543b3bc4b7
Amit Bakshi <abakshi@xcalar.com>
2019-01-18 15:40:53 -0800

    ovirt_setup: Use cached ub14-build when possible
    Change-Id: I0b9bab765faf9cad3b0899a30c9944597bcc3964

--- ovirt/ovirt_setup.sh
+++ ovirt/ovirt_setup.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 # automates environment setup required so ovirt_docker_wrapper can be run
 # Setup: http://wiki.int.xcalar.com/mediawiki/index.php/Ovirttool#Setup
 : "${XLRDIR:?Need to set non-empty XLRDIR}"
@@ -5,6 +6,13 @@ if [ -z "$XLRINFRADIR" ]; then
     export XLRINFRADIR="$(cd $SCRIPTDIR/.. && pwd)"
 fi
 
+set -e
 # ovirt_docker_wrapper will create a ub14 Docker container and run ovirttool in it;
 # setup so ub14 Docker containers can be built
-cd "$XLRDIR/docker/ub14" && make
+if ! docker pull registry.int.xcalar.com/xcalar/ub14-build:ovirttool; then
+    cd "$XLRDIR/docker/ub14"
+    echo "export http_proxy:=${http_proxy:-http://cacher:3128}" > local.mk
+    make ub14-build
+else
+    docker tag registry.int.xcalar.com/xcalar/ub14-build:ovirttool ub14-build:latest
+fi
