64b56c20dd4df83ed806172a6d00015044df8d52
Amit Bakshi <abakshi@xcalar.com>
2019-01-17 18:48:46 -0800

    TOOLS-111 AWS Cfn support for customers
    Change-Id: Iab144a2d7ce660e8fd5b1a2ee53a4c91b81d7181

--- aws/cfn/xdp-standard/deploy.sh
+++ aws/cfn/xdp-standard/deploy.sh
@@ -8,13 +8,17 @@ set -e
 case "$AWS_DEFAULT_REGION" in
     us-west-2) BUCKET=xcrepo;;
     us-east-1) BUCKET=xcrepoe1;;
-    *) BUCKET=xcrepoe1;;
+    *)
+        export AWS_DEFAULT_REGION=us-east-1
+        BUCKET=xcrepoe1
+        ;;
 esac
 
-DIR=$(cd $(dirname ${BASH_SOURCE[0]}) && pwd)
+readonly DIR=$(cd $(dirname ${BASH_SOURCE[0]}) && pwd)
+
 FIND_AMI=false
 DOIT=false
-DRY=true
+DRY=false
 
 # Return the path relative from XLRINFRADIR/aws.
 # ie, ~/xcalar-infra/aws/cfn/xdp-template -> cfn/xdp-template
@@ -22,6 +26,19 @@ infra_aws_s3_path() {
     set -x
     local dir="${1:-$PWD}"
     echo "${dir#${XLRINFRADIR}/aws/cfn/}"
+}
+
+usage() {
+    cat <<EOF
+    usage: $0 [--version VERSION] [--product PRODUCT] [--release|-r RELEASE]
+              [--dry-run]  [--force]
+              [--find-ami Look for newest AMI in each supported region]
+
+    $0 takes VERSION PRODUCT RELEASE , and use this to
+    publish the templates/scripts to those release directories on S3. These can then be referenced
+    by customers for their own deployment. The idea is we never break/change existing deploys.E
+EOF
+    exit 2
 }
 
 s3_sync() {
@@ -52,20 +69,19 @@ jq_get_userata_from_lc() {
     jq -r '.Resources.ClusterLC.Properties.UserData."Fn::Base64"."Fn::Sub"[0]'
 }
 
-
 # STDIN = template
 # $1 = Resource path to UserData
 # $2 = shell script to inject
 # Eg, jq_put_userata_in_lc ClusterLC.Properties.UserData deploy.sh < xdp-standard.json
 jq_put_userata_in_lc() {
-    jq -r ".Resources.ClusterLC.Properties.UserData.\"Fn::Base64\".\"Fn::Sub\"[0] = $(jq -R -s . < deploy.sh)" < xdp-standard.json
+    jq -r ".Resources.ClusterLC.Properties.UserData.\"Fn::Base64\".\"Fn::Sub\"[0] = $(jq -R -s . < scripts/user-data.sh)" < xdp-standard.json
 }
 
 transform() {
     cfn-flip < $DIR/${1}.template > ${1}.json.tmp || return 1
     jq -r '
-        .Mappings.AWSAMIRegionMap."us-east-1".AMZN1HVM = "'$AMI_US_EAST_1'" |
-        .Mappings.AWSAMIRegionMap."us-west-2".AMZN1HVM = "'$AMI_US_WEST_2'" |
+        .Mappings.AWSAMIRegionMap."us-east-1".AMZN1HVM = "'$AMZN1HVM_US_EAST_1'" |
+        .Mappings.AWSAMIRegionMap."us-west-2".AMZN1HVM = "'$AMZN1HVM_US_WEST_2'" |
         .Parameters.BootstrapUrl.Default = "'${BOOTSTRAP_URL}'"
     ' < ${1}.json.tmp | cfn-flip -c > ${1}.yaml.tmp || return 1
     cfn-flip < ${1}.yaml.tmp > ${1}.json.tmp || return 1
@@ -73,53 +89,58 @@ transform() {
     mv ${1}.yaml.tmp ${1}.yaml
 }
 
+## The directory is cfn/{env}/{name}/{build-id}/, where
+## env is prod/dev/test, and
 
 add_args=()
 while [[ $# -gt 0 ]]; do
     cmd="$1"
     shift
     case "$cmd" in
-        --doit) DRY=false;;
-        --dryrun) DRY=true;;
+        -h|--help) usage;;
+        --release|-r) RELEASE="$1"; shift;;
+        --version) VERSION="$1"; shift;;
+        --product) PRODUCT="$1"; shift;;
+        --build-id) BUILD_ID="$1"; shift;;
+        --dryrun|--dry-run) DRY=true;;
         --find-ami) FIND_AMI=true;;
         --force) FORCE=true;;
-        -*) echo >&2 "ERROR: Unknown option $cmd"; exit 1;;
+        -*) echo >&2 "ERROR: Unknown argument $cmd"; usage;;
         *) break;;
     esac
 done
 
-test -n "$RELEASE" || RELEASE=$(cat RELEASE)
 test -n "$VERSION" || VERSION=$(cat VERSION)
+test -n "$RELEASE" || RELEASE=$(cat RELEASE)
 test -n "$ENVIRONMENT" || ENVIRONMENT=$(cat ENVIRONMENT)
+test -n "$BUILD_ID" || BUILD_ID="${VERSION}-${RELEASE}"
+test -n "$PRODUCT" || PRODUCT="$(basename "$(pwd)")"
 
-mkdir -p ${RELEASE}/scripts/
-cd ${RELEASE} || exit 1
-if ! [ -e scripts/user-data.sh ]; then
-    shfmt -i 2 -ci -bn -sr -s < $DIR/scripts/user-data.sh > scripts/user-data.sh
+mkdir -p ${BUILD_ID}/scripts/
+cd ${BUILD_ID} || exit 1
+if $FORCE || ! test -e scripts/user-data.sh; then
+    cp $DIR/scripts/user-data.sh scripts/user-data.sh
 else
     echo >&2 "WARNING: Skipping scripts/user-data.sh because it exists"
 fi
 
-NAME="$(infra_aws_s3_path "$PWD")"
-TARGET=cfn/${ENVIRONMENT}/${NAME}
-
-
 BUCKET_ENDPOINT="https://$(aws_s3_endpoint $BUCKET)"
+TARGET=cfn/${ENVIRONMENT}/${PRODUCT}/${BUILD_ID}
+BASE_URL="${BUCKET_ENDPOINT}/${TARGET}"
+BOOTSTRAP_URL="${BASE_URL}/scripts/user-data.sh"
 
-BOOTSTRAP_URL="${BUCKET_ENDPOINT}/${TARGET}/scripts/user-data.sh"
-
-if ! [ -f AMI.txt ]; then
+if $FORCE || ! test -f AMI.ini; then
     if $FIND_AMI; then
-        AMI_US_EAST_1=$(ec2_find_ami --filters 'Name=tag:BaseOS,Values=AMZN1-2018.03' --owner self --region us-east-1)
-        AMI_US_WEST_2=$(ec2_find_ami --filters 'Name=tag:BaseOS,Values=AMZN1-2018.03' --owner self --region us-west-2)
-        echo "AMI_US_EAST_1=$AMI_US_EAST_1" | tee AMI.txt
-        echo "AMI_US_WEST_2=$AMI_US_WEST_2" | tee -a AMI.txt
+        AMZN1HVM_US_EAST_1=$(ec2_find_ami --filters 'Name=tag:BaseOS,Values=AMZN1-2018.03' --owner self --region us-east-1)
+        AMZN1HVM_US_WEST_2=$(ec2_find_ami --filters 'Name=tag:BaseOS,Values=AMZN1-2018.03' --owner self --region us-west-2)
+        echo "AMZN1HVM_US_EAST_1=$AMZN1HVM_US_EAST_1" | tee AMI.ini
+        echo "AMZN1HVM_US_WEST_2=$AMZN1HVM_US_WEST_2" | tee -a AMI.ini
     else
-        cp $DIR/AMI.txt .
+        cp $DIR/AMI.ini .
     fi
 fi
 
-. AMI.txt
+. AMI.ini
 
 for TEMPLATE in xdp-standard xdp-single; do
     if $FORCE || ! test -e ${TEMPLATE}.yaml;  then
@@ -127,20 +148,25 @@ for TEMPLATE in xdp-standard xdp-single; do
     else
         echo >&2 "WARNING: Skipping transform to ${TEMPLATE}.yaml, because it exists. Use --force"
     fi
-    echo "${BUCKET_ENDPOINT}/${TARGET}/${TEMPLATE}.yaml"
+    templateURL="${BASE_URL}/${TEMPLATE}.yaml"
+    echo "templateURL: $templateURL"
+    cfnURL="$(jinja2 -D templateURL="$templateURL" -D region=$AWS_DEFAULT_REGION -D stackName=XcalarS${TEMPLATE#xdp-s} $DIR/cloudformation-new.url.j2)"
+    echo "cfnURL: $cfnURL"
 done
 
 #if [ -e xdp-launch.json ]; then
 #    jq -r '
-#        .Mappings.AWSAMIRegionMap."us-east-1".AMZN1HVM = "'$AMI_US_EAST_1'" |
-#        .Mappings.AWSAMIRegionMap."us-west-2".AMZN1HVM = "'$AMI_US_WEST_2'"
+#        .Mappings.AWSAMIRegionMap."us-east-1".AMZN1HVM = "'$AMZN1HVM_US_EAST_1'" |
+#        .Mappings.AWSAMIRegionMap."us-west-2".AMZN1HVM = "'$AMZN1HVM_US_WEST_2'"
 #    ' < xdp-launch.json | cfn-flip -c > xdp-launch.yaml
 #    cfn-flip < xdp-launch.yaml > xdp-launch.json
 #fi
 
 if $DRY; then
     add_args+=(--dryrun)
-    echo >&2 "WARNING: Dry run only. Pass --doit, to actually copy."
+    echo >&2 "Dry run mode"
 fi
+
+add_args+=(--exclude='*.json')
 
 s3_sync --acl public-read "${add_args[@]}" . "s3://${BUCKET}/${TARGET}/"
--- aws/cfn/xdp-standard/scripts/user-data.sh
+++ aws/cfn/xdp-standard/scripts/user-data.sh
@@ -7,82 +7,103 @@ LOGFILE=/var/log/user-data.log
 touch $LOGFILE
 chmod 0600 $LOGFILE
 if [ -t 1 ]; then
-  :
+    :
 else
-  exec > >(tee -a $LOGFILE | logger -t user-data -s 2> /dev/console) 2>&1
+    exec > >(tee -a $LOGFILE | logger -t user-data -s 2>/dev/console) 2>&1
 fi
 
 ec2_find_cluster() {
-  aws ec2 describe-instances \
-    --filters Name=tag:$1,Values=$2 Name=instance-state-name,Values=running \
-    --query "Reservations[].Instances[].[AmiLaunchIndex,${3:-PrivateIpAddress}]" \
-    --output text | sort -n | awk '{print $2}'
+    aws ec2 describe-instances \
+        --filters Name=tag:$1,Values=$2 Name=instance-state-name,Values=running \
+        --query "Reservations[].Instances[].[AmiLaunchIndex,${3:-PrivateIpAddress}]" \
+        --output text | sort -n | awk '{print $2}'
 }
 
 eval $(ec2-tags -s -i)
 
-NFS_TYPE=nfs4
-NFS_OPTS="nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport"
 BOOTSTRAP_EXPECT=1
 
 while [ $# -gt 0 ]; do
-  cmd="$1"
-  shift
-  case "$cmd" in
-  --nfs-mount) NFSMOUNT="$1";shift;;
-  --nfs-type) NFS_TYPE="$1"; shift;;
-  --nfs-opts) NFS_OPTS="$1"; shift;;
-  --tag-key)
-    TAG_KEY="$1"
+    cmd="$1"
     shift
-    ;;
-  --tag-value)
-    TAG_VALUE="$1"
-    shift
-    ;;
-  --s3bucket)
-    S3BUCKET="$1"
-    shift
-    ;;
-  --s3prefix)
-    S3PREFIX="$1"
-    shift
-    ;;
-  --bootstrap-expect)
-    BOOTSTRAP_EXPECT="$1"
-    shift
-    ;;
-  --license)
-    test -z "$1" || LICENSE="$1"
-    shift
-    ;;
-  --installer-url)
-    test -z "$1" || INSTALLER_URL="$1"
-    shift
-    ;;
-  --stack-name)
-    STACK_NAME="$1"
-    shift
-    ;;
-  --resource-id)
-    RESOURCE_ID="$1"
-    shift
-    ;;
-  *) echo >&2 "WARNING: Unknown command $cmd" ;;
-  esac
+    case "$cmd" in
+        --nfs-mount)
+            NFSMOUNT="$1"
+            shift
+            ;;
+        --nfs-type)
+            NFS_TYPE="$1"
+            shift
+            ;;
+        --nfs-opts)
+            NFS_OPTS="$1"
+            shift
+            ;;
+        --tag-key)
+            TAG_KEY="$1"
+            shift
+            ;;
+        --tag-value)
+            TAG_VALUE="$1"
+            shift
+            ;;
+        --s3bucket)
+            S3BUCKET="$1"
+            shift
+            ;;
+        --s3prefix)
+            S3PREFIX="$1"
+            shift
+            ;;
+        --bootstrap-expect)
+            BOOTSTRAP_EXPECT="$1"
+            shift
+            ;;
+        --license)
+            test -z "$1" || LICENSE="$1"
+            shift
+            ;;
+        --installer-url)
+            test -z "$1" || INSTALLER_URL="$1"
+            shift
+            ;;
+        --stack-name)
+            STACK_NAME="$1"
+            shift
+            ;;
+        --resource-id)
+            RESOURCE_ID="$1"
+            shift
+            ;;
+        --admin-username)
+            ADMIN_USERNAME="$1"
+            shift
+            ;;
+        --admin-password)
+            ADMIN_PASSWORD="$1"
+            shift
+            ;;
+        --admin-email)
+            ADMIN_EMAIL="$1"
+            shift
+            ;;
+        *)
+            echo >&2 "WARNING: Unknown command $cmd"
+            ;;
+    esac
 done
 
 RELEASE_NAME=$(rpm -qf /etc/system-release --qf '%{NAME}')
 RELEASE_VERSION=$(rpm -qf /etc/system-release --qf '%{VERSION}')
 case "$RELEASE_VERSION" in
-6 | 6*) OSID=el6 ;;
-7 | 7*) OSID=el7 ;;
-2018*) OSID=amzn1 ;;
-2) OSID=amzn2 ;;
-*)
-  echo >&2 "ERROR: Unknown OS version $RELEASE_VERSION"
-  exit 1
-  ;;
+    6 | 6*) OSID=el6 ;;
+    7 | 7*) OSID=el7 ;;
+    2018*) OSID=amzn1 ;;
+    2) OSID=amzn2 ;;
+    *)
+        echo >&2 "ERROR: Unknown OS version $RELEASE_VERSION"
+        exit 1
+        ;;
 esac
 
 INSTANCE_ID=$(curl -sSf http://169.254.169.254/latest/meta-data/instance-id)
@@ -91,163 +112,221 @@ INSTANCE_TYPE=$(curl -sSf http://169.254.169.254/latest/meta-data/instance-type)
 export AWS_DEFAULT_REGION="${AVZONE%[a-f]}"
 
 export PATH=/opt/mssql-tools/bin:/opt/xcalar/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/opt/aws/bin:/bin
-echo "export PATH=$PATH" > /etc/profile.d/path.sh
+echo "export PATH=$PATH" >/etc/profile.d/path.sh
 
 set +e
 if [ -e /etc/ec2.env ]; then
-  . /etc/ec2.env
+    . /etc/ec2.env
 fi
 
-if [ $BOOTSTRAP_EXPECT -gt 1 ] && [ -z "$NFSMOUNT" ]; then
+if [ -z "$NFSMOUNT" ]; then
     case "$AWS_DEFAULT_REGION" in
-        us-east-1 | us-west-2) NFSMOUNT="netstore.${AWS_DEFAULT_REGION}.aws.xcalar.com:/";;
-        *) echo >&2 "Region ${AWS_DEFAULT_REGION} is not supported properly!"; exit 1;;
+        us-east-1) NFSMOUNT=fs-7803cc30:/ ;;
+        us-west-2) NFSMOUNT=fs-d4d4237d:/ ;;
+        *)
+            echo >&2 "Region ${AWS_DEFAULT_REGION} is not supported properly!"
+            exit 1
+            ;;
     esac
 fi
 
 NFSHOST="${NFSMOUNT%%:*}"
-NFSDIR="${NFSMOUNT#${NFSHOST}:/}"
+NFSDIR="${NFSMOUNT#$NFSHOST}"
+NFSDIR="${NFSDIR#:}"
+NFSDIR="${NFSDIR#/}"
 
-if [[ $NFSHOST =~ ^fs-[0-9a-f]{8}$ ]]; then
-    NFSHOST="${NFSHOST}.efs.${AWS_DEFAULT_REGION}.amazonaws.com"
+if [ -z "$NFS_TYPE" ]; then
+    if [[ $NFSHOST =~ ^fs-[0-9a-f]{8}$ ]]; then
+        rpm -q amazon-efs-utils || yum install -y amazon-efs-utils
+        NFS_TYPE=efs
+        NFS_OPTS="_netdev"
+    else
+        NFS_TYPE=nfs
+        NFS_OPTS="nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport"
+    fi
 fi
 
-
 if ! rpm -q xcalar; then
-  yum clean all --enablerepo='*'
-  yum install -y unzip yum-utils epel-release patch
-  yum install -y http://repo.xcalar.net/xcalar-release-${OSID}.rpm
-  yum-config-manager --enable xcalar-deps xcalar-deps-common epel
-  yum install -y jq amazon-efs-utils
+    yum clean all --enablerepo='*'
+    yum install -y unzip yum-utils epel-release patch
+    yum install -y http://repo.xcalar.net/xcalar-release-${OSID}.rpm
+    yum install -y jq amazon-efs-utils
 
-  mkdir -p -m 0700 /var/lib/xcalar-installer
-  cd /var/lib/xcalar-installer
+    mkdir -p -m 0700 /var/lib/xcalar-installer
+    cd /var/lib/xcalar-installer
 
-  yum install -y xcalar-sqldf ## Workaround a packaging bug
+    curl -L "https://s3.amazonaws.com/aws-cli/awscli-bundle.zip" -o awscli-bundle.zip && unzip awscli-bundle.zip && ./awscli-bundle/install -i /opt/aws -b /usr/local/bin/aws
+    yum install -y https://s3.amazonaws.com/cloudformation-examples/aws-cfn-bootstrap-latest.amzn1.noarch.rpm
+    yum install -y ephemeral-disk ec2tools --enablerepo='xcalar-*'
 
-  curl -L "https://s3.amazonaws.com/aws-cli/awscli-bundle.zip" -o awscli-bundle.zip && unzip awscli-bundle.zip && ./awscli-bundle/install -i /opt/aws -b /usr/local/bin/aws
-  yum install -y https://s3.amazonaws.com/cloudformation-examples/aws-cfn-bootstrap-latest.amzn1.noarch.rpm
-  yum install -y ephemeral-disk ec2tools --enablerepo='xcalar-deps'
-
-  if rpm -q java-1.7.0-openjdk > /dev/null 2>&1; then
-    yum remove -y java-1.7.0-openjdk || true
-  fi
-
-  sed -i -r 's/^(G|U)ID_MIN.*$/\1ID_MIN            1000/g' /etc/login.defs
-
-  if test -e installer.sh; then
-    mv installer.sh installer.sh.$$
-  fi
-  if [[ $INSTALLER_URL =~ ^http ]]; then
-    curl -fL "$INSTALLER_URL" -o installer.sh
-  elif [[ $INSTALLER_URL =~ s3:// ]]; then
-    aws s3 cp "$INSTALLER_URL" installer.sh
-  fi
-  if [ $? -eq 0 ] && test -s installer.sh; then
-    export ACCEPT_EULA=Y
-    chmod 0700 installer.sh
-    ./installer.sh --nostart 2>&1 | tee -a installer.log
-    if [ ${PIPESTATUS[0]} -ne 0 ]; then
-      echo "Failed to install ${PWD}/installer.sh (from $INSTALLER_URL)"
-      exit 1
+    if rpm -q java-1.7.0-openjdk >/dev/null 2>&1; then
+        yum remove -y java-1.7.0-openjdk || true
     fi
-  fi
+
+    sed -i -r 's/^(G|U)ID_MIN.*$/\1ID_MIN            1000/g' /etc/login.defs
+
+    if test -e installer.sh; then
+        mv installer.sh installer.sh.$$
+    fi
+    if [[ $INSTALLER_URL =~ ^http ]]; then
+        curl -fL "$INSTALLER_URL" -o installer.sh
+    elif [[ $INSTALLER_URL =~ s3:// ]]; then
+        aws s3 cp "$INSTALLER_URL" installer.sh
+    fi
+    if [ $? -eq 0 ] && test -s installer.sh; then
+        export ACCEPT_EULA=Y
+        chmod 0700 installer.sh
+        ./installer.sh --nostart 2>&1 | tee -a installer.log
+        if [ ${PIPESTATUS[0]} -ne 0 ]; then
+            echo "Failed to install ${PWD}/installer.sh (from $INSTALLER_URL)"
+            exit 1
+        fi
+    fi
 fi
 
 if [ -n "$LICENSE" ]; then
-  if [[ $LICENSE =~ ^s3:// ]]; then
-    aws s3 cp $LICENSE - | base64 -d | gzip -dc > /etc/xcalar/XcalarLic.key
-  else
-    echo "$LICENSE" | base64 -d | gzip -dc > /etc/xcalar/XcalarLic.key
-  fi
+    if [[ $LICENSE =~ ^s3:// ]]; then
+        aws s3 cp $LICENSE - | base64 -d | gzip -dc >/etc/xcalar/XcalarLic.key
+    else
+        echo "$LICENSE" | base64 -d | gzip -dc >/etc/xcalar/XcalarLic.key
+    fi
 fi
 
 touch /etc/xcalar/XcalarLic.key
 chown xcalar:xcalar /etc/xcalar/XcalarLic.key
 
-if [ -n "$NFSMOUNT" ]; then
-  mkdir -p /netstore
-  sed -i '\@/netstore @d' /etc/fstab
-  echo "${NFSMOUNT} /netstore ${NFS_TYPE} ${NFS_OPTS} 0	0" >> /etc/fstab
-  mount /netstore
-fi
+# $1 = server:/path/to/share
+# $2 = /mnt/localpath
+mount_xlrroot() {
+    local NFSHOST="${1%%:*}"
+    local NFSDIR="${1#$NFSHOST}"
+    NFSDIR="${NFSDIR#:}"
+    NFSDIR="${NFSDIR#/}"
+
+    set +e
+    mount -t $NFS_TYPE -o ${NFS_OPTS},timeo=3 $NFSHOST:/$NFSDIR $2
+    local rc=$?
+    if [ $rc -eq 32 ]; then
+        local tmpdir=$(mktemp -d -t nfs.XXXXXX)
+        mount -t $NFS_TYPE -o ${NFS_OPTS},timeo=3 $NFSHOST:/ $tmpdir
+        rc=$?
+        if [ $rc -eq 0 ]; then
+            mkdir -m 0700 -p ${tmpdir}/${NFSDIR}/members
+            chmod 0700 ${tmpdir}/${NFSDIR}
+            chown xcalar:xcalar ${tmpdir}/${NFSDIR} ${tmpdir}/${NFSDIR}/members
+            umount ${tmpdir}
+            mount -t $NFS_TYPE -o ${NFS_OPTS},timeo=3 $NFSHOST:/$NFSDIR $2
+            rc=$?
+        fi
+        rmdir $tmpdir
+    fi
+
+    if [ $rc -eq 0 ]; then
+        sed -i '\@'$2'@d' /etc/fstab
+        echo "${NFSHOST}:/${NFSDIR} $2  $NFS_TYPE  $NFS_OPTS 0 0" >> /etc/fstab
+    fi
+    return $rc
+}
 
 if [ -n "$CLUSTERNAME" ]; then
-  TAG_KEY=ClusterName
-  TAG_VALUE=$CLUSTERNAME
+    TAG_KEY=ClusterName
+    TAG_VALUE=$CLUSTERNAME
 elif [ -n "$AWS_CLOUDFORMATION_STACK_NAME" ]; then
-  TAG_KEY=aws:cloudformation:stack-name
-  TAG_VALUE=$AWS_CLOUDFORMATION_STACK_NAME
+    TAG_KEY=aws:cloudformation:stack-name
+    TAG_VALUE=$AWS_CLOUDFORMATION_STACK_NAME
 elif [ -n "$NAME" ]; then
-  TAG_KEY=Name
-  TAG_VALUE=$NAME
+    TAG_KEY=Name
+    TAG_VALUE=$NAME
 else
-  echo >&2 "No valid tags found"
+    echo >&2 "No valid tags found"
 fi
 
 if [ -n "$TAG_VALUE" ]; then
-  CLUSTER_ID=$TAG_VALUE
-  IPS=()
-  while [ "${#IPS[@]}" -eq 0 ]; do
-    if IPS=($(discover addrs provider=aws addr_type=private_v4 tag_key=$TAG_KEY tag_value=$TAG_VALUE region=$AWS_DEFAULT_REGION)); then
-      break
-    fi
+    CLUSTER_ID=$TAG_VALUE
+    IPS=()
+    while [ "${#IPS[@]}" -eq 0 ]; do
+        if IPS=($(ec2_find_cluster $TAG_KEY $TAG_VALUE)); then
+            break
+        fi
+        sleep 2
+    done
     sleep 5
-  done
-  sleep 5
 else
-  CLUSTER_ID="xcalar-$(uuid-gen)"
-  IPS=(localhost)
+    CLUSTER_ID="xcalar-$(uuid-gen)"
+    IPS=(localhost)
 fi
 NUM_INSTANCES="${#IPS[@]}"
-if [ $NUM_INSTANCES -gt 1 ]; then
-  XLRROOT=/mnt/xcalar
-  mkdir -p /netstore/cluster/$CLUSTER_ID
-  chown xcalar:xcalar /netstore/cluster/$CLUSTER_ID
-  sed -i '\@'${NFSMOUNT}'@d' /etc/fstab
-  echo "${NFSMOUNT}cluster/$CLUSTER_ID ${XLRROOT} ${NFS_TYPE} ${NFS_OPTS} 0	0" >> /etc/fstab
-  mkdir -p ${XLRROOT}
-  mount ${XLRROOT}
-  if ! test -d ${XLRROOT}/jupyterNotebooks; then
-    rsync -avzr /var/opt/xcalar/ ${XLRROOT}/
-  fi
-  /opt/xcalar/scripts/genConfig.sh /etc/xcalar/template.cfg - ${IPS[@]} | sed 's@^Constants.XcalarRootCompletePath=.*$@Constants.XcalarRootCompletePath='${XLRROOT}'@g' | tee /etc/xcalar/default.cfg
+
+MOUNT_OK=false
+XLRROOT=/var/opt/xcalar
+if [ -n "$NFSMOUNT" ]; then
+    if mount_xlrroot $NFSHOST:/${NFSDIR:-cluster/$CLUSTER_ID} /mnt/xcalar; then
+        MOUNT_OK=true
+        XLRROOT=/mnt/xcalar
+    fi
+fi
+
+if [ "$MOUNT_OK" = true ]; then
+    if ! test -d ${XLRROOT}/jupyterNotebooks; then
+        rsync -avzr /var/opt/xcalar/ ${XLRROOT}/
+    fi
+    /opt/xcalar/scripts/genConfig.sh /etc/xcalar/template.cfg - ${IPS[@]} | sed 's@^Constants.XcalarRootCompletePath=.*$@Constants.XcalarRootCompletePath='${XLRROOT}'@g' | tee /etc/xcalar/default.cfg
 else
-  XLRROOT=/var/opt/xcalar
-  mkdir -p $XLRROOT
-  chown xcalar:xcalar $XLRROOT
-  /opt/xcalar/scripts/genConfig.sh /etc/xcalar/template.cfg - localhost | tee /etc/xcalar/default.cfg
+    XLRROOT=/var/opt/xcalar
+    mkdir -p $XLRROOT
+    chown xcalar:xcalar $XLRROOT
+    /opt/xcalar/scripts/genConfig.sh /etc/xcalar/template.cfg - localhost | tee /etc/xcalar/default.cfg
 fi
 
 mkdir -m 0700 -p $XLRROOT/config
-chown xcalar:xcalar $XLRROOT $XLRROOT/config
-chmod 0700 $XLRROOT/config
-cat > $XLRROOT/config/defaultAdmin.json << EOF
+
+if test -x /opt/xcalar/scripts/genDefaultAdmin.sh; then
+    /opt/xcalar/scripts/genDefaultAdmin.sh \
+        --username "$ADMIN_USERNAME" \
+        --email "$ADMIN_EMAIL" \
+        --password "$ADMIN_PASSWORD" > $XLRROOT/config/defaultAdmin.json.tmp \
+        && mv $XLRROOT/config/defaultAdmin.json.tmp $XLRROOT/config/defaultAdmin.json
+fi
+
+if ! test -e $XLRROOT/config/defaultAdmin.json; then
+    cat >$XLRROOT/config/defaultAdmin.json <<EOF
 {
-  "username": "xdpadmin",
-  "password": "9021834842451507407c09c7167b1b8b1c76f0608429816478beaf8be17a292b",
-  "email": "info@xcalar.com",
-  "defaultAdminEnabled": true
+"username": "xdpadmin",
+"password": "9021834842451507407c09c7167b1b8b1c76f0608429816478beaf8be17a292b",
+"email": "info@xcalar.com",
+"defaultAdminEnabled": true
 }
 EOF
+fi
+
+chmod 0700 $XLRROOT/config
 chmod 0600 $XLRROOT/config/defaultAdmin.json
-chown -R xcalar:xcalar $XLRROOT $XLRROOT/config
+chown -R xcalar:xcalar $XLRROOT
 
-XCE_XDBSERDESPATH=${XCE_XDBSERDESPATH:-/var/opt/xcalar/serdes/}
-XCE_CONFIG=${XCE_CONFIG:-/etc/xcalar/default.cfg}
+EPHEMERAL=/ephemeral/data
 
-sed -i '/^Constants.XdbLocalSerDesPath=/d' $XCE_CONFIG
-echo "Constants.XdbLocalSerDesPath=$XCE_XDBSERDESPATH" >> $XCE_CONFIG
-mkdir -p $XCE_XDBSERDESPATH
-chown xcalar:xcalar $XCE_XDBSERDESPATH
+if test -x /usr/bin/ephemeral-disk && ! mountpoint -q $EPHEMERAL; then
+    ephemeral-disk || true
+fi
+
+if test -w $EPHEMERAL; then
+    chmod 0777 $EPHEMERAL
+    XCE_XDBSERDESPATH="${EPHEMERAL}/"
+    XCE_SERDESMODE=2
+fi
+
+if [ -n "$XCE_XDBSERDESPATH" ]; then
+    XCE_SERDESMODE=${XCE_SERDESMODE:-1}
+    XCE_CONFIG=${XCE_CONFIG:-/etc/xcalar/default.cfg}
+    sed -i '/^Constants.XdbLocalSerDesPath=/d' $XCE_CONFIG
+    echo "Constants.XdbLocalSerDesPath=$XCE_XDBSERDESPATH" >>$XCE_CONFIG
+    echo "Constants.XdbSerDesMode=$XCE_SERDESMODE" >>$XCE_CONFIG
+fi
+
 /etc/init.d/xcalar start
 rc=$?
 
 chkconfig xcalar on
 
-#aws s3 cp s3://blim-export/queue_handler.py /usr/local/bin/queue_handler.py
-#/opt/xcalar/bin/python3 /usr/local/bin/queue_handler.py  >> /var/log/queue_handler.log 2>&1 </dev/null &
-
-# test_db
 echo >&2 "All done with user-data.sh (rc=$rc)"
 exit $rc
