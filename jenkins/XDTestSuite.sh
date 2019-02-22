d6878c39862789eade96f8868049fe870ca0d7da
Wenhou Lu <wlu@xcalar.com>
2019-02-22 14:12:11 -0800

    Disable ExpServer test
    Change-Id: Iada87df1678a7e51d9e1ca657083387b6ff91552

--- jenkins/XDTestSuite.sh
+++ jenkins/XDTestSuite.sh
@@ -152,9 +152,9 @@ if  [ $JOB_NAME = "GerritSQLCompilerTest" ]; then
     npm test -- sqlTest https://localhost:8443 || exitCode=$?
 elif [ $JOB_NAME = "XDUnitTest" ]; then
     npm test -- unitTest https://localhost:8443 || exitCode=$?
-elif [ $JOB_NAME = "GerritExpServerTest" ]; then
-    npm test -- expServer || exitCode=$?
-    exitCode=0
+# elif [ $JOB_NAME = "GerritExpServerTest" ]; then
+#     npm test -- expServer || exitCode=$?
+#     exitCode=0
 else
     npm test -- testSuite https://localhost:8443 || exitCode=$?
 fi
