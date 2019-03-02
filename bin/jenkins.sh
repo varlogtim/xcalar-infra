9a0cf96aab2c744e0ea111da2536b24c2b4d6baa
Amit Bakshi <abakshi@xcalar.com>
2019-03-02 14:53:08 -0800

    AzureCluster: Externalize script
    Change-Id: I7d77ed195c3adc0cd4449dbf56c679cc2ac963c0

---
+++ bin/jenkins.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+
+# Common set up
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+export XLRINFRADIR="$(cd "$DIR/.." && pwd)"
+export PATH=$XLRINFRADIR/bin:$PATH:/opt/xcalar/bin
+
+export PYCURL_SSL_LIBRARY=openssl
+export LDFLAGS=-L/usr/local/opt/openssl/lib
+export CPPFLAGS=-I/usr/local/opt/openssl/include
+
+cd $XLRINFRADIR
+
+#rm -rf .venv
+test -e .venv || /opt/xcalar/bin/virtualenv .venv
+.venv/bin/pip install -q -r requirements.txt
+source .venv/bin/activate
+source $XLRINFRADIR/bin/infra-sh-lib
+source $XLRINFRADIR/azure/azure-sh-lib
+
+
+if [ -z "$XLRDIR" ] && [ -e doc/env/xc_aliases ]; then
+    export XLRDIR=$PWD
+fi
+
+if [ -n "$XLRDIR" ]; then
+    . doc/env/xc_aliases
+    if ! xcEnvEnter "$HOME/.local/lib/$JOB_NAME"; then
+        exit 1
+    fi
+    setup_proxy
+fi
+
+source $XLRINFRADIR/bin/infra-sh-lib
+
+# First look in local (Xcalar) repo for a script and fall back to the one in xcalar-infra
+for SCRIPT in "${XLRINFRADIR}/jenkins/${JOB_NAME}.sh"; do
+    if test -x "$SCRIPT"; then
+        break
+    fi
+done
+
+if ! test -x "${SCRIPT}"; then
+    echo >&2 "No jenkins script for for $JOB_NAME"
+    exit 1
+fi
+
+"$SCRIPT" "$@"
+ret=$?
+
+exit $ret
---
+++ jenkins/AzureCluster.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+if [ -z "$EMAIL" ]; then
+    echo "MUST SPECIFY EMAIL"
+    exit 1
+fi
+
+cd $XLRINFRADIR/azure
+
+az_login
+
+if [ "${INSTALLER_URL:0:1}" == / ]; then
+    echo "Uploading $INSTALLER_URL to Azure Blobstore..."
+    INSTALLER_URL="$(installer-url.sh -d az $INSTALLER_URL)" || exit 1
+fi
+INSTALLER_URL="${INSTALLER_URL%\?*}"
+echo "Installer: $INSTALLER_URL"
+
+APP=xdp-azurecluster-${BUILD_NUMBER}
+GROUP=${APP}-rg
+
+set -e
+
+TMPDIR=$(mktemp -d -t azure.XXXXXX)
+LOCATION=${LOCATION:-westus2}
+
+if ! az group show -g $GROUP > /dev/null 2>&1; then
+    say "Creating a new resource group $GROUP"
+
+    az group create -g $GROUP -l $LOCATION -ojson > /dev/null
+    trap "az group delete -g $GROUP --no-wait -y" EXIT
+fi
+
+if ! az_deploy -g $GROUP -l $LOCATION -i "$INSTALLER_URL" --count $NUM_NODES \
+    --size $INSTANCE_TYPE --name $APP --email "$EMAIL" > output.json; then
+    cat output.json >&2
+    echo >&2 "Failed to deploy your template"
+    exit 1
+fi
+
+URL="https://${APP}.${LOCATION}.cloudapp.azure.com"
+
+echo "Login at $URL using User: xdpadmin, Password: Welcome1"
+
+echo $URL > url.txt
+
+trap '' EXIT
+exit 0
