b432ec4409c559f42c78fab22d84508a60554724
Robert Stephens <rstephens@xcalar.com>
2019-04-05 16:09:24 -0700

    add infinite wait for cluster start
    Change-Id: Ic20d655c5ce40f04179e5c09c1f94de70a3ddc33

--- jenkins/PerformanceRegressionTest.sh
+++ jenkins/PerformanceRegressionTest.sh
@@ -4,6 +4,11 @@ source doc/env/xc_aliases
 
 trap '(xclean; kill $(jobs -p)) || true' SIGINT SIGTERM EXIT
 
+if [ "$JOB_NAME" != "" ]; then
+    # Don't fail on delayed cluster start
+    export TIME_TO_WAIT_FOR_CLUSTER_START="${TIME_TO_WAIT_FOR_CLUSTER_START:-99999}"
+fi
+
 # Build xcalar-gui so that expServer will run
 export XLRGUIDIR=$PWD/xcalar-gui
 (cd $XLRGUIDIR && make dev)
