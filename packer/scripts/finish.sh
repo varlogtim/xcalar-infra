bb023600d4c399a11a421f9736c8cc9c360bbee3
Amit Bakshi <abakshi@xcalar.com>
2019-04-15 20:24:16 -0700

    AWS & Azure: Make sure the image doesn't have keys in it
    Change-Id: I6668095c5df2b15d3121c612d43e6694c359f6f5

--- packer/scripts/finish.sh
+++ packer/scripts/finish.sh
@@ -13,6 +13,10 @@ truncate -s 0 /var/log/secure /va/var/log/messages /var/log/dmesg /var/log/audit
 
 rm -f /var/log/startupscript.log /var/log/dmesg.old /var/log/cfn-* /var/log/cloud-init* /var/log/user-data*
 rm -f /etc/hostname /root/.bash_history /home/*/.bash_history
+if [[ "$BUILD_TYPE" =~ amazon ]] || [[ "$BUILD_TYPE" =~ azure ]]; then
+    echo >&2 "Detected $BUILD_TYPE, deleting authorized_keys"
+    rm -fv /root/.ssh/authorized_keys /home/*/.ssh/authorized_keys
+fi
 
 rm -rf /var/lib/cloud/instances/*
 
@@ -26,9 +30,9 @@ history -c
 export HISTSIZE=0
 export HISTFILESIZE=0
 
-#if test -e /usr/sbin/waagent; then
-#	echo >&2 "Running Azure deprovisioner ..."
-#	/usr/sbin/waagent -force -deprovision+user && export HISTSIZE=0 && sync
-#fi
+if test -e /usr/sbin/waagent; then
+	echo >&2 "Running Azure deprovisioner ..."
+	/usr/sbin/waagent -force -deprovision+user && export HISTSIZE=0 && sync
+fi
 sync
 exit 0
