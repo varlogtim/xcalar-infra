f4b0996bffa537dfc4d4be7557196b9c8d65c82a
Wenhou Lu <wlu@xcalar.com>
2019-08-28 12:11:29 -0700

    Enable CORS on all apis
    Change-Id: I933f51ee31d2301a08a5c49a6a70293f5f0dd646

--- aws/lambdaFns/saas/sam-saas/lib/python/util/http_util.py
+++ aws/lambdaFns/saas/sam-saas/lib/python/util/http_util.py
@@ -1,6 +1,11 @@
 import json
 def _make_reply(code, message):
-    return {"statusCode": code, "body": json.dumps(message)}
+    return {
+        'statusCode': code,
+        'body': json.dumps(message),
+        'headers': {
+            'Access-Control-Allow-Origin': '*'
+        }}
 
 def _http_status(resp):
-    return resp["ResponseMetadata"]["HTTPStatusCode"]
+    return resp['ResponseMetadata']['HTTPStatusCode']
