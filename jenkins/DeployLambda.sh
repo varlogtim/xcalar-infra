08b775a82cf7bc90fe55357495ecf2e4279b6c8b
Mingke Tang <mtang@xcalar.com>
2019-09-05 13:32:17 -0700

    SQL-864 lambda function deployment automation
    Change-Id: I77c494b6ac2081e2e2f72b9d43786ee5b06f1e52

---
+++ jenkins/DeployLambda.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+set -ex
+export AWS_DEFAULT_REGION=us-west-2
+export XLRINFRADIR=${XLRINFRADIR-$HOME/xcalar-infra}
+SAM_SAAS_DIR="$XLRINFRADIR/aws/lambdaFns/saas/sam-saas"
+
+
+if ! aws s3 ls ${S3_BUCKET}; then
+    aws s3 mb ${S3_BUCKET}
+fi
+
+if ! aws dynamodb describe-table --table-name ${USER_TABLE_NAME}; then
+    aws dynamodb create-table --table-name ${USER_TABLE_NAME}
+        --attribute-definitions AttributeName=user_name,AttributeType=S \
+        --key-schema AttributeName=user_name,KeyType=HASH \
+        --billing-mode PAY_PER_REQUEST
+fi
+
+if ! aws dynamodb describe-table --table-name ${BILLING_TABLE_NAME}; then
+    aws dynamodb create-table --table-name ${BILLING_TABLE_NAME} \
+        --attribute-definitions AttributeName=user_name,AttributeType=S AttributeName=timestamp,AttributeType=S \
+        --key-schema AttributeName=user_name,KeyType=HASH AttributeName=timestamp,KeyType=RANGE \
+        --billing-mode PAY_PER_REQUEST
+fi
+
+
+(cd "$SAM_SAAS_DIR" &&
+sam build &&
+sam package --output-template packaged.yaml --s3-bucket ${S3_BUCKET} &&
+sam deploy --template-file packaged.yaml \
+           --capabilities CAPABILITY_IAM \
+           --stack-name ${STACK_NAME} \
+           --role-arn ${ROLE})
+
+
+
+
+
+
+
