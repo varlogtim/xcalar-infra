c0fd2591738d0e2a559c58b2aa4b621038a8ade8
Jiyuan Li <jli@xcalar.com>
2019-10-25 17:47:54 -0700

    ENG-4928 Automate the cloudLogin build process - regex change remove /
    Change-Id: If64250052ce326ab99d43f096f92577370e22dac

--- jenkins/UpdateCloudXD.sh
+++ jenkins/UpdateCloudXD.sh
@@ -9,10 +9,10 @@ if ! aws s3 ls ${S3_BUCKET}; then
     aws s3 mb "s3://${S3_BUCKET}" --region ${AWS_REGION}
 fi
 
-MAIN_URL=`aws ssm get-parameter --region ${AWS_REGION} --name "/xcalar/cloud/main/${MAIN_LAMBDA_STACK_NAME}" --query "Parameter.Value" | sed -e 's/^".*XCE_SAAS_MAIN_LAMBDA_URL=//' -e 's/\\\\n.*"$//' -e 's/"$//'`
-AUTH_URL=`aws ssm get-parameter --region ${AWS_REGION} --name "/xcalar/cloud/auth/${AUTH_LAMBDA_STACK_NAME}" --query "Parameter.Value" | sed -e 's/^".*XCE_SAAS_AUTH_LAMBDA_URL=//' -e 's/\\\\n.*"$//' -e 's/"$//'`
-USER_POOL_ID=`aws ssm get-parameter --region ${AWS_REGION} --name "/xcalar/cloud/auth/${AUTH_LAMBDA_STACK_NAME}" --query "Parameter.Value" | sed -e 's/^".*XCE_CLOUD_USER_POOL_ID=//' -e 's/\\\\n.*"$//' -e 's/"$//'`
-CLIENT_ID=`aws ssm get-parameter --region ${AWS_REGION} --name "/xcalar/cloud/auth/${AUTH_LAMBDA_STACK_NAME}" --query "Parameter.Value" | sed -e 's/^".*XCE_CLOUD_CLIENT_ID=//' -e 's/\\\\n.*"$//' -e 's/"$//'`
+MAIN_URL=`aws ssm get-parameter --region ${AWS_REGION} --name "/xcalar/cloud/main/${MAIN_LAMBDA_STACK_NAME}" --query "Parameter.Value" | sed -e 's/^".*XCE_SAAS_MAIN_LAMBDA_URL=//' -e 's/\\\\n.*"$//' -e 's/"$//' -e 's/\/$//'`
+AUTH_URL=`aws ssm get-parameter --region ${AWS_REGION} --name "/xcalar/cloud/auth/${AUTH_LAMBDA_STACK_NAME}" --query "Parameter.Value" | sed -e 's/^".*XCE_SAAS_AUTH_LAMBDA_URL=//' -e 's/\\\\n.*"$//' -e 's/"$//' -e 's/\/$//'`
+USER_POOL_ID=`aws ssm get-parameter --region ${AWS_REGION} --name "/xcalar/cloud/auth/${AUTH_LAMBDA_STACK_NAME}" --query "Parameter.Value" | sed -e 's/^".*XCE_CLOUD_USER_POOL_ID=//' -e 's/\\\\n.*"$//' -e 's/"$//' -e 's/\/$//'`
+CLIENT_ID=`aws ssm get-parameter --region ${AWS_REGION} --name "/xcalar/cloud/auth/${AUTH_LAMBDA_STACK_NAME}" --query "Parameter.Value" | sed -e 's/^".*XCE_CLOUD_CLIENT_ID=//' -e 's/\\\\n.*"$//' -e 's/"$//' -e 's/\/$//'`
 
 echo "Building XD"
 cd $XLRGUIDIR
