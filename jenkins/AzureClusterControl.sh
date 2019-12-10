f6078d414bd034beec2b809d672df8e1f21f8870
Amit Bakshi <abakshi@xcalar.com>
2019-12-10 10:34:17 -0800

    ENG-6030 Add autoshutdown
    Change-Id: I093e95cc5a60a401334100752ec62bbe1470c830

--- jenkins/AzureClusterControl.sh
+++ jenkins/AzureClusterControl.sh
@@ -8,13 +8,13 @@ source azure/azure-sh-lib
 az_login
 
 for groupName in ${CLUSTER_NAME} ${CLUSTER_NAME%-rg}-rg ${CLUSTER_NAME}-rg ${CLUSTER_NAME%-*} ${CLUSTER_NAME%-*}-rg; do
-  if az group show -g $groupName; then
+  if az group show -g "$groupName"; then
      if [ "$RG_COMMAND" = delete ]; then
-        az group delete -g ${groupName} --yes
+        az group delete -g "${groupName}" --yes
      elif [ "$RG_COMMAND" == scheduled_shutdown ]; then
-        az_rg_scheduled_shutdown -g ${groupName} --time "${TIME:-2300}" --timezone "${TIMEZONE:-pst}" --enable "${ENABLE:-true}"
+        az_rg_scheduled_shutdown -g "${groupName}" --time "${TIME:-2300}" --timezone "${TIMEZONE:-pst}" --enable "${AUTOSHUTDOWN:-true}"
      else
-        az_rg_${RG_COMMAND} ${groupName}
+        az_rg_${RG_COMMAND} "${groupName}"
      fi
      exit $?
   fi
