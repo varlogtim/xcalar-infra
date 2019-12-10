a546ef9d32a955ff92aef2ebea02b7455fc00fe3
Amit Bakshi <abakshi@xcalar.com>
2019-12-10 10:28:17 -0800

    ENG-6030 Add AzureClusterControl
    Change-Id: I73dd37b6e2652b53f4fda63fe6776de421e4299d

---
+++ jenkins/AzureClusterControl.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set +x
+export XLRINFRADIR=$PWD
+export PATH=$PWD/bin:/opt/xcalar/bin:$PATH
+source azure/azure-sh-lib
+
+az_login
+
+for groupName in ${CLUSTER_NAME} ${CLUSTER_NAME%-rg}-rg ${CLUSTER_NAME}-rg ${CLUSTER_NAME%-*} ${CLUSTER_NAME%-*}-rg; do
+  if az group show -g $groupName; then
+     if [ "$RG_COMMAND" = delete ]; then
+        az group delete -g ${groupName} --yes
+     elif [ "$RG_COMMAND" == scheduled_shutdown ]; then
+        az_rg_scheduled_shutdown -g ${groupName} --time "${TIME:-2300}" --timezone "${TIMEZONE:-pst}" --enable "${ENABLE:-true}"
+     else
+        az_rg_${RG_COMMAND} ${groupName}
+     fi
+     exit $?
+  fi
+done
+echo "No cluster named ${CLUSTER_NAME} found"
+exit 1
