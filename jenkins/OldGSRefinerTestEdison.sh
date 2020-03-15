7531f02d0811afbb0c1c6c1bd9b461d014e09a5e
Robert Stephens <rstephens@xcalar.com>
2020-03-15 13:14:22 -0700

    ENG-7385 - don't rely on public wheels, they're out of sync
    Change-Id: I409d1f4649c1b899d5edb502f57143cb90f98f19

--- jenkins/OldGSRefinerTestEdison.sh
+++ jenkins/OldGSRefinerTestEdison.sh
@@ -33,19 +33,20 @@ if [ -z $INSTANCES ]; then
     exit 1
 fi
 
-
 say "OldGSRefinerTest BUILD ===="
+# XXXrs - Tech Debt?
 cd $XLRDIR
-#cmBuild clean
-#cmBuild config debug
-# XXXrs - Tech Debt
-# Only want to get the python in place to run our Python script,
-# but can't # figure out the right make target in a timely manner.
+cmBuild clean
+cmBuild config debug
+# XXXrs - Only want to get the python in place to run our Python script,
+# but can't figure out the lightest-weight make target.
 # "xce" is (very) heavyweight but works reliably, so just use it (forever?).
-#cmBuild xce
-python3 -m pip install -U pip
-curl https://storage.googleapis.com/repo.xcalar.net/xcalar-sdk/requirements-2.2.0.txt --output requirements.txt
-pip install -r requirements.txt
+cmBuild xce
+
+# XXXrs - Tried to get away with the following, but too easy to get out-of-sync
+#python3 -m pip install -U pip
+#curl https://storage.googleapis.com/repo.xcalar.net/xcalar-sdk/requirements-2.2.0.txt --output requirements.txt
+#pip install -r requirements.txt
 
 say "OldGSRefinerTest RUN old_gs_refiner_test.py ===="
 
