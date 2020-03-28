a48f7b61fc0ffbd6028b8f1b2048aacc62a3e976
Amit Bakshi <abakshi@xcalar.com>
2020-03-27 22:29:58 -0700

    ENG-6030 Update fio-test.sh script
    Change-Id: Id409da7726b8721dfe5745bbdaa04a49d860245e

--- scripts/fio-test.sh
+++ scripts/fio-test.sh
@@ -2,6 +2,14 @@
 
 set -e
 
+if test -e /etc/system-release; then
+    RPM=1
+    FMT=rpm
+else
+    RPM=0
+    FMT=deb
+fi
+
 build_fio() {
     FIOVER=3.18
     FIOURL=https://github.com/axboe/fio/archive/fio-${FIOVER}.tar.gz
@@ -14,7 +22,7 @@ build_fio() {
         make -j$(nproc)
         make DESTDIR=$DESTDIR install
     )
-    fpm -s dir -t rpm --name fio --version $FIOVER --iteration 10 -f -C $DESTDIR
+    fpm -s dir -t $FMT --name fio --version $FIOVER --iteration 10 -f -C $DESTDIR
 }
 
 build_ioping() {
@@ -31,40 +39,59 @@ build_ioping() {
         mv $DESTDIR/usr/local/* $DESTDIR/usr/
         rm -rf ${DESTDIR:?Dont nuke my computer}/usr/local
     )
-    fpm -s dir -t rpm --name ioping --version $IOVER --iteration 10 -f -C $DESTDIR
+    fpm -s dir -t $FMT --name ioping --version $IOVER --iteration 10 -f -C $DESTDIR
 }
 
 deps() {
-    sudo yum install -y make gcc libaio-devel || (apt-get update && apt-get install -y make gcc libaio-dev < /dev/null)
+    if ((RPM)); then
+        yum install -y make gcc libaio-devel
+    else
+        apt-get update && apt-get install -y make gcc libaio-dev
+    fi
 }
 
 get_or_build() {
-    if ! command -v "$1" >/dev/null; then
-        if ! yum install -y "$1" --enablerepo='xcalar*'; then
-            if ! yum install -y $(cd $(dirname ${BASH_SOURCE[0]}) && pwd)/${1}*.rpm; then
-                if eval build_"${1}"; then
-                    yum install -y "${1}"*.rpm
-                fi
-            fi
-        fi
+    if test -e /etc/system-release; then
+        yum install -y $1 --enablerepo='xcalar*'
+    else
+        apt-get install -y $1
+    fi
+    if [ $? -ne 0 ]; then
+        deps
+        eval build_"$1"
     fi
 }
 
-export PATH=/usr/local/bin:/usr/local/sbin:/usr/bin:/usr/sbin:/sbin:/bin
+checkFs() {
+    fs=$(findmnt -nT ${1:-$(pwd)} | awk '{print $3}')
+    case "$fs" in
+        ext*|xfs) echo >&2 "Filesystem $fs is ok";;
+        nfs*) echo >&2 "You must run this on local storage, not on $fs!"; exit 2;;
+        *) echo >&2 "Unknown filesystem $fs, better safe than sorry. Bailing"; exit 2;;
+    esac
+}
 
-if ! command -v fio || ! command -v ioping; then
-    if ! yum localinstall -y $(cd $(dirname ${BASH_SOURCE[0]}) && pwd)/{fpm,ioping}*.rpm; then
-        deps
-        build_fio
-        build_ioping
-        if ! yum install -y {fpm,ioping}*.rpm; then
-            exit 1
-        fi
-    fi
+
+if [ $(id -u) != 0 ]; then
+    echo >&2 "Need to run as root"
+    exit 1
 fi
 
-sudo ioping -c 10 .
+checkFs "$(pwd)"
 
-sudo fio --randrepeat=1 --ioengine=libaio --direct=1 --gtod_reduce=1 --name=test --filename=test --bs=4k --iodepth=64 --size=4G --readwrite=randrw --rwmixread=75
-sudo fio --randrepeat=1 --ioengine=libaio --direct=1 --gtod_reduce=1 --name=test --filename=test --bs=4k --iodepth=64 --size=4G --readwrite=randread
-sudo fio --randrepeat=1 --ioengine=libaio --direct=1 --gtod_reduce=1 --name=test --filename=test --bs=4k --iodepth=64 --size=4G --readwrite=randwrite
+export PATH=/usr/local/bin:/usr/local/sbin:/usr/bin:/usr/sbin:/sbin:/bin
+
+set +e
+for req in fio ioping; do
+    if ! command -v $req; then
+        get_or_build $req
+    fi
+done
+
+set -e
+
+ioping -c 10 .
+
+fio --randrepeat=1 --ioengine=libaio --direct=1 --gtod_reduce=1 --name=test --filename=test --bs=4k --iodepth=64 --size=4G --readwrite=randrw --rwmixread=75
+fio --randrepeat=1 --ioengine=libaio --direct=1 --gtod_reduce=1 --name=test --filename=test --bs=4k --iodepth=64 --size=4G --readwrite=randread
+fio --randrepeat=1 --ioengine=libaio --direct=1 --gtod_reduce=1 --name=test --filename=test --bs=4k --iodepth=64 --size=4G --readwrite=randwrite
