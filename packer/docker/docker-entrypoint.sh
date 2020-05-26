4cc5779c57bc7f8de0ab3686301b5233b1c2b891
Amit Bakshi <abakshi@xcalar.com>
2020-05-26 16:39:17 -0700

    ENG-8020 dcc: XCE container management fixes
    - Many small fixes to dcc tool from user feedback
    - Integrate ytt to help with duplication [YAML sprawl](https://get-ytt.io/)
    - Fixes for Packer image building
    
    Change-Id: I16f0a90b5da70b70132026c3d842172fc455f8fb

--- packer/docker/docker-entrypoint.sh
+++ packer/docker/docker-entrypoint.sh
@@ -7,16 +7,37 @@ if [ "$(id -u)" != 0 ]; then
     exit 1  # shouldn't reach here
 fi
 
-SERVICES="xcalar-usrnode.service xcalar-caddy.service"
+if ! test -e /usr/bin/java; then
+    if ! _java_cmd="$(command -v java)"; then
+        if [ -z "$_java_cmd" ]; then
+            export JAVA_HOME=/opt/xcalar/lib/java8/jre
+        fi
+    fi
+fi
+if test -z "$JAVA_HOME" || ! test -e "$JAVA_HOME"; then
+    export JAVA_HOME=/opt/xcalar/lib/java8/jre
+fi
+export PATH=$PATH:${JAVA_HOME}/bin
+if ! test -e /usr/bin/java; then
+    ln -sfn $JAVA_HOME/bin/java /usr/bin/java
+fi
 
 touch /etc/sysconfig/dcc
+cat > /etc/sysconfig/dcc <<EOF
+AWS_DEFAULT_REGION=${AWS_DEFAULT_REGION:-us-west-2}
+CLUSTER=${CLUSTER:-xcalar}
+JAVA_HOME=$JAVA_HOME
+PATH=$PATH
+XLRDIR=/opt/xcalar
+EOF
+
+SERVICES="xcalar-sqldf.service xcalar-jupyter.service xcalar-usrnode.service xcalar-caddy.service"
 for SVC in $SERVICES; do
     mkdir -p /etc/systemd/system/${SVC}.d || continue
     cat > /etc/systemd/system/${SVC}.d/99-dcc.conf <<EOF
 [Service]
-Environment=AWS_DEFAULT_REGION=${AWS_DEFAULT_REGION:-us-west-2}
-Environment=CLUSTER=${CLUSTER:-xcalar}
 EnvironmentFile=/etc/sysconfig/dcc
+UnsetEnvironment=PYTHONHOME
 ${ENVLIST:+Environment=ENVLIST=$ENVLIST}
 ${ENVLIST:+EnvironmentFile=$ENVLIST}
 EOF
