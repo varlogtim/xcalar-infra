747d2b77c3d642a23c103d2fa9c7e25484d4780f
Amit Bakshi <abakshi@xcalar.com>
2020-05-28 08:34:12 -0700

    ENG-6030 BuildPacker fixes
    Change-Id: Id7415050d48e407f6833c557de8d30d11da16efc

--- jenkins/BuildPacker.sh
+++ jenkins/BuildPacker.sh
@@ -1,28 +1,39 @@
 #!/bin/bash
-set -e
+set -ex
 
 export XLRDIR=${XLRDIR:-$PWD}
 export XLRINFRADIR=${XLRINFRADIR:-$PWD/xcalar-infra}
-export PATH=$XLRDIR/bin:$XLRINFRADIR/bin:$PATH
+export PATH=$XLRINFRADIR/bin:$XLRDIR/bin:$PATH
+export REGISTRY=${REGISTRY:-$DEFAULT_REGISTRY}
+REPO=${REPO:-xcalar/xcalar}
 
-cd $XLRDIR
-. doc/env/xc_aliases
+mkdir -p ${OUTDIR}
 
 export PATH="$XLRINFRADIR/bin:$XLRDIR/bin:$PATH"
 
 cd $XLRINFRADIR
 . bin/activate
 
-export VAULT_TOKEN=$($XLRINFRADIR/bin/vault-auth-puppet-cert.sh --print-token)
+if [ -n "$EXECUTOR_NUMBER" ]; then
+    git clean -fxd packer/
+    docker rm $(docker ps -aq) || true
+
+    if images=($(docker images --filter reference="${REGISTRY}/${REPO}:*" --format '{{.Repository}}:{{.Tag}}')); then
+        docker rmi "${images[@]}" || true
+    fi
+fi
+
+DIR=$(dirname ${XLRINFRADIR}/${PACKERCONFIG})
+cd $DIR
+
+vault-auth-puppet-cert.sh
+set -o pipefail
 vault kv get -format=json -field=data secret/xcalar_licenses/cloud | jq -r '{license:.}' > license.json
 export LICENSE_DATA=$PWD/license.json
-
-(
-
+cp ${OUTDIR}/*-manifest.json . || true
 for BUILDER in ${BUILDERS//,/ }; do
-    DIR=$(dirname ${XLRINFRADIR}/${PACKERCONFIG}) && \
-    cd $DIR && \
-    make $BUILDER "INSTALLER=$INSTALLER" "INSTALLER_URL=$INSTALLER_URL" "REGISTRY=${REGISTRY:-registry.int.xcalar.com}"
+    make $BUILDER "INSTALLER=$INSTALLER" ${INSTALLER_URL:+"INSTALLER_URL=$INSTALLER_URL"} "REGISTRY=$REGISTRY"
 done
-)
-exit $?
+cp *-manifest.json ${OUTDIR}/ || true
+
+exit 0
