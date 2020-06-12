e359fb5e553633b531c76f6494c7ef4ed583bc30
Robert Stephens <rstephens@xcalar.com>
2020-06-12 13:32:26 -0700

    ENG-7224 - add IMD merge to TPC tests
    Change-Id: Ic744dd7fe4a9346054a45931b04b83017fab6f11

---
+++ jenkins/TPCTests.sh
@@ -0,0 +1,101 @@
+#!/bin/bash
+set -e
+set -x
+
+# Run tpc_tests.py against the given host/port.
+# Assumes Xcalar server managed elsewhere.
+
+say () {
+    echo >&2 "$*"
+}
+
+say "TPCTests START ===="
+
+say "TPCTests validate/default environment inputs ===="
+
+# Construct TEST_ID
+TEST_ID="${JOB_NAME}:${BUILD_ID}"
+
+TEST_JDBC_PATH="${TEST_JDBC_PATH:-${XLRDIR}/src/sqldf/tests/test_jdbc.py}"
+
+if [ -z $JDBC_HOST ]; then
+    say "ERROR: JDBC_HOST cannot be empty"
+    exit 1
+fi
+
+JDBC_PORT="${JDBC_PORT:-10000}"
+API_PORT="${API_PORT:-443}"
+USER="${USER:-admin}"
+PASS="${PASS:-admin}"
+
+TPCDS_WORKERS="${TPCDS_WORKERS:-0}"
+TPCDS_SF="${TPCDS_SF:-10}"
+TPCDS_PLAN="${TPCDS_PLAN:-/netstore/datasets/tpcds_new/sf_10}"
+TPCDS_LOOPS="${TPCDS_LOOPS:-1}"
+TPCDS_SEED="${TPCDS_SEED:-123}"
+TPCDS_IMD_MERGE="${TPCDS_IMD_MERGE:-false}"
+
+TPCH_WORKERS="${TPCH_WORKERS:-0}"
+TPCH_SF="${TPCH_SF:-10}"
+TPCH_PLAN="${TPCH_PLAN:-/netstore/datasets/tpch_new/sf_10}"
+TPCH_LOOPS="${TPCH_LOOPS:-1}"
+TPCH_SEED="${TPCH_SEED:-456}"
+TPCH_IMD_MERGE="${TPCH_IMD_MERGE:-false}"
+
+# XXXrs - this seems heavyweight but can't find anything else :(
+say "TPCTests do XCE build to stage python packages for SDK ===="
+cd $XLRDIR
+cmBuild clean
+cmBuild config debug
+cmBuild xce
+
+say "TPCTests run tpc_tests.py ===="
+
+
+XLRINFRADIR="${XLRINFRADIR:-${XLRDIR}/xcalar-infra}"
+pydir="${XLRINFRADIR}/jenkins/python"
+
+# Common arguments...
+ARGS=" --test_id=$TEST_ID --test_jdbc_path=$TEST_JDBC_PATH --jdbc_host=$JDBC_HOST --jdbc_port=$JDBC_PORT"
+ARGS="$ARGS --api_port=$API_PORT --user=$USER --pass=$PASS"
+
+if [ $TPCDS_WORKERS -gt 0 ] || [ $TPCDS_IMD_MERGE = "true" ]; then
+    ARGS="$ARGS --tpcds_workers=$TPCDS_WORKERS --tpcds_sf=$TPCDS_SF --tpcds_plan=$TPCDS_PLAN"
+    ARGS="$ARGS --tpcds_loops=$TPCDS_LOOPS --tpcds_seed=$TPCDS_SEED"
+    if [ -n "$TPCDS_SKIPS" ]; then
+        ARGS="$ARGS --tpcds_skips=$TPCDS_SKIPS"
+    fi
+fi
+if [ $TPCDS_IMD_MERGE = "true" ]; then
+    ARGS="$ARGS --tpcds_imd_merge"
+fi
+if [ $TPCH_WORKERS -gt 0 ] || [ $TPCH_IMD_MERGE = "true" ]; then
+    ARGS="$ARGS --tpch_workers=$TPCH_WORKERS --tpch_sf=$TPCH_SF --tpch_plan=$TPCH_PLAN"
+    ARGS="$ARGS --tpch_loops=$TPCH_LOOPS --tpch_seed=$TPCH_SEED"
+    if [ -n "$TPCH_SKIPS" ]; then
+        ARGS="$ARGS --tpch_skips=$TPCH_SKIPS"
+    fi
+fi
+if [ $TPCH_IMD_MERGE = "true" ]; then
+    ARGS="$ARGS --tpch_imd_merge"
+fi
+
+
+set +e
+python ${pydir}/tpc_tests.py $ARGS
+rtn=$?
+
+# If we happen to be running in a "cloud context"
+# Expect $VmProvider and $CLUSTER to be set appropriately.
+if [ ! -z $VmProvider ]; then
+    # Generate support bundle(s)
+    source "${XLRINFRADIR}/bin/clusterCmds.sh"
+    initClusterCmds
+    genSupport "${CLUSTER}"
+else
+    # XXXrs - WORKING HERE - Collect post-test artifacts from remote machine.
+    #         Can/should this also be support bundles?
+    say ""
+fi
+say "TPCTests END ===="
+exit $rtn
---
+++ jenkins/python/tpc_tests.py
@@ -0,0 +1,220 @@
+#!/usr/bin/env python3
+
+# Copyright 2020 Xcalar, Inc. All rights reserved.
+#
+# No use, or distribution, of this source code is permitted in any form or
+# means without a valid, written license agreement with Xcalar, Inc.
+# Please refer to the included "COPYING" file for terms and conditions
+# regarding the use and redistribution of this software.
+
+
+import argparse
+import logging
+import os
+import random
+import shlex
+import signal
+import subprocess
+import sys
+import time
+
+if __name__ == '__main__':
+    sys.path.append(os.environ.get('XLRINFRADIR', ''))
+
+from py_common.env_configuration import EnvConfiguration
+
+os.environ["XLR_PYSDK_VERIFY_SSL_CERT"] = "false"
+
+cfg = EnvConfiguration({'LOG_LEVEL': {'default': logging.DEBUG}}) # XXXrs DEBUG!
+
+# Configure logging
+logging.basicConfig(level=cfg.get('LOG_LEVEL'),
+                    format="'%(asctime)s - %(levelname)s - %(threadName)s - %(funcName)s - %(message)s",
+                    handlers=[logging.StreamHandler(sys.stdout)])
+logger = logging.getLogger(__name__)
+
+# Arrange for orderly shutdown on signal 
+
+WAIT_TIMEOUT = 10
+SHUTDOWN = False
+def do_shutdown(signum, frame):
+    logger.info("signal {}".format(signum))
+    SHUTDOWN = True
+
+
+signal.signal(signal.SIGINT, do_shutdown)
+signal.signal(signal.SIGHUP, do_shutdown)
+signal.signal(signal.SIGTERM, do_shutdown)
+
+"""
+XXXrs - FUTURE - performance options...
+if [ -z "$PERF_PREFIX" ]; then
+    perf_options=""
+else
+    export NETSTORE="${NETSTORE:-/netstore/qa/jenkins}"
+    RESULTS_PATH="${NETSTORE}/${JOB_NAME}/${BUILD_ID}"
+    mkdir -p "$RESULTS_PATH"
+    perf_options="-o ${RESULTS_PATH}/${PERF_PREFIX}"
+fi
+"""
+
+def test_jdbc_cmds(*, tpctype, argsdict, imd):
+    """
+    Return a pair of commands: (load_cmd, test_cmd)
+    """
+    sid = random.randint(1000000000,9999999999)
+    xcSess = "{}Sess_{}".format(tpctype, sid)
+    sdkSess = "{}SdkSess_{}".format(tpctype, sid)
+
+    # Base part of any commands...
+    cmd = "{} -H {} --jdbc-port {} -a {} -U {} -P {}"\
+          .format(argsdict['test_jdbc_path'],
+                  argsdict['jdbc_host'],
+                  argsdict['jdbc_port'],
+                  argsdict['api_port'],
+                  argsdict['user'],
+                  argsdict['pass'])
+
+    cmd += " -t test_{}_xd_dataflows".format(tpctype)
+    cmd += " -p {}".format(argsdict["{}_plan".format(tpctype)])
+    cmd += " --SF {}".format(argsdict["{}_sf".format(tpctype)])
+    cmd += " --xcSess {}".format(xcSess)
+    cmd += " --sdkSess {}".format(sdkSess)
+    load_cmd = "{} -w 0 -K".format(cmd)
+
+    cmd += " --ignore-xcalar" # ENG-8720
+    cmd += " --loop {} -s {}".format(argsdict["{}_loops".format(tpctype)],
+                                     argsdict["{}_seed".format(tpctype)])
+
+    skips = argsdict.get("{}_skips".format(tpctype), None)
+    if skips:
+        for name in skips.split(','):
+            cmd += " --skip {}".format(name)
+
+    if imd:
+        cmd += " -w 1 --testMergeOp"
+    else:
+        cmd += " -w {}".format(argsdict["{}_workers".format(tpctype)])
+
+    return (load_cmd, cmd)
+
+def subproc_start(*, cmd):
+    logger.info("START: {}".format(cmd))
+    args = ["unbuffer"]
+    args.extend(shlex.split(cmd))
+    return subprocess.Popen(args)
+
+def subprocs_done(*, subprocs):
+    done = True
+    for info in subprocs:
+        rc = info['rc']
+        if rc is not None:
+            continue
+        p = info['proc']
+        rc = p.poll()
+        if rc is None:
+            done = False
+            continue
+        info['rc'] = rc
+        try:
+            p.wait(timeout=WAIT_TIMEOUT)
+        except subprocess.TimeoutExpired:
+            logger.exception()
+    return done
+
+def subprocs_stop(*, subprocs):
+    for info in subprocs:
+        if info['rc'] is not None:
+            continue
+        info['proc'].send_signal(signal.SIGTERM)
+        try:
+            info['rc'] = p.wait(timeout=WAIT_TIMEOUT)
+        except subprocess.TimeoutExpired:
+            logger.exception()
+
+def subprocs_rc(*, subprocs):
+    # return first non-zero exit code
+    for info in subprocs:
+        rc = info['rc']
+        if rc:
+            return rc
+    return 0
+
+if __name__ == '__main__':
+
+
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("--test_id", help="Test ID string", required=True)
+    parser.add_argument("--test_jdbc_path", help="Path to test_jdbc", required=True)
+    parser.add_argument("--jdbc_host", help="JDBC server hostname", required=True)
+    parser.add_argument("--jdbc_port", help="JDBC server port", default=10000)
+    parser.add_argument("--api_port", help="API port", default=443)
+    parser.add_argument("--user", help="username", default="admin")
+    parser.add_argument("--pass", help="password", default="admin")
+
+    parser.add_argument("--tpcds_imd_merge", help="Do TPC-DS IMD merge test", action="store_true")
+
+    parser.add_argument("--tpcds_sf", help="TPC-DS Scale Factor", default=10)
+    parser.add_argument("--tpcds_plan", help="TPC-DS Plan Path", default="/netstore/datasets/tpcds_new/sf_10")
+    parser.add_argument("--tpcds_workers", help="TPC-DS workers", type=int, default=0)
+    parser.add_argument("--tpcds_loops", help="TPC-DS test iterations (loops)", type=int, default=1)
+    parser.add_argument("--tpcds_seed", help="TPC-DS random seed", type=int, default=123)
+    parser.add_argument("--tpcds_skips", help="Comma-separated list of TPC-DS queries to skip")
+
+    parser.add_argument("--tpch_imd_merge", help="Do TPC-H IMD merge test", action="store_true")
+
+    parser.add_argument("--tpch_sf", help="TPC-H Scale Factor", default=10)
+    parser.add_argument("--tpch_plan", help="TPC-H Plan Path", default="/netstore/datasets/tpch_new/sf_10")
+    parser.add_argument("--tpch_workers", help="TPC-H workers", type=int, default=0)
+    parser.add_argument("--tpch_loops", help="TPC-H test iterations (loops)", type=int, default=1)
+    parser.add_argument("--tpch_seed", help="TPC-H random seed", type=int, default=456)
+    parser.add_argument("--tpch_skips", help="Comma-separated list of TPC-H queries to skip")
+
+    args = parser.parse_args()
+    argsdict = vars(args)
+
+    cmds = []
+    if argsdict["tpcds_workers"] > 0:
+        cmds.append(test_jdbc_cmds(tpctype="tpcds", argsdict=argsdict, imd=False))
+
+    if argsdict["tpcds_imd_merge"]:
+        cmds.append(test_jdbc_cmds(tpctype="tpcds", argsdict=argsdict, imd=True))
+
+    if argsdict["tpch_workers"] > 0:
+        cmds.append(test_jdbc_cmds(tpctype="tpch", argsdict=argsdict, imd=False))
+
+    if argsdict["tpch_imd_merge"]:
+        cmds.append(test_jdbc_cmds(tpctype="tpch", argsdict=argsdict, imd=True))
+
+    load_cmds, test_cmds = zip(*cmds)
+
+    logger.debug("load_cmds: {}".format(load_cmds))
+    logger.debug("test_cmds: {}".format(test_cmds))
+
+    subprocs = []
+    for idx,cmd in enumerate(load_cmds):
+        p = subproc_start(cmd=cmd)
+        subprocs.append({'proc': p, 'rc': None})
+    while not SHUTDOWN and not subprocs_done(subprocs=subprocs):
+        #logger.debug("sleeping...")
+        time.sleep(1)
+    if SHUTDOWN:
+        subprocs_stop(subprocs=subprocs)
+        sys.exit(1)
+
+    rc = subprocs_rc(subprocs=subprocs)
+    if (rc):
+        sys.exit(rc)
+
+    subprocs = []
+    for idx,cmd in enumerate(test_cmds):
+        p = subproc_start(cmd=cmd)
+        subprocs.append({'proc': p, 'rc': None})
+    while not SHUTDOWN and not subprocs_done(subprocs=subprocs):
+        #logger.debug("sleeping...")
+        time.sleep(1)
+    if SHUTDOWN:
+        subprocs_stop(subprocs=subprocs)
+        sys.exit(1)
+    sys.exit(subprocs_rc(subprocs=subprocs))
