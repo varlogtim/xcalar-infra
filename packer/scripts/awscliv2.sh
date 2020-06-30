baec57e301bd290e351cfbab24d18a65356fe345
Amit Bakshi <abakshi@xcalar.com>
2020-06-30 14:09:36 -0700

    ENG-6030 az/awscli: Wrapper for hosts lacking these
    Some hosts can't run azcli
    
    Change-Id: I0cc5692cb3c6853091b7c0d89c1c09c6be225a31

--- packer/scripts/awscliv2.sh
+++ packer/scripts/awscliv2.sh
@@ -3,24 +3,32 @@ set -e
 TMPDIR=$(mktemp -d /tmp/awscli-XXXXXX)
 cd $TMPDIR
 curl -L "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-if ! command -v unzip >/dev/null; then
-    sudo yum install -y unzip
+if command -v sudo >/dev/null; then
+    SUDO='sudo -H'
 fi
-unzip awscliv2.zip
+if command -v bsdtar >/dev/null; then
+    $SUDO tar zxf awscliv2.zip
+elif command -v unzip >/dev/null; then
+    $SUDO unzip awscliv2.zip
+else
+    echo >&2 "Need unzip or bsdtar"
+    $SUDO yum install -y unzip
+    $SUDO unzip awscliv2.zip
+fi
 ver=$(aws/dist/aws --version | cut -d' ' -f1 | cut -d'/' -f2)
 bundle=awscliv2-bundle-${ver}.tar.gz
 tar czf $bundle aws
 PREFIX=/opt/awscliv2
 ITERATION=${ITERATION:-1}
 
-sudo rm -rf $PREFIX
-sudo mkdir -p $PREFIX
-sudo -H aws/install -i $PREFIX -b /usr/bin
-sudo ln -sfn ${PREFIX}/v2/current/bin/aws_completer /usr/bin/
-echo 'complete -C /usr/bin/aws_completer aws' | sudo tee /usr/share/bash-completion/completions/aws >/dev/null
+$SUDO rm -rf $PREFIX
+$SUDO mkdir -p $PREFIX
+$SUDO aws/install -i $PREFIX -b /usr/bin
+$SUDO ln -sfn ${PREFIX}/v2/current/bin/aws_completer /usr/bin/
+echo 'complete -C /usr/bin/aws_completer aws' | $SUDO tee /usr/share/bash-completion/completions/aws >/dev/null
 cd - >/dev/null
 
 TAR=awscliv2-${ver}-${ITERATION}.tar
-tar cvf $TAR  -C / .${PREFIX} ./usr/bin/aws ./usr/bin/aws_completer ./usr/share/bash-completion/completions/aws
+$SUDO tar cvf $TAR  -C / .${PREFIX} ./usr/bin/aws ./usr/bin/aws_completer ./usr/share/bash-completion/completions/aws
 
-rm -rf $TMPDIR
+$SUDO rm -rf $TMPDIR
