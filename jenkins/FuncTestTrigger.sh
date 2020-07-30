8af25c8085c7c131c84f8df9ce0ba3352a31209e
Robert Stephens <rstephens@xcalar.com>
2020-07-30 13:22:51 -0700

    ENG-9303 - restore genBuildArtifacts
    Installer-based tests don't have access to jenkinsUtils.sh
    So I'm restoring the prior in-line genBuildArtifacts so we
    can mane progress...
    
    Change-Id: I3949944327326d39c6bc1889484870d1e9631d54

--- jenkins/FuncTestTrigger.sh
+++ jenkins/FuncTestTrigger.sh
@@ -48,6 +48,62 @@ funcstatsd () {
     fi
 }
 
+
+genBuildArtifacts() {
+    # drive on for any errors
+    set +e
+
+    mkdir -p ${NETSTORE}/${JOB_NAME}/${BUILD_ID}
+    mkdir -p `pwd`/tmpdir
+
+    find /tmp ! -path /tmp -newer /tmp/${JOB_NAME}_${BUILD_ID}_START_TIME 2>/dev/null | xargs cp --parents -rt `pwd`/tmpdir/
+
+    PIDS=()
+    for dir in tmpdir /var/log/xcalar /var/opt/xcalar/dataflows; do
+        if [ -d $dir ]; then
+            if [ "$dir" = "/var/log/xcalar" ]; then
+                tar -cf var_log_xcalar.tar.bz2 --use-compress-prog=pbzip2 $dir &
+            elif [ "$dir" = "/var/opt/xcalar/dataflows" ]; then
+                tar -cf xcalar_dataflows.tar.bz2 --use-compress-prog=pbzip2 $dir &
+            else
+                tar -cf $dir.tar.bz2 --use-compress-prog=pbzip2 $dir &
+            fi
+            PIDS+=($!)
+        fi
+    done
+
+    wait "${PIDS[@]}"
+    ret=$?
+    if [ $ret -ne 0 ]; then
+        echo >&2 "ERROR($ret): tar failed"
+    fi
+
+    for dir in tmpdir /var/log/xcalar /var/opt/xcalar/dataflows; do
+        if [ "$dir" = "/var/log/xcalar" ]; then
+            cp var_log_xcalar.tar.bz2 ${NETSTORE}/${JOB_NAME}/${BUILD_ID}
+            rm var_log_xcalar.tar.bz2
+            rm $dir/*
+        elif [ "$dir" = "/var/opt/xcalar/dataflows" ]; then
+            cp xcalar_dataflows.tar.bz2 ${NETSTORE}/${JOB_NAME}/${BUILD_ID}
+            rm xcalar_dataflows.tar.bz2
+        else
+            if [ -f $dir.tar.bz2 ]; then
+                cp $dir.tar.bz2 ${NETSTORE}/${JOB_NAME}/${BUILD_ID}
+                rm $dir.tar.bz2
+                if [ -d $dir ]; then
+                    rm -r $dir/*
+                fi
+            fi
+        fi
+    done
+
+    echo >&2 "Build artifacts copied to ${NETSTORE}/${JOB_NAME}/${BUILD_ID}"
+
+    rm /tmp/${JOB_NAME}_${BUILD_ID}_START_TIME
+}
+
+trap "genBuildArtifacts" EXIT
+
 # Build the source
 source doc/env/xc_aliases
 
@@ -77,12 +133,8 @@ mkdir -p $INSTALL_OUTPUT_DIR
 set +e
 $INSTALLER_PATH -d $INSTALL_OUTPUT_DIR -v
 rm $XCE_CONFIG
-
-# XLRDIR is under INSTALL_OUTPUT_DIR so this can't happen earlier
-. $XLRDIR/bin/jenkins/jenkinsUtils.sh
 set -e
 
-trap "genBuildArtifacts" EXIT
 
 $XLRDIR/scripts/genConfig.sh $INSTALL_OUTPUT_DIR/etc/xcalar/template.cfg $XCE_CONFIG `hostname`
 echo "$CONFIG_PARAMS" | tee -a $XCE_CONFIG
