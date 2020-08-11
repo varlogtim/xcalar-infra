51611534e2a308c2c016f37f053468916a8ffc62
Robert Stephens <rstephens@xcalar.com>
2020-08-11 14:46:46 -0700

    ENG-9303 - collect support bundle artifacts
    Change-Id: I04e934ef73a6cce703cbb20c51ca97e587cf7e8f

--- jenkins/FuncTestTrigger.sh
+++ jenkins/FuncTestTrigger.sh
@@ -121,6 +121,7 @@ done
 restartXcalar || true
 
 if xccli -c version 2>&1 | grep -q 'Error'; then
+    genSupport
     echo "Could not even start usrnodes after install"
     exit 1
 fi
@@ -200,23 +201,29 @@ for jj in `seq 1 $NUM_ITERATIONS`; do
         done
 
         if [ $anyfailed -eq 1 ]; then
+            genSupport
+
+            # --------------------
+            # Obsolete
+            # --------------------
+            # XXXrs - Cores etc. should be captured by genSupport
             # copy out the usrnode binary and retinas
-            now=$(date +"%Y%m%d_%H%M%S")
-            artdir="`pwd`/${now}_${BUILD_ID}"
-            mkdir -p $artdir
-            filepath="${artdir}/usrnode.$now"
-            retinapath="${artdir}/retina.$now"
-            exportpath="${artdir}/export.$now"
-            pubTablepath="${artdir}/pubTable.$now"
-            # XXXrs - Can race if multiple builds running on same node.
-            #         Blindly sweeps up any cores left-over from other
-            #         builds which failed to properly clean up. :/
-            mv core.childnode.* "$artdir"
-            mv core.usrnode.* "$artdir"
-            cp $XLRDIR/bin/usrnode "$filepath"
-            cp -r /var/opt/xcalar/dataflows/ "$retinapath"
-            cp -r /var/opt/xcalar/export/ "$exportpath"
-            cp -r /var/opt/xcalar/published/ "$pubTablepath"
+            #now=$(date +"%Y%m%d_%H%M%S")
+            #artdir="`pwd`/${now}_${BUILD_ID}"
+            #mkdir -p $artdir
+            #filepath="${artdir}/usrnode.$now"
+            #retinapath="${artdir}/retina.$now"
+            #exportpath="${artdir}/export.$now"
+            #pubTablepath="${artdir}/pubTable.$now"
+            ## XXXrs - Can race if multiple builds running on same node.
+            ##         Blindly sweeps up any cores left-over from other
+            ##         builds which failed to properly clean up. :/
+            #mv core.childnode.* "$artdir"
+            #mv core.usrnode.* "$artdir"
+            #cp $XLRDIR/bin/usrnode "$filepath"
+            #cp -r /var/opt/xcalar/dataflows/ "$retinapath"
+            #cp -r /var/opt/xcalar/export/ "$exportpath"
+            #cp -r /var/opt/xcalar/published/ "$pubTablepath"
             # mark the test as failed
             funcstatsd "$Test" "FAIL" "$gitsha"
             echo "not ok ${ii} - $Test" | tee -a $TAP
