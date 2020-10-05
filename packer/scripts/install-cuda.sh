980b6f618e1c777985042cee7549637a70d7125a
Amit Bakshi <abakshi@xcalar.com>
2020-10-05 12:54:07 -0700

    ENG-6030 Use newest NVIDIA driver instead of the CUDA one
    Change-Id: I199be91daaa9cb78f8df01974f7109ab5d9cf903

--- packer/scripts/install-cuda.sh
+++ packer/scripts/install-cuda.sh
@@ -11,15 +11,15 @@ CUDA_VERSION=${CUDA_VERSION:-10.0}
 BASE_URL="${BASE_URL:-https://storage.googleapis.com/repo.xcalar.net/deps/nvidia}"
 case "$CUDA_VERSION" in
     10.0)
-        CUDA_COMPONENTS="${CUDA_COMPONENTS:-cuda_10.0.130_410.48_linux.run cudnn-10.0-linux-x64-v7.6.5.32.tgz}"
+        CUDA_COMPONENTS="${CUDA_COMPONENTS:-NVIDIA-Linux-x86_64-455.23.05.run cuda_10.0.130_410.48_linux.run cudnn-10.0-linux-x64-v7.6.5.32.tgz}"
         PYTHON_PKGS="${PYTHON_PKGS:-tensorflow-gpu==1.13.1 pandas==0.22 keras==2.4.3}"
         ;;
     10.1)
-        CUDA_COMPONENTS="${CUDA_COMPONENTS:-cuda_10.1.243_418.87.00_linux.run cudnn-10.0-linux-x64-v7.6.5.32.tgz}"
+        CUDA_COMPONENTS="${CUDA_COMPONENTS:-NVIDIA-Linux-x86_64-455.23.05.run cuda_10.1.243_418.87.00_linux.run cudnn-10.0-linux-x64-v7.6.5.32.tgz}"
         PYTHON_PKGS="${PYTHON_PKGS:-tensorflow-gpu==1.13.1 pandas==0.22 keras==2.4.3}"
         ;;
     11.1)
-        CUDA_COMPONENTS="${CUDA_COMPONENTS:-cuda_11.1.0_455.23.05_linux.run cudnn-10.0-linux-x64-v7.6.5.32.tgz}"
+        CUDA_COMPONENTS="${CUDA_COMPONENTS:-NVIDIA-Linux-x86_64-455.23.05.run cuda_11.1.0_455.23.05_linux.run cudnn-10.0-linux-x64-v7.6.5.32.tgz}"
         PYTHON_PKGS="${PYTHON_PKGS:-tensorflow>=2.0 pandas==0.22 keras==2.4.3}"
         ;;
     *) echo >&2 "ERROR: Don't know how to install CUDA $CUDA_VERSION";;
@@ -33,9 +33,13 @@ cd "$TMP"
 for ii in $CUDA_COMPONENTS; do
 	curl -f -L -O "${BASE_URL}/${ii}"
 	case "$ii" in
+        NVIDIA*.run)
+			yum install -y dkms kernel-devel
+            bash "$ii" --accept-license --no-questions  --silent
+            ;;
 		cuda_*.run)
 			yum install -y dkms kernel-devel
-			bash "$ii" --silent --toolkit --driver
+			bash "$ii" --silent --toolkit
 			BN=$(basename $(readlink -f /usr/local/cuda) | sed 's/\./-/g')
             test -e /etc/ld.so.conf.d/${BN}.conf || echo "$(readlink -f /usr/local/cuda/lib64)" > /etc/ld.so.conf.d/${BN}.conf
 			ldconfig
